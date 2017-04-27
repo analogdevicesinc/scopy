74e9e0099dc1df75975ea899c3dc269b7398093c
Paul Cercueil <paul.cercueil@analog.com>
2017-04-27 13:01:34 +0200

    Add OscScaleZoomer class
    The OscScaleZoomer object can be used on plots that use OscScaleDraw on
    their xTop and yLeft axes.
    
    This provides the ability to zoom on plots, and shows the value
    corresponding to the position of the mouse cursor right next to it,
    using the proper units.
    
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

---
+++ src/osc_scale_zoomer.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "DisplayPlot.h"
+#include "osc_scale_zoomer.h"
+
+#include <QString>
+
+using namespace adiscope;
+
+OscScaleZoomer::OscScaleZoomer(QWidget *parent) : QwtPlotZoomer(parent)
+{
+	setTrackerMode(QwtPicker::AlwaysOn);
+
+	QPen pen(QColor("#999999"));
+	setRubberBandPen(pen);
+	setTrackerPen(pen);
+
+	setTrackerFont(QFont("DejaVu Sans", 10, 75));
+	setZoomBase();
+}
+
+OscScaleZoomer::~OscScaleZoomer()
+{
+}
+
+QwtText OscScaleZoomer::trackerText(const QPoint& pos) const
+{
+	const OscScaleDraw *draw_x = static_cast<const OscScaleDraw *>(
+			plot()->axisScaleDraw(QwtPlot::xTop));
+	const OscScaleDraw *draw_y = static_cast<const OscScaleDraw *>(
+			plot()->axisScaleDraw(QwtPlot::yLeft));
+	QwtDoublePoint dp = QwtPlotZoomer::invTransform(pos);
+	QString text;
+
+	text += draw_x->label(dp.x()).text();
+	text += ", ";
+	text += draw_y->label(dp.y()).text();
+
+	return QwtText(text);
+}
+
+void OscScaleZoomer::cancel()
+{
+	reset();
+}
+
+QColor OscScaleZoomer::getColor() const
+{
+	return trackerPen().color();
+}
+
+void OscScaleZoomer::setColor(const QColor& color)
+{
+	QPen pen(color);
+	setRubberBandPen(pen);
+	setTrackerPen(pen);
+}
---
+++ src/osc_scale_zoomer.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef OSC_SCALE_ZOOMER_H
+#define OSC_SCALE_ZOOMER_H
+
+#include <qwt_plot_zoomer.h>
+
+namespace adiscope {
+	class OscScaleZoomer : public QwtPlotZoomer
+	{
+		Q_OBJECT
+
+		Q_PROPERTY(QColor color READ getColor WRITE setColor);
+
+	public:
+		explicit OscScaleZoomer(QWidget *parent);
+		~OscScaleZoomer();
+
+		void cancel();
+
+		QColor getColor() const;
+		void setColor(const QColor& color);
+
+	private:
+		QwtText trackerText(const QPoint& pos) const;
+	};
+}
+
+#endif /* OSC_SCALE_ZOOMER_H */
