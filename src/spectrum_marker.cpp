af8c6b5e6663c422184825c2245dab6d3dd113d5
Dan Nechita <dan.nechita@analog.com>
2017-06-14 11:58:51 +0300

    src/spectrum_marker: Create markers for spectrum analyzer from QwtPlotMarker
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

---
+++ src/spectrum_marker.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2017 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "spectrum_marker.hpp"
+
+#include <qwt_plot.h>
+#include <qwt_symbol.h>
+
+using namespace adiscope;
+
+SpectrumMarker::SpectrumMarker(const QString &title, bool movable) :
+	QwtPlotMarker(title), m_xAxis(QwtPlot::xBottom),
+	m_yAxis(QwtPlot::yLeft), m_movable(movable)
+{
+}
+
+SpectrumMarker::SpectrumMarker(const QString &title, QwtAxisId xAxis,
+	QwtAxisId yAxis, bool movable) :
+	QwtPlotMarker(title), m_xAxis(xAxis), m_yAxis(yAxis), m_movable(movable)
+{
+}
+
+SpectrumMarker::~SpectrumMarker()
+{
+}
+
+bool SpectrumMarker::selected() const
+{
+	return m_selected;
+}
+
+void SpectrumMarker::setSelected(bool on)
+{
+	if (m_selected != on) {
+		m_selected = on;
+
+		QBrush brush = symbol()->brush();
+		if (on) {
+			brush.setColor(symbol()->pen().color());
+		} else {
+			brush.setColor(QColor(255, 242, 0));
+		}
+
+		QwtSymbol *new_sym = new QwtSymbol(symbol()->style(),
+			brush, symbol()->pen(), symbol()->size());
+		setSymbol(new_sym);
+	}
+}
+
+bool SpectrumMarker::movable() const
+{
+	return m_movable;
+}
+
+void SpectrumMarker::setMovable(bool on)
+{
+	m_movable = on;
+}
+
+QPoint SpectrumMarker::plotValueToPixels(const QPointF& point) const
+{
+	QPoint pixelPoint(0, 0);
+
+	if (plot()) {
+		const QwtScaleMap xMap = plot()->canvasMap(m_xAxis);
+		const QwtScaleMap yMap = plot()->canvasMap(m_yAxis);
+		QPointF p = QwtScaleMap::transform(xMap, yMap, point);
+		pixelPoint = QPoint(p.x(),p.y());
+	}
+
+	return pixelPoint;
+}
+
+QPointF SpectrumMarker::plotPixelsToValue(const QPoint& point) const
+{
+	QPointF valuePoint(0.0, 0.0);
+
+	if (plot()) {
+		const QwtScaleMap xMap = plot()->canvasMap(m_xAxis);
+		const QwtScaleMap yMap = plot()->canvasMap(m_yAxis);
+
+		valuePoint = QwtScaleMap::invTransform(xMap, yMap, point);
+	}
+
+	return valuePoint;
+}
+
+QRect SpectrumMarker::boundingRectPixels() const
+{
+	QPoint markerPixPos = plotValueToPixels(value());
+	QRect symbolRect = symbol()->boundingRect();
+
+	return QRect(symbolRect.topLeft() + markerPixPos, symbolRect.size());
+}
---
+++ src/spectrum_marker.hpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef SPECTRUM_MARKER_H
+#define SPECTRUM_MARKER_H
+
+#include <qwt_plot_marker.h>
+
+namespace adiscope {
+
+class SpectrumMarker: public QwtPlotMarker
+{
+public:
+	SpectrumMarker(const QString& title, bool movable = true);
+	SpectrumMarker(const QString &title, QwtAxisId xAxis, QwtAxisId yAxis,
+		bool movable = true);
+	virtual ~SpectrumMarker();
+
+	bool selected() const;
+	void setSelected(bool on);
+
+	bool movable() const;
+	void setMovable(bool on);
+
+	QPoint plotValueToPixels(const QPointF& point) const;
+	QPointF plotPixelsToValue(const QPoint& point) const;
+
+	QRect boundingRectPixels() const;
+
+private:
+	QwtAxisId m_xAxis;
+	QwtAxisId m_yAxis;
+	bool m_selected;
+	bool m_movable;
+};
+
+} /* namespace adiscope */
+
+#endif /* SPECTRUM_MARKER_H */
