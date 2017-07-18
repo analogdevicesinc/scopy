7f21090b00eef8d62443f9e057ce7cfbbf1c552e
Daniel Guramulta <daniel.guramulta@analog.com>
2017-07-18 17:07:41 +0300

    Created a drag zone for the menu options
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

---
+++ src/dragzone.cpp
@@ -0,0 +1,68 @@
+#include "dragzone.h"
+#include "ui_dragzone.h"
+
+using namespace adiscope;
+
+DragZone::DragZone(QWidget *parent) :
+	QWidget(parent),
+	ui(new Ui::DragZone)
+{
+	ui->setupUi(this);
+	position = 8;
+	setAcceptDrops(true);
+	this->installEventFilter(this);
+}
+
+DragZone::~DragZone()
+{
+	delete ui;
+	this->removeEventFilter(this);
+}
+
+void DragZone::dragEnterEvent(QDragEnterEvent *event)
+{
+	if (event->mimeData()->hasFormat("menu/option")){
+		short from = (short)event->mimeData()->data("menu/option")[1];
+		if (from == position){
+			event->ignore();
+			return;
+		}
+	}
+	event->accept();
+}
+
+void DragZone::dragMoveEvent(QDragMoveEvent *event)
+{
+	Q_EMIT highlightLastSeparator(true);
+	event->accept();
+}
+
+void DragZone::dragLeaveEvent(QDragLeaveEvent *event)
+{
+	Q_EMIT highlightLastSeparator(false);
+	event->accept();
+}
+
+void DragZone::dropEvent(QDropEvent *event)
+{
+	Q_EMIT highlightLastSeparator(false);
+	short from, to;
+	if (event->source() == this && event->possibleActions() & Qt::MoveAction){
+		return;
+	}
+	if (event->mimeData()->hasFormat("menu/option")){
+		from = (short)event->mimeData()->data("menu/option")[1];
+		to = (short)position;
+		Q_EMIT requestPositionChange(from, to, true);
+	}
+}
+
+bool DragZone::eventFilter(QObject *watched, QEvent *event)
+{
+	if (event->type() == QEvent::DragEnter){
+		QDragEnterEvent *enterEvent = static_cast<QDragEnterEvent *>(event);
+		if (!enterEvent->mimeData()->hasFormat("menu/option"))
+			return true;
+		}
+	return QWidget::event(event);
+}
---
+++ src/dragzone.h
@@ -0,0 +1,40 @@
+#ifndef DRAGZONE_H
+#define DRAGZONE_H
+
+#include <QWidget>
+#include <QMouseEvent>
+#include <QDragMoveEvent>
+#include <QDropEvent>
+#include <QMimeData>
+
+namespace Ui {
+class DragZone;
+}
+
+namespace adiscope{
+class DragZone : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit DragZone(QWidget *parent = 0);
+	~DragZone();
+
+	bool eventFilter(QObject *watched, QEvent *event);
+
+Q_SIGNALS:
+	void requestPositionChange(int, int, bool);
+	void highlightLastSeparator(bool);
+
+private Q_SLOTS:
+	void dragEnterEvent(QDragEnterEvent *event);
+	void dragMoveEvent(QDragMoveEvent *event);
+	void dragLeaveEvent(QDragLeaveEvent *event);
+	void dropEvent(QDropEvent *event);
+
+private:
+	Ui::DragZone *ui;
+	int position;
+};
+}
+#endif // DRAGZONE_H
