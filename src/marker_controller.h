dab0429d3fae5c7dddf68d213e0e8e5f0f443538
Dan Nechita <Dan.Nechita@analog.com>
2017-08-18 18:43:01 +0300

    SpectrumAnalyzer: Add class that supports drag and drop for markers
    The class MarkerController provides the means for dragging and dropping
    marker on the plot of the spectrum analyzer.
    
    Signed-off-by: Dan Nechita <Dan.Nechita@analog.com>

---
+++ src/marker_controller.h
@@ -0,0 +1,64 @@
+#ifndef MARKER_CONTROLLER_H
+#define MARKER_CONTROLLER_H
+
+#include <QObject>
+#include <QList>
+#include <qwt_plot.h>
+#include <memory>
+
+class QwtPlotPicker;
+
+namespace adiscope {
+
+class SpectrumMarker;
+
+class MarkerController: public QObject
+{
+	Q_OBJECT
+
+public:
+	typedef std::shared_ptr<SpectrumMarker> marker_sptr;
+
+	explicit MarkerController(QwtPlot *plot);
+	MarkerController(const MarkerController&) = delete;
+	void operator=(const MarkerController&) = delete;
+
+	bool enabled() const;
+	void setEnabled(bool en);
+
+	void registerMarker(marker_sptr marker);
+	void unRegisterMarker(marker_sptr marker);
+
+	marker_sptr selectedMarker() const;
+	void markerBringToFront(marker_sptr marker);
+
+	const QwtPlot *plot() const;
+	QwtPlot *plot();
+
+Q_SIGNALS:
+	void markerSelected(std::shared_ptr<SpectrumMarker>);
+	void markerPositionChanged(std::shared_ptr<SpectrumMarker>);
+	void markerReleased(std::shared_ptr<SpectrumMarker>);
+
+private Q_SLOTS:
+	void onPickerSelected(QPointF);
+	void onPickerMoved(QPoint);
+
+private:
+	void selectMarker(marker_sptr marker);
+
+private:
+	QwtPlotPicker *d_picker;
+
+	QList<marker_sptr> d_markers;
+	QList<marker_sptr> d_mrks_overlap_order;
+	marker_sptr d_picked_mrk;
+	marker_sptr d_selected_mkr;
+
+	bool d_item_moving;
+	double d_mrks_default_z;
+};
+
+} /* namespace adiscope */
+
+#endif // MARKER_CONTROLLER_H
