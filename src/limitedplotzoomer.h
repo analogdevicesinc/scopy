1e1ba4b2221e2bae22c60125151c39a1eb84a8b4
Daniel Guramulta <daniel.guramulta@analog.com>
2017-08-25 13:11:02 +0300

    LimitedPlotZoomer: Created a zoomer that has a limit when zooming
    This will not allow a user to zoom outside of the plot with respect to
    the x-axis.
    
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

---
+++ src/limitedplotzoomer.h
@@ -0,0 +1,17 @@
+#ifndef LIMITEDPLOTZOOMER_H
+#define LIMITEDPLOTZOOMER_H
+
+#include <qwt_plot_zoomer.h>
+
+namespace adiscope {
+class LimitedPlotZoomer : public QwtPlotZoomer
+{
+public:
+        LimitedPlotZoomer(QWidget*, bool doReplot = false);
+
+protected:
+        virtual void zoom(const QRectF &);
+};
+}
+
+#endif // LIMITEDPLOTZOOMER_H
