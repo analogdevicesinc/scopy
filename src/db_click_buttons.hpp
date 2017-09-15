089dd7e40105a32223777d0657396461de7da635
Dan Nechita <Dan.Nechita@analog.com>
2017-09-15 12:10:31 +0300

    src/db_click_buttons: Group of buttons that can be toggled by 2Xclicks
    The widget is designed as a panel with buttons that can have only one
    button selected at a time and once a button is selected, only the it can
    be toggled.
    
    Signed-off-by: Dan Nechita <Dan.Nechita@analog.com>

---
+++ src/db_click_buttons.hpp
@@ -0,0 +1,53 @@
+#ifndef DB_CLICK_BUTTONS_HPP
+#define DB_CLICK_BUTTONS_HPP
+
+#include <QWidget>
+#include <QPushButton>
+#include <QList>
+#include <QString>
+
+namespace Ui {
+class DbClickButtons;
+}
+
+namespace adiscope {
+
+class DbClickButtons: public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit DbClickButtons(QWidget *parent = 0, int maxRowBtnCount = 5);
+	~DbClickButtons();
+
+	int selectedButton() const;
+	void setSelectedButton(int btnId);
+
+	bool buttonChecked(int btnId) const;
+	void setButtonChecked(int btnId, bool checked);
+
+	int buttonCount() const;
+	void setButtonCount(int count);
+
+Q_SIGNALS:
+	void buttonSelected(int);
+	void buttonToggled(int, bool);
+
+private Q_SLOTS:
+	void onButtonClicked();
+
+private:
+	void toggleButton(int btnId);
+
+private:
+	Ui::DbClickButtons *ui;
+	QList<QPushButton *> btn_list;
+	QList<bool> btn_states;
+	int selected_btn;
+	int max_row_btn_cnt;
+	QList<QString> color_codes;
+};
+
+} // namespace adiscope
+
+#endif // DB_CLICK_BUTTONS_HPP
