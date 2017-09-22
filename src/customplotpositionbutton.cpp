b469788124ddafc4fc222ffd9f433d59f1c03cf2
Daniel Guramulta <daniel.guramulta@analog.com>
2017-09-22 15:10:30 +0300

    CustomPlotPositionButton: Added a new widget
    With this widget the user can control the position of an object that is
    drawn on the plot. The widget will allow the user to select 4 position:
    top left, top right, bottom left and bottom right.
    
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

---
+++ src/customplotpositionbutton.cpp
@@ -0,0 +1,52 @@
+#include "customplotpositionbutton.h"
+#include "ui_customplotpositionbutton.h"
+#include "dynamicWidget.hpp"
+
+using namespace adiscope;
+
+CustomPlotPositionButton::CustomPlotPositionButton(QWidget *parent) :
+	ColoredQWidget(parent),
+	ui(new Ui::CustomPlotPositionButton)
+{
+	ui->setupUi(this);
+
+	setDynamicProperty(ui->leftTop, "selected" , true);
+	setDynamicProperty(ui->rightTop, "selected" , false);
+	setDynamicProperty(ui->rightBot, "selected" , false);
+	setDynamicProperty(ui->leftBot, "selected" , false);
+
+	ui->leftTop->setChecked(true);
+
+	btns = new QButtonGroup(this);
+
+	connect(ui->leftTop, &QPushButton::toggled, [=](bool on){
+		setDynamicProperty(ui->leftTop, "selected" , on);
+		if (on)
+			Q_EMIT positionChanged(topLeft);
+	});
+	connect(ui->rightTop, &QPushButton::toggled, [=](bool on){
+		setDynamicProperty(ui->rightTop, "selected" , on);
+		if (on)
+			Q_EMIT positionChanged(topRight);
+	});
+	connect(ui->rightBot, &QPushButton::toggled, [=](bool on){
+		setDynamicProperty(ui->rightBot, "selected" , on);
+		if (on)
+			Q_EMIT positionChanged(bottomRight);
+	});
+	connect(ui->leftBot, &QPushButton::toggled, [=](bool on){
+		setDynamicProperty(ui->leftBot, "selected" , on);
+		if (on)
+			Q_EMIT positionChanged(bottomLeft);
+	});
+
+	btns->addButton(ui->leftBot);
+	btns->addButton(ui->leftTop);
+	btns->addButton(ui->rightBot);
+	btns->addButton(ui->rightTop);
+}
+
+CustomPlotPositionButton::~CustomPlotPositionButton()
+{
+	delete ui;
+}
---
+++ src/customplotpositionbutton.h
@@ -0,0 +1,40 @@
+#ifndef CUSTOMPLOTPOSITIONBUTTON_H
+#define CUSTOMPLOTPOSITIONBUTTON_H
+
+#include <QWidget>
+#include "coloredQWidget.hpp"
+#include <QButtonGroup>
+
+namespace Ui {
+class CustomPlotPositionButton;
+}
+
+namespace adiscope {
+class CustomPlotPositionButton : public ColoredQWidget
+{
+	Q_OBJECT
+
+public:
+
+	enum ReadoutsPosition {
+		topLeft,
+		topRight,
+		bottomLeft,
+		bottomRight
+	};
+
+	explicit CustomPlotPositionButton(QWidget *parent = 0);
+	~CustomPlotPositionButton();
+
+Q_SIGNALS:
+	void positionChanged(CustomPlotPositionButton::ReadoutsPosition position);
+
+
+private:
+	Ui::CustomPlotPositionButton *ui;
+	QButtonGroup *btns;
+};
+}
+
+
+#endif // CUSTOMPLOTPOSITIONBUTTON_H
