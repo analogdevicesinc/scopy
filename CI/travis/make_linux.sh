c104cc4091e8b466fc463eccd236c84027ce1953
AlexandraTrifan <Alexandra.Trifan@analog.com>
2017-12-07 13:22:19 +0200

    .travis.yml: Add initial Linux build script for Scopy.
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

---
+++ CI/travis/make_linux.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cd ${TRAVIS_BUILD_DIR}/build
+mkdir -p appdir/usr/bin/decoders
+mkdir -p appdir/usr/lib/python3.5
+mkdir -p appdir/usr/lib/python2.7
+
+sudo apt-get remove --auto-remove python3.4
+cp /usr/lib/python3.5/* appdir/usr/lib/python3.5/
+cp /usr/lib/python2.7/* appdir/usr/lib/python2.7/
+cp ${TRAVIS_BUILD_DIR}/resources/decoders/* appdir/usr/bin/decoders/
+
+echo "import sys,os
+prefix = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.path[0]))))
+sys.path = [ prefix+s for s in sys.path if not s.startswith(prefix) ]" > appdir/usr/lib/python3.5/sitecustomize.py
+
+echo "import sys,os
+prefix = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.path[0]))))
+sys.path = [ prefix+s for s in sys.path if not s.startswith(prefix) ]" > appdir/usr/lib/python2.7/sitecustomize.py
+
+export PYTHONPATH=${TRAVIS_BUILD_DIR}/build/appdir/usr/lib/python3.5
+
+cmake -DCMAKE_PREFIX_PATH=/opt/qt59/lib/cmake -DCMAKE_INSTALL_PREFIX=appdir/usr ..
+make -j4
+make -j4 install ; find appdir/
