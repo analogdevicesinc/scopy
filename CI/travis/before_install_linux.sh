37ef2c4ac3a4183c96bf789137719c104e0f5da9
AlexandraTrifan <Alexandra.Trifan@analog.com>
2017-12-07 11:19:35 +0200

    CI/travis/before_install_linux.sh: Add libmatio to the list of dependencies.
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

--- CI/travis/before_install_linux.sh
+++ CI/travis/before_install_linux.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository --yes ppa:fkrull/deadsnakes
 sudo add-apt-repository --yes ppa:beineri/opt-qt592-trusty
 sudo apt-get -qq update
 sudo apt-get install -y git cmake libzip-dev libusb-1.0-0-dev autoconf libtool libxml2 libxml2-dev python3.5 python-dev python3.5-dev libfftw3-dev libffi-dev
-sudo apt-get install -y libmount-dev libpcre3-dev libglib2.0-dev libsigc++-2.0-dev libglibmm-2.4-dev doxygen libglu1-mesa-dev curl flex bison
+sudo apt-get install -y libmount-dev libpcre3-dev libglib2.0-dev libsigc++-2.0-dev libglibmm-2.4-dev doxygen libglu1-mesa-dev curl flex bison libmatio2 libmatio-dev
 sudo apt-get install -y --force-yes qt59base qt59declarative qt59quickcontrols qt59svg qt59tools
 
 source /opt/qt59/bin/qt59-env.sh && qmllint client/qml/*.qml
