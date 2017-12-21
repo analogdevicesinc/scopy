c9d3c6f79dbb0f18b7f0649964ecb4df5b967aaf
Mircea Caprioru <mircea.caprioru@analog.com>
2017-12-21 14:47:00 +0200

    Debugger: Added 2017 as the copyright year. Fixed update value initialization.
    Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Analog Devices, Inc.
+ * Copyright 2017 Analog Devices, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
