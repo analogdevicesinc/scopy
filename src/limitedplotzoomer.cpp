1ea646f3c97d2c67f3a44c82e4d573d3b6501c65
Daniel Guramulta <Daniel.Guramulta@Analog.com>
2018-01-10 18:03:15 +0200

    LimitedPlotZoomer: when limiting zoom compute a bounded rectangle
    In this way when zooming outside the plot we take into consideration the original rectangle of the plot and not the new one that results after a zoom
    
    Signed-off-by: Daniel Guramulta <Daniel.Guramulta@Analog.com>

--- src/limitedplotzoomer.cpp
+++ src/limitedplotzoomer.cpp
@@ -15,17 +15,7 @@ void LimitedPlotZoomer::resetZoom()
 
 void LimitedPlotZoomer::zoom(const QRectF &rect)
 {
-    double xMinValue = plot()->axisInterval(xAxis()).minValue();
-    double xMaxValue = plot()->axisInterval(xAxis()).maxValue();
+	QRectF boundedRect = rect & zoomBase();
 
-    double x1 = rect.left();
-    double x2 = rect.right();
-
-    if (x1 < xMinValue)
-            x1 = xMinValue;
-    if (x2 > xMaxValue)
-            x2 = xMaxValue;
-
-    QwtPlotZoomer::zoom(QRectF(QPointF(x1,rect.top()),
-			QPointF(x2, rect.bottom())));
+	QwtPlotZoomer::zoom(boundedRect);
 }
