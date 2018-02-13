6a2e7783d60997e6991c8588d6b3ecf038cb3559
Daniel Guramulta <Daniel.Guramulta@Analog.com>
2018-02-13 17:40:09 +0200

    Add missing license to previous files
    Signed-off-by: Daniel Guramulta <Daniel.Guramulta@Analog.com>

--- src/homepage_controls.cpp
+++ src/homepage_controls.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2018 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include "homepage_controls.h"
 #include "ui_homepage_controls.h"
 #include <QDebug>
