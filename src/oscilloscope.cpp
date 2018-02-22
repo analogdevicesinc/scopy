8581566955e6a18713fab0eba898b440705c291a
AlexandraTrifan <Alexandra.Trifan@analog.com>
2018-02-22 19:11:42 +0200

    Oscilloscope: Fix FFT when samples are displayed sequentially.
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

--- src/oscilloscope.cpp
+++ src/oscilloscope.cpp
@@ -2209,7 +2209,7 @@ void adiscope::Oscilloscope::onHorizScaleValueChanged(double value)
 
 	// Compute the appropriate value for fft_size
 	double power;
-	if (symmBufferMode->isEnhancedMemDepth()) {
+	if (symmBufferMode->isEnhancedMemDepth() || plot_samples_sequentially) {
 		power = floor(log2(active_plot_sample_count));
 	} else {
 		power = ceil(log2(active_plot_sample_count));
@@ -2334,7 +2334,7 @@ void adiscope::Oscilloscope::onTimePositionChanged(double value)
 
 	// Compute the appropriate value for fft_size
 	double power;
-	if (symmBufferMode->isEnhancedMemDepth()) {
+	if (symmBufferMode->isEnhancedMemDepth() || plot_samples_sequentially) {
 		power = floor(log2(active_plot_sample_count));
 	} else {
 		power = ceil(log2(active_plot_sample_count));
