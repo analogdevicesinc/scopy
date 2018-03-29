45c8d8bf6a6dd755d6b5c09d6dc632fd1e33f82b
AlexandraTrifan <Alexandra.Trifan@analog.com>
2018-03-29 11:32:55 +0300

    src/pulseview/decoderstack.cpp: Increase the decoded chunk length in order to have new decode labels only when a transition occurs.
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

--- src/pulseview/pv/data/decoderstack.cpp
+++ src/pulseview/pv/data/decoderstack.cpp
@@ -54,7 +54,7 @@ namespace data {
 
 const double DecoderStack::DecodeMargin = 1.0;
 const double DecoderStack::DecodeThreshold = 0.2;
-const int64_t DecoderStack::DecodeChunkLength = 256;
+const int64_t DecoderStack::DecodeChunkLength = 1024 * 16;
 const unsigned int DecoderStack::DecodeNotifyPeriod = 1024;
 
 mutex DecoderStack::global_srd_mutex_;
