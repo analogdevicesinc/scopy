5a1a23165e3aa667d31eb02af96c480be0b8fbf7
AlexandraTrifan <Alexandra.Trifan@analog.com>
2018-04-19 14:56:40 +0300

    UserNotes: Create a new Scopy section to handle the user notes. This allows the user to save and restore from the .ini file a set of paths to certain (.html) files which are later displayed in the application.
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

---
+++ src/user_notes.cpp
@@ -0,0 +1,402 @@
+/*
+ * Copyright 2018 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "user_notes.hpp"
+#include "ui_user_notes.h"
+#include "ui_note.h"
+#include "dynamicWidget.hpp"
+
+#include <QFileInfo>
+#include <QFileDialog>
+
+using namespace adiscope;
+
+UserNotes::UserNotes(QWidget *parent) :
+        QWidget(parent),
+        ui(new Ui::UserNotes),
+        notes_api(new UserNotes_API(this)),
+        notes_group(new QButtonGroup(this)),
+        m_note_count(0)
+{
+        ui->setupUi(this);
+
+        connect(ui->addBtn, SIGNAL(toggled(bool)),
+                this, SLOT(add_btn_clicked(bool)));
+        connect(ui->browseBtn, SIGNAL(clicked(bool)),
+                SLOT(browse_btn_clicked(bool)));
+        connect(ui->saveBtn, SIGNAL(clicked(bool)),
+                SLOT(save_btn_clicked(bool)));
+        connect(ui->removeBtn, SIGNAL(clicked(bool)),
+                this, SLOT(remove_btn_clicked(bool)));
+        connect(ui->stackedWidget, SIGNAL(moved(int)),
+                this, SLOT(pageMoved(int)));
+
+        notes_group->addButton(ui->addBtn);
+        ui->addBtn->setChecked(true);
+        ui->pathLineEdit->setReadOnly(true);
+
+        notes_api->setObjectName(QString("notes"));
+
+
+	QSettings oldSettings;
+	QFile scopy(oldSettings.fileName());
+	QFile tempFile(oldSettings.fileName() + ".bak");
+        QSettings settings(tempFile.fileName(), QSettings::IniFormat);
+
+        notes_api->load(settings);
+        ui->stackedWidget->setStyleSheet("background-color: black;");
+}
+
+UserNotes::~UserNotes()
+{
+	QSettings oldSettings;
+	QFile scopy(oldSettings.fileName());
+	QFile tempFile(oldSettings.fileName() + ".bak");
+        QSettings settings(tempFile.fileName(), QSettings::IniFormat);
+
+        notes_api->save(settings);
+        delete notes_api;
+
+        for (auto n : m_notes) {
+                delete n;
+        }
+        m_notes.clear();
+
+        delete ui;
+}
+
+ApiObject* UserNotes::api()
+{
+	return notes_api;
+}
+
+void UserNotes::add_btn_clicked(bool clicked)
+{
+	setDynamicProperty(ui->addWidget, "selected", clicked);
+	if (clicked) {
+		ui->removeBtn->setEnabled(false);
+		ui->stackedWidget->slideToIndex(0);
+	}
+}
+
+void UserNotes::browse_btn_clicked(bool clicked)
+{
+	/* Cleanup warnings related to old path */
+	setDynamicProperty(ui->pathLineEdit, "invalid", false);
+	ui->pathWarning->setText("");
+
+	QString pathToFile = "";
+	auto export_dialog( new QFileDialog( this ) );
+	export_dialog->setWindowModality( Qt::WindowModal );
+	export_dialog->setFileMode( QFileDialog::AnyFile );
+	export_dialog->setAcceptMode( QFileDialog::AcceptOpen );
+	export_dialog->setNameFilters({"(*.html)"});
+	if (export_dialog->exec()){
+		QFile f(export_dialog->selectedFiles().at(0));
+		pathToFile = f.fileName();
+		ui->pathLineEdit->setText(pathToFile);
+
+	}
+}
+
+void UserNotes::save_btn_clicked(bool clicked)
+{
+        if (ui->nameLineEdit->text() == "") {
+                ui->nameLineEdit->setText("Note " +
+                                          QString::number(m_note_count));
+        }
+        auto note = addNote(ui->nameLineEdit->text(),
+                            ui->pathLineEdit->text());
+
+        if (note->getPage()) {
+                setDynamicProperty(ui->pathLineEdit, "invalid", false);
+                ui->pathWarning->setText("");
+                ui->pathLineEdit->clear();
+                ui->nameLineEdit->clear();
+                note->click();
+        } else {
+                setDynamicProperty(ui->pathLineEdit, "invalid", true);
+                ui->pathWarning->setText(" Invalid or empty file!");
+        }
+}
+
+Note* UserNotes::getSelectedNote()
+{
+        for (auto n : m_notes) {
+                if (n->getSelected()) {
+                        return n;
+                }
+        }
+        return nullptr;
+}
+
+int UserNotes::getNoteIndex(Note *note)
+{
+        if (!note)
+                return -1;
+        for (int i = 0; i < m_notes.size(); i++) {
+                if (m_notes.at(i) == note) {
+                        return i;
+                }
+        }
+}
+
+void UserNotes::note_selected(bool selected)
+{
+        if (selected) {
+                ui->removeBtn->setEnabled(true);
+                Note* n = getSelectedNote();
+                if (n) {
+                        loadPageForNote(n, n->getPath());
+                        ui->stackedWidget->slideToIndex(getNoteIndex(getSelectedNote()) + 1);
+                } else {
+                        ui->stackedWidget->slideToIndex(0);
+                }
+        }
+}
+
+void UserNotes::remove_btn_clicked(bool toggled)
+{
+        if (getSelectedNote()) {
+                auto n = getSelectedNote();
+                int pos = getNoteIndex(getSelectedNote());
+                ui->stackedWidget->removeWidget(n->getPage());
+                notes_group->removeButton(n->noteButton());
+                if (pos > 0) {
+                        m_notes.at(pos-1)->click();
+                } else {
+                        ui->addBtn->click();
+                }
+                delete n;
+                m_notes.erase(m_notes.begin() + pos);
+        }
+}
+
+void UserNotes::pageMoved(int direction)
+{
+	if (ui->addBtn->isChecked()) {
+		(direction > 0) ? m_notes.at(0)->click() : ui->addBtn->click();
+	} else {
+		int selectedIdx = getNoteIndex(getSelectedNote()) + direction;
+		(selectedIdx >= 0) ? m_notes.at(selectedIdx)->click() :
+				    ui->addBtn->click();
+	}
+}
+
+Note* UserNotes::addNote(QString name, QString path)
+{
+        Note *newNote = new Note(name, path, this);
+
+        connect(newNote, SIGNAL(selected(bool)),
+                this, SLOT(note_selected(bool)));
+
+        notes_group->addButton(newNote->noteButton());
+        ui->notesList->insertWidget(ui->notesList->count() - 1, newNote);
+        m_notes.push_back(newNote);
+        m_note_count++;
+        loadPageForNote(newNote, path);
+        return newNote;
+}
+
+void UserNotes::loadPageForNote(Note *note, QString path)
+{
+        int pos = getNoteIndex(note);
+        pos++;
+
+        QTextBrowser *index = note->getPage();
+        if (!index) {
+                index = new QTextBrowser(ui->stackedWidget);
+        }
+        index->setFrameShape(QFrame::NoFrame);
+
+	QFileInfo fileInfo(path);
+	if (fileInfo.exists()) {
+		QFile indexFile(path);
+		indexFile.open(QFile::ReadOnly);
+		if (!indexFile.readAll().isEmpty()) {
+			index->setSearchPaths(QStringList(fileInfo.dir().absolutePath()));
+			indexFile.close();
+			index->setSource(QUrl::fromLocalFile(fileInfo.filePath()));
+			index->setOpenExternalLinks(true);
+		} else {
+			indexFile.close();
+			index->clear();
+			index->append("Warning: The file is empty!");
+		}
+	} else {
+		index->clear();
+		index->append("Warning: The path is invalid!");
+	}
+
+        if (index) {
+                note->setPage(index);
+                ui->stackedWidget->insertWidget(pos, note->getPage());
+        }
+}
+
+void UserNotes::clearAllNotes()
+{
+        ui->addBtn->click();
+        for (auto n : m_notes) {
+                delete n;
+        }
+        m_notes.clear();
+}
+
+/*
+ * Note class
+ */
+
+Note::Note(QString name, QString path, QWidget *parent) :
+        QWidget(parent),
+        ui(new Ui::Note),
+        m_selected(false),
+        m_name(name),
+        m_path(path),
+        m_page(nullptr)
+{
+        ui->setupUi(this);
+        ui->name->setText(name);
+        connect(ui->btn, SIGNAL(toggled(bool)),
+                this, SLOT(setSelected(bool)));
+}
+
+Note::~Note()
+{
+	if (m_page) {
+		delete m_page;
+		m_page = nullptr;
+	}
+	delete ui;
+}
+
+QString Note::getName() const
+{
+	return m_name;
+}
+
+void Note::setName(const QString &name)
+{
+	m_name = name;
+	ui->name->setText(name);
+}
+
+QString Note::getPath() const
+{
+	return m_path;
+}
+
+void Note::setPath(const QString &path)
+{
+	m_path = path;
+}
+
+bool Note::getSelected() const
+{
+	return m_selected;
+}
+
+void Note::setSelected(bool sel)
+{
+        m_selected = sel;
+        setDynamicProperty(ui->widget, "selected", sel);
+        Q_EMIT selected(sel);
+}
+
+QTextBrowser *Note::getPage() const
+{
+        return m_page;
+}
+
+void Note::setPage(QTextBrowser *page)
+{
+        m_page = page;
+}
+
+QPushButton* Note::noteButton()
+{
+        return ui->btn;
+}
+
+void Note::click()
+{
+        ui->btn->setChecked(true);
+}
+
+/*
+ * Note_API class
+ */
+void Note_API::setPath(QString path)
+{
+        note->setPath(path);
+}
+
+QString Note_API::getPath()
+{
+        return note->getPath();
+}
+
+void Note_API::setName(QString name)
+{
+        note->setName(name);
+}
+
+QString Note_API::getName()
+{
+        return note->getName();
+}
+
+
+/*
+ * UserNotes_API class
+ */
+
+int UserNotes_API::notes_list_size()
+{
+        refreshApi();
+        return notes_list_api.size();
+}
+
+void UserNotes_API::set_notes_list_size(int size)
+{
+        notesPanel->clearAllNotes();
+        for (int i = 0; i < size; i++){
+                auto n = notesPanel->addNote("", "");
+        }
+}
+
+QVariantList UserNotes_API::getNotes()
+{
+        QVariantList list;
+        refreshApi();
+
+        for (auto *each : notes_list_api) {
+                list.append(QVariant::fromValue(each));
+        }
+        return list;
+}
+
+void UserNotes_API::refreshApi()
+{
+        qDeleteAll(notes_list_api);
+        notes_list_api.clear();
+        for (auto note : notesPanel->m_notes) {
+                notes_list_api.append(new Note_API(note));
+        }
+}
