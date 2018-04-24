a718aae1b3fc631bf6bae1f0d48f8e199b33ed88
Daniel Guramulta <Daniel.Guramulta@Analog.com>
2018-04-24 16:31:34 +0300

    FileManager: Manage the files opened for import/export
    Signed-off-by: Daniel Guramulta <Daniel.Guramulta@Analog.com>

---
+++ src/filemanager.cpp
@@ -0,0 +1,342 @@
+/*
+ * Copyright 2018 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "filemanager.h"
+#include "config.h"
+
+#include <QDebug>
+#include <QFile>
+#include <QDate>
+
+using namespace adiscope;
+
+FileManager::FileManager(QString toolName) :
+        hasHeader(false),
+        sampleRate(0),
+        toolName(toolName),
+        additionalInformation(""),
+        nrOfSamples(0)
+{
+
+}
+
+FileManager::~FileManager()
+{
+
+}
+
+unsigned int FileManager::open(QString fileName,
+                                  FileManager::FilePurpose filepurpose)
+{
+        //read the data from the file if filepurpose is Import
+        //throws exception if the file is corrupted, has a header but not the scopy one
+        //columns with different sizes etc..
+
+        QVector<QVector<QString>> raw_data;
+
+        openedFor = filepurpose;
+
+        if (fileName.endsWith(".csv")) {
+                separator = ",";
+                fileType = CSV;
+        } else if (fileName.endsWith(".txt")) {
+                separator = "\t";
+                fileType = TXT;
+                //find sep to read txt files
+        }
+
+        //clear previous data if the manager was used for other exports
+        data.clear();
+        columnNames.clear();
+        this->filename = fileName;
+
+        if (filepurpose == IMPORT) {
+
+                if (fileName.isEmpty()) {
+                        throw FileManagerException("No file selected");
+                }
+
+                QFile file(fileName);
+                if (!file.open(QIODevice::ReadOnly)) {
+                        throw FileManagerException("Can't open selected file");
+                }
+
+                QTextStream in(&file);
+
+                for (int i = 0; i < data.size(); ++i)
+                        data[i].clear();
+
+                while (!in.atEnd()) {
+                        QVector<QString> line_data;
+                        QString line = in.readLine();
+                        QStringList list = line.split(separator, QString::SkipEmptyParts);
+                        for (QString list_item : list) {
+                                line_data.push_back(list_item);
+                        }
+
+                        raw_data.push_back(line_data);
+                }
+
+                //check if it has a header or not
+                /*
+                *  Header format
+                *
+                *       ;Scopy version <separator> abcdefg
+                *       ;Exported on <separator> Wed Apr 4 13:49:01 2018
+                *       ;Device <separator> M2K
+                *       ;Nr of samples <separator> 1234
+                *       ;Sample rate <separator> 1234 or 0 if it does not have samp. rate
+                *       ;Tool: <separator> Oscilloscope/ Spectrum ...
+                *       ;Additional Information
+                */
+
+                hasHeader = ScopyFileHeader::hasValidHeader(raw_data);
+                try {
+                        if (hasHeader) {
+
+                                format = SCOPY;
+
+                                //first column in data is the time!!! when retrieving channel data start from data[1]
+
+                                sampleRate = std::stod(raw_data[4][1].toStdString());
+                                //should be 0 if read from network/spectrum analyzer exported file
+
+                                for (int j = 1; j < raw_data[7].size(); ++j)
+                                        columnNames.push_back(raw_data[7][j]);
+
+                                data.resize(raw_data.size() - 8);
+                                for (int i = 0; i < data.size(); ++i) {
+                                        data[i].resize(raw_data[i + 8].size() - 1);
+                                }
+
+                                for (int i = 8; i < raw_data.size(); ++i) {
+                                        for (int j = 1; j < raw_data[i].size(); ++j) {
+                                              data[i - 8][j - 1] = std::stod(raw_data[i][j].toStdString());
+                                        }
+                                }
+                        } else {
+
+                                format = RAW;
+
+                                data.resize(raw_data.size());
+                                for (int i = 0; i < data.size(); ++i) {
+                                        data[i].resize(raw_data[i].size());
+                                }
+
+                                for (int i = 0; i < raw_data.size(); ++i) {
+                                        for (int j = 0; j < raw_data[i].size(); ++j) {
+                                              data[i][j] = std::stod(raw_data[i][j].toStdString());
+                                        }
+                                }
+                        }
+                } catch (std::invalid_argument &i) {
+                        // if stod fails to convert a value to double it will throw an invalid argument exception
+                        // in this case it means that the file is corrupted
+                        throw FileManagerException("File is corrupted!");
+                }
+
+                nrOfSamples = data.size();
+        }
+}
+
+void FileManager::save(QVector<double> data, QString name)
+{
+        this->columnNames.push_back(name);
+
+        if (this->data.size() == 0) {
+                this->data.resize(data.size());
+        }
+
+        for (int i = 0; i < data.size(); ++i) {
+                this->data[i].push_back(data[i]);
+        }
+}
+
+void FileManager::save(QVector<QVector<double> > data, QStringList columnNames)
+{
+        for (auto &column : data) {
+                this->data.push_back(column);
+        }
+
+        for (auto &column_name : columnNames) {
+                this->columnNames.push_back(column_name);
+        }
+}
+
+QVector<double> FileManager::read(int index)
+{
+        if (index < 0 || index + 1 >= data.size()) {
+                return QVector<double>();
+        }
+
+        if (hasHeader) {
+                index++;
+        }
+
+        QVector<double> channel_data;
+        for (int i = 0; i < nrOfSamples; ++i) {
+                channel_data.push_back(data[i][index]);
+        }
+
+        return channel_data;
+}
+
+QVector<QVector<double>> FileManager::read()
+{
+        return data;
+}
+
+void FileManager::setColumnName(int index, QString name)
+{
+        if (index < 0 || index >= data.size()) {
+                return;
+        }
+
+        columnNames[index] = name;
+}
+
+QString FileManager::getColumnName(int index)
+{
+        if (index < 0 || index >= columnNames.size()) {
+                return "";
+        }
+
+        if (hasHeader) {
+                index++;
+        }
+
+        return columnNames[index];
+}
+
+double FileManager::getSampleRate() const
+{
+        return sampleRate;
+}
+
+void FileManager::setSampleRate(double sampleRate)
+{
+        this->sampleRate = sampleRate;
+}
+
+double FileManager::getNrOfSamples() const
+{
+        return nrOfSamples;
+}
+
+int FileManager::getNrOfChannels() const
+{
+        if (hasHeader) {
+                return data[0].size() - 1;
+        } else {
+                return data[0].size();
+        }
+}
+
+void FileManager::performWrite()
+{
+        if (openedFor == IMPORT) {
+                qDebug() << "Can't write when opened for import!";
+                return;
+        }
+
+        QFile exportFile(filename);
+        exportFile.open(QIODevice::WriteOnly);
+        QTextStream exportStream(&exportFile);
+
+
+        QStringList header = ScopyFileHeader::getHeader();
+
+        //prepare header
+        exportStream << header[0] << separator << QString(SCOPY_VERSION_GIT) << "\n";
+        exportStream << header[1] << separator << QDate::currentDate().toString("dddd MMMM dd/MM/yyyy") << "\n";
+        exportStream << header[2] << separator << "M2K" << "\n";
+        exportStream << header[3] << separator << data.size() << "\n";
+        exportStream << header[4] << separator << sampleRate << "\n";
+        exportStream << header[5] << separator << toolName << "\n";
+        exportStream << header[6] << separator << additionalInformation << "\n";
+
+        //column names row
+        exportStream << "Sample" << separator;
+        for (QString columnName : columnNames) {
+                exportStream << columnName << separator;
+        }
+        exportStream << "\n";
+
+        for (int i = 0; i < data.size(); ++i) {
+                exportStream << QString::number(i) << separator;
+                for (int j = 0; j < data[i].size(); ++j) {
+                        exportStream << data[i][j] << separator;
+                }
+                exportStream << "\n";
+        }
+
+        exportFile.close();
+}
+
+QString FileManager::getAdditionalInformation() const
+{
+        return additionalInformation;
+}
+
+void FileManager::setAdditionalInformation(const QString &value)
+{
+
+        additionalInformation = value;
+}
+
+FileManager::FileFormat FileManager::getFormat() const
+{
+        return format;
+}
+
+void FileManager::setFormat(const FileManager::FileFormat &value)
+{
+        format = value;
+}
+
+bool ScopyFileHeader::hasValidHeader(QVector<QVector<QString>> data)
+{
+
+        QStringList header_elements = getHeader();
+
+        if (data.size() < header_elements.size()) {
+                return false;
+        }
+
+        for (int i = 0; i < header_elements.size(); ++i) {
+                if (data[i][0] != header_elements[i]) {
+                        return false;
+                }
+        }
+
+        return true;
+}
+
+QStringList ScopyFileHeader::getHeader()
+{
+
+        QStringList header_elements = QStringList() << ";Scopy version"
+                                         << ";Exported on"
+                                         << ";Device"
+                                         << ";Nr of samples"
+                                         << ";Sample rate"
+                                         << ";Tool"
+                                         << ";Additional Information";
+        return header_elements;
+}
---
+++ src/filemanager.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2018 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include <QString>
+#include <QVector>
+#include <QStringList>
+
+#include <exception>
+#include <iostream>
+
+
+namespace adiscope {
+class FileManager
+{
+public:
+
+	enum FilePurpose {
+		EXPORT,
+		IMPORT
+	};
+
+	enum FileFormat {
+		RAW,
+		SCOPY
+	};
+
+	enum FileType {
+		CSV,
+		TXT
+	};
+
+        FileManager(QString toolName);
+        ~FileManager();
+
+        unsigned int open(QString fileName, FileManager::FilePurpose filepurpose = EXPORT);
+
+        void save(QVector<double> data, QString name);
+        void save(QVector<QVector<double>> data, QStringList column_names);
+
+        QVector<double> read(int index);
+        QVector<QVector<double>> read();
+
+        void setColumnName(int index, QString name);
+        QString getColumnName(int index);
+
+        double getSampleRate() const;
+        void setSampleRate(double sampleRate);
+
+        double getNrOfSamples() const;
+        int getNrOfChannels() const;
+
+        void performWrite();
+
+        QString getAdditionalInformation() const;
+        void setAdditionalInformation(const QString& value);
+
+        FileFormat getFormat() const;
+        void setFormat(const FileFormat &value);
+
+private:
+
+        QVector<QVector<double>> data;
+        QStringList columnNames;
+        QString filename;
+        bool hasHeader;
+        double sampleRate;
+        double nrOfSamples;
+        FilePurpose openedFor;
+        FileFormat format;
+        FileType fileType;
+        QString separator;
+        QString toolName;
+        QString additionalInformation;
+
+};
+
+class ScopyFileHeader {
+public:
+        static bool hasValidHeader(QVector<QVector<QString>> data);
+        static QStringList getHeader();
+};
+
+class FileManagerException : public std::runtime_error {
+public:
+        FileManagerException(const char* msg) : std::runtime_error(msg) {}
+        ~FileManagerException() throw() {}
+};
+}
+
+#endif // FILEMANAGER_H
