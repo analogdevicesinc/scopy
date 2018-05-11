d69b647e0e9bc4889119c5ed9860791ace82012b
AntoniuMiclaus <antoniu.miclaus@analog.com>
2018-05-11 14:10:41 +0300

    Unary Decoder: edit header + add copyright
    Signed-off-by: AntoniuMiclaus <antoniu.miclaus@analog.com>

--- resources/decoders/unary/__init__.py
+++ resources/decoders/unary/__init__.py
@@ -2,6 +2,7 @@
 ## This file is part of the libsigrokdecode project.
 ##
 ## Copyright (C) 2013 Uwe Hermann <uwe@hermann-uwe.de>
+## Copyright (C) 2018 Analog Devices, Inc.
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -20,14 +21,10 @@
 
 '''
 This protocol decoder can decode synchronous unary code buses with various
-number of data bits/channels and one (optional) clock line.
-
-If no clock line is supplied, the decoder works slightly differently in
-that it interprets every transition on any of the supplied data channels
-like there had been a clock transition.
+number of data bits/channels.
 
 It is required to use the lowest data channels, and use consecutive ones.
-For example, for a 4-bit sync unary code bus, channels D0/D1/D2/D3 (and CLK)
+For example, for a 4-bit sync unary code bus, channels D0/D1/D2/D3
 should be used. Using combinations like D7/D12/D3/D15 is not supported.
 For an 8-bit bus you should use D0-D7, for a 16-bit bus use D0-D15 and so on.
 '''
--- resources/decoders/unary/pd.py
+++ resources/decoders/unary/pd.py
@@ -1,6 +1,7 @@
 #### This file is part of the libsigrokdecode project.
 ##
 ## Copyright (C) 2013 Uwe Hermann <uwe@hermann-uwe.de>
+## Copyright (C) 2018 Analog Devices, Inc.
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
