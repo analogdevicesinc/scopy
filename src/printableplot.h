556cd4a5ddd495d4260970743b63d87d642818c5
Daniel Guramulta <Daniel.Guramulta@Analog.com>
2018-05-21 14:06:20 +0300

    PrintablePlot: plot that can be printed using the printPlot method
    Signed-off-by: Daniel Guramulta <Daniel.Guramulta@Analog.com>

---
+++ src/printableplot.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef PRINTABLEPLOT_H
+#define PRINTABLEPLOT_H
+
+/*Qwt includes*/
+#include <qwt_plot.h>
+#include <qwt_plot_renderer.h>
+#include <qwt_legend.h>
+
+/*Qt includes*/
+#include <QWidget>
+
+namespace adiscope {
+class PrintablePlot : public QwtPlot
+{
+        Q_OBJECT
+public:
+        PrintablePlot(QWidget *parent);
+
+        void dropBackground(bool drop);
+
+public Q_SLOTS:
+        void printPlot();
+
+private:
+        QwtPlotRenderer d_plotRenderer;
+        QwtLegend *legendDisplay;
+};
+}
+
+#endif // PRINTABLEPLOT_H
