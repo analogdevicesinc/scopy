47a38ea184fd01d0b094240ce226f61824152541
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-06-27 15:07:10 +0300

    CI/travis/before_install_{linux,darwin}.sh: wrap build rules into functions
    Makes things a bit neater & reduces some duplication.
    
    The aim [long-term] is to replace these with dependencies from the system.
    But for now, these are needed to be built.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/before_install_darwin.sh
+++ CI/travis/before_install_darwin.sh
@@ -7,7 +7,6 @@ brew install qt
 echo 'export PATH="$(brew --prefix qt)/bin:$PATH"' >> ~/.bash_profile
 brew link --force qt
 
-brew unlink bison
 brew install cmake fftw bison autoconf automake libtool libzip glib glibmm doxygen wget boost gnu-sed libmatio dylibbundler libxml2 pkg-config
 brew install --build-from-source libusb
 brew install llvm
@@ -18,13 +17,7 @@ brew upgrade cmake
 brew upgrade bison
 brew upgrade fftw
 
-
-# Required for Python
-export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
-export PATH=$PATH:$(brew --prefix)/share/python
-mkdir -p deps
-cd deps
-WORKDIR=$PWD
+source ./CI/travis/before_install_lib.sh
 
 # Get pip
 curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
@@ -35,138 +28,20 @@ sudo pip install --ignore-installed six
 brew pip mako
 brew pip cheetah
 
-# Build dependencies
-cd ${WORKDIR}
-rm -rf volk-1.3
-if [ ! -d volk-1.3 ]; then
-  wget http://libvolk.org/releases/volk-1.3.tar.gz
-  tar -xzf volk-1.3.tar.gz
-  cd volk-1.3
-  mkdir build && cd build
-  cmake ..
-  make >/dev/null
-else
-  cd volk-1.3/build
-fi
-sudo make install
+make_build_git "libsigrok" "https://github.com/sigrokproject/libsigrok" "" "" "./autogen.sh"
 
-cd ${WORKDIR}
-rm -rf gnuradio
-if [ ! -d gnuradio ]; then
-  git clone https://github.com/analogdevicesinc/gnuradio -b signal_source_phase
-  cd gnuradio
-  mkdir build
-  cd build
-  cmake -DENABLE_INTERNAL_VOLK:BOOL=OFF -DENABLE_GR_FEC:BOOL=OFF -DENABLE_GR_DIGITAL:BOOL=OFF -DENABLE_GR_DTV:BOOL=OFF -DENABLE_GR_ATSC:BOOL=OFF -DENABLE_GR_AUDIO:BOOL=OFF -DENABLE_GR_CHANNELS:BOOL=OFF -DENABLE_GR_NOAA:BOOL=OFF -DENABLE_GR_PAGER:BOOL=OFF -DENABLE_GR_TRELLIS:BOOL=OFF -DENABLE_GR_VOCODER:BOOL=OFF ..
-  make -j4
-else
-  cd gnuradio
-  git pull
-  cd build
-fi
-sudo make install >/dev/null
+make_build_wget "libsigrokdecode-0.4.1" "http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.4.1.tar.gz"
 
-cd ${WORKDIR}
-if [ ! -d qwt ]; then
-  git clone https://github.com/osakared/qwt.git -b qwt-6.1-multiaxes
-  cd qwt
-  curl https://raw.githubusercontent.com/analogdevicesinc/scopy/master/qwt-6.1-multiaxes.patch |patch -p1 --forward
-  qmake
-  make -j4 >/dev/null
-else
-  cd qwt
-  make -j4 >/dev/null
-fi
-sudo make install
+qmake_build_git "qwt" "https://github.com/osakared/qwt.git" "qwt-6.1-multiaxes" "qwt.pro" "patch_qwt"
 
-cd ${WORKDIR}
-rm qwtpolar-1.1.1.tar.bz2*
-if [ ! -d qwtpolar-1.1.1 ]; then
-  wget --no-check-certificate https://downloads.sourceforge.net/project/qwtpolar/qwtpolar/1.1.1/qwtpolar-1.1.1.tar.bz2
-  tar xvjf qwtpolar-1.1.1.tar.bz2
-  cd qwtpolar-1.1.1
-  curl -o qwtpolar-qwt-6.1-compat.patch https://raw.githubusercontent.com/analogdevicesinc/scopy-flatpak/master/qwtpolar-qwt-6.1-compat.patch
-  patch -p1 < qwtpolar-qwt-6.1-compat.patch
+qmake_build_wget "qwtpolar-1.1.1" "https://downloads.sourceforge.net/project/qwtpolar/qwtpolar/1.1.1/qwtpolar-1.1.1.tar.bz2" "qwtpolar.pro" "patch_qwtpolar"
 
-  # Disable components that we won't build
-  gsed -i "/^QWT_POLAR_CONFIG\\s*+=\\s*QwtPolarExamples$/s/^/#/g" qwtpolarconfig.pri
-  gsed -i "/^QWT_POLAR_CONFIG\\s*+=\\s*QwtPolarDesigner$/s/^/#/g" qwtpolarconfig.pri
-  gsed -i "/QWT_POLAR_CONFIG\\s*+=\\s*QwtPolarFramework$/s/^/#/g" qwtpolarconfig.pri
-  # Fix prefix
-  gsed -i "s/^\\s*QWT_POLAR_INSTALL_PREFIX.*$/QWT_POLAR_INSTALL_PREFIX=\/usr\/local/g" qwtpolarconfig.pri
-  gsed -i "/^QWT_POLAR_INSTALL_HEADERS/s/$/\/qwt/g" qwtpolarconfig.pri
-  cat qwtpolarconfig.pri
-  qmake LIBS+="-L/usr/local/lib -lqwt" INCLUDEPATH+="/usr/local/include/qwt" qwtpolar.pro
-  make >/dev/null
-else
-  cd qwtpolar-1.1.1
-fi
-sudo make install
+cmake_build_wget "volk-1.3" "http://libvolk.org/releases/volk-1.3.tar.gz"
 
-cd ${WORKDIR}
-rm master_latest_libiio${LDIST}.pkg*
-wget http://swdownloads.analog.com/cse/travis_builds/master_latest_libiio${LDIST}.pkg
-sudo installer -pkg master_latest_libiio${LDIST}.pkg -target /
+cmake_build_git "gnuradio" "https://github.com/analogdevicesinc/gnuradio" "signal_source_phase" "-DENABLE_INTERNAL_VOLK:BOOL=OFF -DENABLE_GR_FEC:BOOL=OFF -DENABLE_GR_DIGITAL:BOOL=OFF -DENABLE_GR_DTV:BOOL=OFF -DENABLE_GR_ATSC:BOOL=OFF -DENABLE_GR_AUDIO:BOOL=OFF -DENABLE_GR_CHANNELS:BOOL=OFF -DENABLE_GR_NOAA:BOOL=OFF -DENABLE_GR_PAGER:BOOL=OFF -DENABLE_GR_TRELLIS:BOOL=OFF -DENABLE_GR_VOCODER:BOOL=OFF"
 
-cd ${WORKDIR}
-rm -rf libad9361-iio
-if [ ! -d libad9361-iio ]; then
-  git clone https://github.com/analogdevicesinc/libad9361-iio
-  cd libad9361-iio
-  mkdir build
-  cd build
-  cmake ..
-  make
-else
-  cd libad9361-iio
-  git pull
-  cd build
-fi
-sudo make install
+cmake_build_git "libiio" "https://github.com/analogdevicesinc/libiio" "" "-DINSTALL_UDEV_RULE:BOOL=OFF"
 
-cd ${WORKDIR}
-rm -rf gr-iio
-if [ ! -d gr-iio ]; then
-  git clone https://github.com/analogdevicesinc/gr-iio
-  cd gr-iio
-  mkdir build
-  cd build
-  cmake ..
-  make -j4
-else
-  cd gr-iio
-  git pull
-  cd build
-  make -j4
-fi
-sudo make install
+cmake_build_git "libad9361-iio" "https://github.com/analogdevicesinc/libad9361-iio"
 
-brew upgrade python
-
-cd ${WORKDIR}
-rm -rf libsigrok
-if [ ! -d libsigrok ]; then
-  git clone https://github.com/sigrokproject/libsigrok/
-  cd libsigrok
-  ./autogen.sh
-  CC=gcc CXX=g++ CXXFLAGS=-std=c++11 ./configure --disable-static --enable-shared --disable-all-drivers --enable-bindings --enable-cxx
-  make
-else
-  cd libsigrok
-  make >/dev/null
-fi
-sudo make install
-
-cd ${WORKDIR}
-rm -rf libsigrokdecode-0.4.1
-if [ ! -d libsigrokdecode-0.4.1 ]; then
-  wget http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.4.1.tar.gz
-  tar -xzvf libsigrokdecode-0.4.1.tar.gz
-  cd libsigrokdecode-0.4.1
-  ./configure
-  make >/dev/null
-else
-  cd libsigrokdecode-0.4.1
-fi
-sudo make install
-
+cmake_build_git "gr-iio" "https://github.com/analogdevicesinc/gr-iio"
---
+++ CI/travis/before_install_lib.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+set -e
+
+NUM_JOBS=4
+WORKDIR="${PWD}/deps"
+mkdir -p "$WORKDIR"
+STAGINGDIR="${WORKDIR}/staging"
+
+__cmake() {
+	local args="$1"
+	mkdir -p build
+	pushd build # build
+
+	cmake -DCMAKE_PREFIX_PATH="$STAGINGDIR" -DCMAKE_INSTALL_PREFIX="$STAGINGDIR" \
+		-DCMAKE_EXE_LINKER_FLAGS="-L${STAGINGDIR}/lib" \
+		$args .. $SILENCED
+	CFLAGS=-I${STAGINGDIR}/include LDFLAGS=-L${STAGINGDIR}/lib make -j${NUM_JOBS} $SILENCED
+
+	make install
+	popd
+}
+
+__make() {
+	$preconfigure
+	$configure --prefix="$STAGINGDIR" $SILENCED
+	CFLAGS=-I${STAGINGDIR}/include LDFLAGS=-L${STAGINGDIR}/lib $make -j${NUM_JOBS} $SILENCED
+	$make install
+}
+
+__qmake() {
+	$patchfunc
+	qmake "$qtarget" $SILENCED
+	CFLAGS=-I${STAGINGDIR}/include LDFLAGS=-L${STAGINGDIR}/lib \
+		make -j${NUM_JOBS} $SILENCED
+	make install
+}
+
+__build_common() {
+	local dir="$1"
+	local buildfunc="$2"
+	local getfunc="$3"
+	local subdir="$4"
+	local args="$5"
+
+	pushd "$WORKDIR" # deps dir
+
+	# if we have this folder, we may not need to download it
+	[ -d "$dir" ] || $getfunc
+
+	pushd "$dir" # this dep dir
+	[ -z "$subdir" ] || pushd "$subdir" # in case there is a build subdir or smth
+
+	$buildfunc "$args"
+
+	popd
+	popd
+	[ -z "$subdir" ] || popd
+}
+
+wget_and_untar() {
+	[ -d "$WORKDIR/$dir" ] || {
+		local tar_file="${dir}.tar.gz"
+		wget "$url" -O "$tar_file"
+		tar -xvf "$tar_file" > /dev/null
+	}
+}
+
+git_clone_update() {
+	if [ -d "$WORKDIR/$dir" ] ; then
+		pushd "$WORKDIR/$dir"
+		git pull
+		popd
+	else
+		[ -z "$branch" ] || branch="-b $branch"
+		git clone $branch "$url" "$dir"
+	fi
+}
+
+cmake_build_wget() {
+	local dir="$1"
+	local url="$2"
+
+	__build_common "$dir" "__cmake" "wget_and_untar"
+}
+
+cmake_build_git() {
+	local dir="$1"
+	local url="$2"
+	local branch="$3"
+	local args="$4"
+
+	__build_common "$dir" "__cmake" "git_clone_update" "" "$args"
+}
+
+make_build_wget() {
+	local dir="$1"
+	local url="$2"
+	local configure="${3:-./configure}"
+	local make="${4:-make}"
+
+	__build_common "$dir" "__make" "wget_and_untar"
+}
+
+make_build_git() {
+	local dir="$1"
+	local url="$2"
+	local configure="${3:-./configure}"
+	local make="${4:-make}"
+	local preconfigure="$5"
+
+	__build_common "$dir" "__make" "git_clone_update"
+}
+
+qmake_build_wget() {
+	local dir="$1"
+	local url="$2"
+	local qtarget="$3"
+	local patchfunc="$4"
+
+	__build_common "$dir" "__qmake" "wget_and_untar"
+}
+
+qmake_build_git() {
+	local dir="$1"
+	local url="$2"
+	local branch="$3"
+	local qtarget="$4"
+	local patchfunc="$5"
+
+	__build_common "$dir" "__qmake" "git_clone_update"
+}
+
+patch_qwt() {
+	[ -f qwt_patched ] || {
+	patch -p1 <<-EOF
+--- a/qwtconfig.pri
++++ b/qwtconfig.pri
+@@ -19,7 +19,7 @@ QWT_VERSION      = \$\${QWT_VER_MAJ}.\$\${QWT_VER_MIN}.\$\${QWT_VER_PAT}
+ QWT_INSTALL_PREFIX = \$\$[QT_INSTALL_PREFIX]
+ 
+ unix {
+-    QWT_INSTALL_PREFIX    = /usr/local/qwt-\$\$QWT_VERSION-svn
++    QWT_INSTALL_PREFIX    = $STAGINGDIR
+     # QWT_INSTALL_PREFIX = /usr/local/qwt-\$\$QWT_VERSION-svn-qt-\$\$QT_VERSION
+ }
+ 
+@@ -161,7 +161,7 @@ QWT_CONFIG     += QwtPlayground
+ 
+ macx:!static:CONFIG(qt_framework, qt_framework|qt_no_framework) {
+ 
+-    QWT_CONFIG += QwtFramework
++    #QWT_CONFIG += QwtFramework
+ }  
+ 
+ ######################################################################
+--- a/src/src.pro
++++ b/src/src.pro
+@@ -30,7 +30,8 @@ contains(QWT_CONFIG, QwtDll) {
+     
+         # we increase the SONAME for every minor number
+ 
+-        QWT_SONAME=libqwt.so.\$\${VER_MAJ}.\$\${VER_MIN}
++        !macx: QWT_SONAME=libqwt.so.\$\${VER_MAJ}.\$\${VER_MIN}
++        macx: QWT_SONAME=\$\${QWT_INSTALL_LIBS}/libqwt.dylib
+         QMAKE_LFLAGS *= \$\${QMAKE_LFLAGS_SONAME}\$\${QWT_SONAME}
+         QMAKE_LFLAGS_SONAME=
+     }
+--- a/textengines/mathml/mathml.pro
++++ b/textengines/mathml/mathml.pro
+@@ -57,7 +57,8 @@ contains(QWT_CONFIG, QwtDll) {
+ 
+         # we increase the SONAME for every minor number
+ 
+-        QWT_SONAME=libqwtmathml.so.\$\${VER_MAJ}.\$\${VER_MIN}
++        !macx: QWT_SONAME=libqwtmathml.so.\$\${VER_MAJ}.\$\${VER_MIN}
++        macx: QWT_SONAME=\$\${QWT_INSTALL_LIBS}/libqwtmathml.dylib
+         QMAKE_LFLAGS *= \$\${QMAKE_LFLAGS_SONAME}\$\${QWT_SONAME}
+         QMAKE_LFLAGS_SONAME=
+     }   
+EOF
+		touch qwt_patched
+	}
+	return 0
+}
+
+patch_qwtpolar() {
+	[ -f qwtpolar-qwt-6.1-compat.patch ] || {
+		wget https://raw.githubusercontent.com/analogdevicesinc/scopy-flatpak/master/qwtpolar-qwt-6.1-compat.patch
+		patch -p1 < qwtpolar-qwt-6.1-compat.patch 
+
+		patch -p1 <<-EOF
+--- a/qwtpolarconfig.pri
++++ b/qwtpolarconfig.pri
+@@ -16,7 +16,9 @@ QWT_POLAR_VER_PAT      = 1
+ QWT_POLAR_VERSION      = \$\${QWT_POLAR_VER_MAJ}.\$\${QWT_POLAR_VER_MIN}.\$\${QWT_POLAR_VER_PAT}
+ 
+ unix {
+-    QWT_POLAR_INSTALL_PREFIX    = /usr/local/qwtpolar-\$\$QWT_POLAR_VERSION
++    QWT_POLAR_INSTALL_PREFIX    = $STAGINGDIR
++    QMAKE_CXXFLAGS              = -I${STAGINGDIR}/include
++    QMAKE_LFLAGS                = -L${STAGINGDIR}/lib
+ }
+ 
+ win32 {
+@@ -70,14 +72,14 @@ QWT_POLAR_INSTALL_FEATURES  = \$\${QWT_POLAR_INSTALL_PREFIX}/features
+ # Otherwise you have to build it from the designer directory.
+ ######################################################################
+ 
+-QWT_POLAR_CONFIG     += QwtPolarDesigner
++#QWT_POLAR_CONFIG     += QwtPolarDesigner
+ 
+ ######################################################################
+ # If you want to auto build the examples, enable the line below
+ # Otherwise you have to build them from the examples directory.
+ ######################################################################
+ 
+-QWT_POLAR_CONFIG     += QwtPolarExamples
++#QWT_POLAR_CONFIG     += QwtPolarExamples
+ 
+ ######################################################################
+ # When Qt has been built as framework qmake wants 
+@@ -86,6 +88,6 @@ QWT_POLAR_CONFIG     += QwtPolarExamples
+ 
+ macx:CONFIG(qt_framework, qt_framework|qt_no_framework) {
+ 
+-    QWT_POLAR_CONFIG += QwtPolarFramework
++    #QWT_POLAR_CONFIG += QwtPolarFramework
+ }
+ 
+EOF
+	}
+}
