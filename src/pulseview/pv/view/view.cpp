b51a123ebaa37b8fb21fe909c3a8e8b95ea2a490
Daniel Guramulta <Daniel.Guramulta@analog.com>
2018-09-21 17:21:23 +0300

    src/pulseview/pv/view/view.cpp: fix uninitialised member

--- src/pulseview/pv/view/view.cpp
+++ src/pulseview/pv/view/view.cpp
@@ -120,7 +120,8 @@ View::View(Session &session, QWidget *parent) :
 	next_flag_text_('A'),
 	trigger_markers_(),
 	hover_point_(-1, -1),
-	start_plot_offset_(0)
+	start_plot_offset_(0),
+	scroll_needs_defaults(true)
 {
 	setFrameShape(QFrame::NoFrame);
 
