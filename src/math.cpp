6cfd9fac1bc0e9a184bab9f0ef84035443673646
Adrian Suciu <adrian.suciu@analog.com>
2018-11-07 18:28:02 +0200

    gr-scopy: added gr-scopy OOT
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- src/math.cpp
+++ src/math.cpp
@@ -24,7 +24,7 @@
 #include <QLocale>
 #include <QMenu>
 
-#include <gnuradio/iio/math.h>
+#include <gnuradio/scopy/math.h>
 
 using namespace adiscope;
 
@@ -169,7 +169,7 @@ void Math::validateFunction()
 	QString function = ui.function->text();
 
 	try {
-		gr::iio::iio_math::make(function.toStdString(), num_inputs);
+		gr::scopy::iio_math::make(function.toStdString(), num_inputs);
 
 		Q_EMIT functionValid(function);
 
