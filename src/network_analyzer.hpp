bb28135b6a26d9e4d0bb0d1833096666f9fb5786
Daniel Guramulta <daniel.guramulta@analog.com>
2018-11-16 13:16:19 +0200

    Updated Network Analyzer
    Reworked the run algorithm, it should show less spikes now
    Added Run / Single commands
    
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

--- src/network_analyzer.hpp
+++ src/network_analyzer.hpp
@@ -30,6 +30,12 @@
 #include <QtConcurrentRun>
 #include "customPushButton.hpp"
 #include "scroll_filter.hpp"
+#include <gnuradio/top_block.h>
+#include <gnuradio/blocks/head.h>
+#include <gnuradio/blocks/vector_sink_s.h>
+#include <gnuradio/analog/sig_source_f.h>
+#include <gnuradio/blocks/float_to_short.h>
+
 
 
 extern "C" {
@@ -67,46 +73,78 @@ namespace adiscope {
 
 	private:
 		Ui::NetworkAnalyzer *ui;
-		struct iio_channel *dac1, *dac2, *amp1, *amp2;
+		struct iio_channel *amp1, *amp2;
+		std::vector<iio_channel *> dac_channels;
 		struct iio_device *adc;
 		std::shared_ptr<GenericAdc> adc_dev;
 		boost::shared_ptr<iio_manager> iio;
-        dBgraph m_dBgraph;
+
+		// Sine generation blocks
+		gr::top_block_sptr top_block;
+		gr::analog::sig_source_f::sptr source_block;
+		gr::blocks::float_to_short::sptr f2s_block;
+		gr::blocks::head::sptr head_block;
+		gr::blocks::vector_sink_s::sptr vector_block;
+
+		QVector<unsigned long> sampleRates;
+		size_t fixedRate;
+
+
+
+	dBgraph m_dBgraph;
 	dBgraph m_phaseGraph;
 	bool wasChecked;
 
-        PlotLineHandleH *d_hCursorHandle1;
-        PlotLineHandleH *d_hCursorHandle2;
-        bool d_cursorsEnabled;
+	typedef struct NetworkAnalyzerIteration {
+		NetworkAnalyzerIteration():
+			frequency(0),
+			rate(0),
+			bufferSize(0) {}
+		NetworkAnalyzerIteration(double frequency,
+					 size_t rate,
+					 size_t bufferSize):
+			frequency(frequency),
+			rate(rate),
+			bufferSize(bufferSize) {}
 
-        ScaleSpinButton *samplesCount;
-        ScaleSpinButton *minFreq;
-        ScaleSpinButton *maxFreq;
-        ScaleSpinButton *amplitude;
-        PositionSpinButton *offset;
-        PositionSpinButton *magMax;
-        PositionSpinButton *magMin;
-        PositionSpinButton *phaseMax;
-        PositionSpinButton *phaseMin;
+		double frequency;
+		size_t rate;
+		size_t bufferSize;
+		size_t adcRate;
+		size_t adcBuffer;
+	} networkIteration;
+
+	QVector<networkIteration> iterations;
+
+
+	PlotLineHandleH *d_hCursorHandle1;
+	PlotLineHandleH *d_hCursorHandle2;
+	bool d_cursorsEnabled;
+
+	ScaleSpinButton *samplesCount;
+	ScaleSpinButton *minFreq;
+	ScaleSpinButton *maxFreq;
+	ScaleSpinButton *amplitude;
+	PositionSpinButton *offset;
+	PositionSpinButton *magMax;
+	PositionSpinButton *magMin;
+	PositionSpinButton *phaseMax;
+	PositionSpinButton *phaseMin;
+
 	void setMinimumDistanceBetween(SpinBoxA *min, SpinBoxA *max, double distance);
 
-        HorizHandlesArea *d_bottomHandlesArea;
+	HorizHandlesArea *d_bottomHandlesArea;
 
-        QQueue<QPair<CustomPushButton *, bool>> menuButtonActions;
+	QQueue<QPair<CustomPushButton *, bool>> menuButtonActions;
 
-        MouseWheelWidgetGuard *wheelEventGuard;
+	MouseWheelWidgetGuard *wheelEventGuard;
 
 		QFuture<void> thd;
 		bool stop;
 
 		void run();
 
-		static size_t get_sin_samples_count(
-				const struct iio_device *dev,
-				unsigned long rate,
-				double frequency);
-
-		static struct iio_buffer * generateSinWave(
+		struct iio_buffer * generateSinWave(
 				const struct iio_device *dev,
 				double frequency,
 				double amplitude,
@@ -114,29 +152,31 @@ namespace adiscope {
 				unsigned long rate,
 				size_t samples_count);
 
-		static unsigned long get_best_sample_rate(
-				const struct iio_device *dev,
-				double frequency);
 		void configHwForNetworkAnalyzing();
 
 		void triggerRightMenuToggle(CustomPushButton *btn, bool checked);
 		void toggleRightMenu(CustomPushButton *btn, bool checked);
 
+		void computeFrequencyArray();
+                void updateGainMode();
+		void computeCaptureParams(double frequency, size_t &buffer_size, size_t &adc_rate);
+
 	private Q_SLOTS:
 		void startStop(bool start);
-		void updateNumSamples();
+		void updateNumSamples(bool force = false);
+		void plot(double frequency, double mag, double mag2, double phase);
 
-        void toggleCursors(bool en);
-        void onVbar1PixelPosChanged(int pos);
-        void onVbar2PixelPosChanged(int pos);
-        void readPreferences();
-        void onGraphIndexChanged(int);
-        void on_btnExport_clicked();
-        void rightMenuFinished(bool opened);
+	void toggleCursors(bool en);
+	void onVbar1PixelPosChanged(int pos);
+	void onVbar2PixelPosChanged(int pos);
+	void readPreferences();
+	void onGraphIndexChanged(int);
+	void on_btnExport_clicked();
+	void rightMenuFinished(bool opened);
 
-        public Q_SLOTS:
+	public Q_SLOTS:
 
-        void showEvent(QShowEvent *event);
+	void showEvent(QShowEvent *event);
 
 	Q_SIGNALS:
 		void sweepDone();
