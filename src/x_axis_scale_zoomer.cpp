3412ec5399d703b4292ee08c335412e7efbcef2b
Daniel Guramulta <daniel.guramulta@analog.com>
2018-11-28 12:30:08 +0200

    src/x_axis_scale_zoomer.*: override trackerText method
    This method will be responsible for displaying the tracker text on the plot. In this way we can have different values on the scale draws and still be able to display absolute value on the tracker text
    
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

--- src/x_axis_scale_zoomer.cpp
+++ src/x_axis_scale_zoomer.cpp
@@ -1,4 +1,7 @@
 #include "x_axis_scale_zoomer.h"
+#include "qwt_scale_draw.h"
+
+#include <dbgraph.hpp>
 
 using namespace adiscope;
 
@@ -20,5 +23,15 @@ void XAxisScaleZoomer::zoom(const QRectF& rect)
 	boundedRect.setBottom(baseRect.bottom());
 
 	QwtPlotZoomer::zoom(boundedRect);
+}
 
+QwtText XAxisScaleZoomer::trackerText(const QPoint &p) const
+{
+	QwtText t;
+	QPointF dp = QwtPlotZoomer::invTransform(p);
+
+	const dBgraph *plt = dynamic_cast<const dBgraph *>(plot());
+	t.setText(plt->getScaleValueFormat(dp.x(), QwtPlot::xTop, 4) + ", " +
+		  plt->getScaleValueFormat(dp.y(), QwtPlot::yLeft));
+	return t;
 }
