4d7660afb3e5e5a27c666108582b924647ae2e59
Daniel Guramulta <daniel.guramulta@analog.com>
2019-01-29 12:37:14 +0200

    src/toolmenu/item.*: tool menu and tool menu item
    Special menu that offers items based on available tools for a connected device
    
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

---
+++ src/toolmenu.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2019 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef TOOLMENU_H
+#define TOOLMENU_H
+
+#include "basemenu.h"
+#include "filter.hpp"
+#include "toolmenuitem.h"
+#include "preferences.h"
+
+#include <QString>
+#include <QLabel>
+#include <QButtonGroup>
+
+namespace adiscope {
+class ToolMenu : public BaseMenu
+{
+	Q_OBJECT
+public:
+	explicit ToolMenu(Preferences *preferences, QWidget *parent = nullptr);
+	~ToolMenu();
+
+	void loadToolsFromFilter(Filter *filter);
+	ToolMenuItem *getToolMenuItemFor(enum tool tool);
+
+	QButtonGroup *getButtonGroup();
+
+Q_SIGNALS:
+	void toolSelected(tool);
+	void enableInfoWidget(bool);
+
+private Q_SLOTS:
+	void _updateToolList(short from, short to);
+
+private:
+	void _buildAllAvailableTools();
+	void _saveState();
+	void _loadState();
+	void _readPreferences();
+private:
+
+	static const QStringList d_availableTools;
+	static const QStringList d_availableIcons;
+
+	QVector<QPair<BaseMenuItem *, tool>> d_tools;
+	QVector<int> d_positions;
+	QVector<BaseMenuItem *> d_compatibleTools;
+	QString d_current_hw_name;
+	QButtonGroup *d_buttonGroup;
+	Preferences *d_preferences;
+};
+}
+
+#endif // TOOLMENU_H
