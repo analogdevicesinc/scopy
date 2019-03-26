64aaa4811756df1953c69ad57db001c1ed4566d9
Daniel Guramulta <daniel.guramulta@analog.com>
2019-03-26 17:19:57 +0200

    SmoothCurveFitter: add a curve fitter to smooth out the waveform when used
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

---
+++ src/smoothcurvefitter.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "smoothcurvefitter.h"
+
+#include <qwt_spline_cubic.h>
+#include <qwt_spline_parametrization.h>
+
+using namespace adiscope;
+
+SmoothCurveFitter::SmoothCurveFitter():
+        QwtCurveFitter(QwtCurveFitter::Path),
+	d_spline(new QwtSplineCubic())
+{
+	d_spline->setParametrization(QwtSplineParametrization::ParameterUniform);
+}
+
+QPolygonF SmoothCurveFitter::fitCurve(const QPolygonF &points) const
+{
+	return d_spline->polygon(points, 0.5);
+}
+
+QPainterPath SmoothCurveFitter::fitCurvePath(const QPolygonF &points) const
+{
+	return d_spline->painterPath(points);
+}
---
+++ src/smoothcurvefitter.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file LICENSE.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef SMOOTHCURVEFITTER_H
+#define SMOOTHCURVEFITTER_H
+
+#include <memory>
+
+#include <qwt_curve_fitter.h>
+#include <qwt_spline.h>
+
+namespace adiscope {
+class SmoothCurveFitter : public QwtCurveFitter
+{
+public:
+	SmoothCurveFitter();
+
+	virtual QPolygonF fitCurve( const QPolygonF &points ) const;
+	virtual QPainterPath fitCurvePath( const QPolygonF &points ) const;
+
+private:
+        std::unique_ptr<QwtSpline> d_spline;
+};
+}
+
+#endif // SMOOTHCURVEFITTER_H
