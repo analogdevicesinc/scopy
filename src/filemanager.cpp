6aebb054b1f8f41eb510301747879d4f3e2ee9da
Alexandra.Trifan <Alexandra.Trifan@analog.com>
2019-09-24 15:10:26 +0300

    src/filemanager.cpp/hpp: Fix indentation in order to match the entire app.
    No functional changes.
    Scopy source code uses tabs for indentation, these 2 files used spaces.
    
    Signed-off-by: Alexandra.Trifan <Alexandra.Trifan@analog.com>

--- src/filemanager.cpp
+++ src/filemanager.cpp
@@ -27,8 +27,8 @@
 using namespace adiscope;
 
 FileManager::FileManager(QString toolName) :
-        hasHeader(false),
-        sampleRate(0),
+	hasHeader(false),
+	sampleRate(0),
 	nrOfSamples(0),
 	toolName(toolName)
 {
@@ -41,72 +41,72 @@ FileManager::~FileManager()
 }
 
 void FileManager::open(QString fileName,
-                                  FileManager::FilePurpose filepurpose)
+		       FileManager::FilePurpose filepurpose)
 {
-        //read the data from the file if filepurpose is Import
-        //throws exception if the file is corrupted, has a header but not the scopy one
-        //columns with different sizes etc..
+	//read the data from the file if filepurpose is Import
+	//throws exception if the file is corrupted, has a header but not the scopy one
+	//columns with different sizes etc..
 
-        QVector<QVector<QString>> raw_data;
+	QVector<QVector<QString>> raw_data;
 	bool srOk = true;
 	bool dataOk = true;
 
-        openedFor = filepurpose;
+	openedFor = filepurpose;
 
-        if (fileName.endsWith(".csv")) {
-                separator = ",";
-                fileType = CSV;
-        } else if (fileName.endsWith(".txt")) {
-                separator = "\t";
-                fileType = TXT;
-                //find sep to read txt files
-        }
+	if (fileName.endsWith(".csv")) {
+		separator = ",";
+		fileType = CSV;
+	} else if (fileName.endsWith(".txt")) {
+		separator = "\t";
+		fileType = TXT;
+		//find sep to read txt files
+	}
 
-        //clear previous data if the manager was used for other exports
-        data.clear();
-        columnNames.clear();
-        this->filename = fileName;
+	//clear previous data if the manager was used for other exports
+	data.clear();
+	columnNames.clear();
+	this->filename = fileName;
 
-        if (filepurpose == IMPORT) {
+	if (filepurpose == IMPORT) {
 
-                if (fileName.isEmpty()) {
-                        throw FileManagerException("No file selected");
-                }
+		if (fileName.isEmpty()) {
+			throw FileManagerException("No file selected");
+		}
 
-                QFile file(fileName);
-                if (!file.open(QIODevice::ReadOnly)) {
-                        throw FileManagerException("Can't open selected file");
-                }
+		QFile file(fileName);
+		if (!file.open(QIODevice::ReadOnly)) {
+			throw FileManagerException("Can't open selected file");
+		}
 
-                QTextStream in(&file);
+		QTextStream in(&file);
 
-                for (int i = 0; i < data.size(); ++i)
-                        data[i].clear();
+		for (int i = 0; i < data.size(); ++i)
+			data[i].clear();
 
-                while (!in.atEnd()) {
-                        QVector<QString> line_data;
-                        QString line = in.readLine();
-                        QStringList list = line.split(separator, QString::SkipEmptyParts);
-                        for (QString list_item : list) {
-                                line_data.push_back(list_item);
-                        }
+		while (!in.atEnd()) {
+			QVector<QString> line_data;
+			QString line = in.readLine();
+			QStringList list = line.split(separator, QString::SkipEmptyParts);
+			for (QString list_item : list) {
+				line_data.push_back(list_item);
+			}
 			if (line_data.size() > 0) {
 				raw_data.push_back(line_data);
 			}
-                }
+		}
 
-                //check if it has a header or not
-                /*
-                *  Header format
-                *
-                *       ;Scopy version <separator> abcdefg
-                *       ;Exported on <separator> Wed Apr 4 13:49:01 2018
-                *       ;Device <separator> M2K
-                *       ;Nr of samples <separator> 1234
-                *       ;Sample rate <separator> 1234 or 0 if it does not have samp. rate
-                *       ;Tool: <separator> Oscilloscope/ Spectrum ...
-                *       ;Additional Information
-                */
+		//check if it has a header or not
+		/*
+		*  Header format
+		*
+		*       ;Scopy version <separator> abcdefg
+		*       ;Exported on <separator> Wed Apr 4 13:49:01 2018
+		*       ;Device <separator> M2K
+		*       ;Nr of samples <separator> 1234
+		*       ;Sample rate <separator> 1234 or 0 if it does not have samp. rate
+		*       ;Tool: <separator> Oscilloscope/ Spectrum ...
+		*       ;Additional Information
+		*/
 
 		hasHeader = ScopyFileHeader::hasValidHeader(raw_data);
 		if (hasHeader) {
@@ -165,86 +165,86 @@ void FileManager::open(QString fileName,
 
 void FileManager::save(QVector<double> data, QString name)
 {
-        this->columnNames.push_back(name);
+	this->columnNames.push_back(name);
 
-        if (this->data.size() == 0) {
-                this->data.resize(data.size());
-        }
+	if (this->data.size() == 0) {
+		this->data.resize(data.size());
+	}
 
-        for (int i = 0; i < data.size(); ++i) {
-                this->data[i].push_back(data[i]);
-        }
+	for (int i = 0; i < data.size(); ++i) {
+		this->data[i].push_back(data[i]);
+	}
 }
 
 void FileManager::save(QVector<QVector<double> > data, QStringList columnNames)
 {
-        for (auto &column : data) {
-                this->data.push_back(column);
-        }
+	for (auto &column : data) {
+		this->data.push_back(column);
+	}
 
-        for (auto &column_name : columnNames) {
-                this->columnNames.push_back(column_name);
-        }
+	for (auto &column_name : columnNames) {
+		this->columnNames.push_back(column_name);
+	}
 }
 
 QVector<double> FileManager::read(int index)
 {
-        if (index < 0 || index + 1 >= data.size()) {
-                return QVector<double>();
-        }
+	if (index < 0 || index + 1 >= data.size()) {
+		return QVector<double>();
+	}
 
-        if (hasHeader) {
-                index++;
-        }
+	if (hasHeader) {
+		index++;
+	}
 
-        QVector<double> channel_data;
-        for (int i = 0; i < nrOfSamples; ++i) {
-                channel_data.push_back(data[i][index]);
-        }
+	QVector<double> channel_data;
+	for (int i = 0; i < nrOfSamples; ++i) {
+		channel_data.push_back(data[i][index]);
+	}
 
-        return channel_data;
+	return channel_data;
 }
 
 QVector<QVector<double>> FileManager::read()
 {
-        return data;
+	return data;
 }
 
 void FileManager::setColumnName(int index, QString name)
 {
-        if (index < 0 || index >= data.size()) {
-                return;
-        }
+	if (index < 0 || index >= data.size()) {
+		return;
+	}
 
-        columnNames[index] = name;
+	columnNames[index] = name;
 }
 
 QString FileManager::getColumnName(int index)
 {
-        if (index < 0 || index >= columnNames.size()) {
-                return "";
-        }
+	if (index < 0 || index >= columnNames.size()) {
+		return "";
+	}
 
-        if (hasHeader) {
-                index++;
-        }
+	if (hasHeader) {
+		index++;
+	}
 
-        return columnNames[index];
+	return columnNames[index];
 }
 
 double FileManager::getSampleRate() const
 {
-        return sampleRate;
+	return sampleRate;
 }
 
 void FileManager::setSampleRate(double sampleRate)
 {
-        this->sampleRate = sampleRate;
+	this->sampleRate = sampleRate;
 }
 
 double FileManager::getNrOfSamples() const
 {
-        return nrOfSamples;
+	return nrOfSamples;
 }
 
 int FileManager::getNrOfChannels() const
@@ -253,67 +253,67 @@ int FileManager::getNrOfChannels() const
 		return 0;
 	}
 
-        if (hasHeader) {
-                return data[0].size() - 1;
-        } else {
-                return data[0].size();
-        }
+	if (hasHeader) {
+		return data[0].size() - 1;
+	} else {
+		return data[0].size();
+	}
 }
 
 void FileManager::performWrite()
 {
 	QString additionalInfo = "";
-        if (openedFor == IMPORT) {
-                qDebug() << "Can't write when opened for import!";
-                return;
-        }
+	if (openedFor == IMPORT) {
+		qDebug() << "Can't write when opened for import!";
+		return;
+	}
 
-        QFile exportFile(filename);
-        exportFile.open(QIODevice::WriteOnly);
-        QTextStream exportStream(&exportFile);
+	QFile exportFile(filename);
+	exportFile.open(QIODevice::WriteOnly);
+	QTextStream exportStream(&exportFile);
 
 	additionalInfo = (additionalInformation.size() != 0) ? additionalInformation[0] : "";
 
-        QStringList header = ScopyFileHeader::getHeader();
+	QStringList header = ScopyFileHeader::getHeader();
 
-        //prepare header
-        exportStream << header[0] << separator << QString(SCOPY_VERSION_GIT) << "\n";
-        exportStream << header[1] << separator << QDate::currentDate().toString("dddd MMMM dd/MM/yyyy") << "\n";
-        exportStream << header[2] << separator << "M2K" << "\n";
-        exportStream << header[3] << separator << data.size() << "\n";
-        exportStream << header[4] << separator << sampleRate << "\n";
-        exportStream << header[5] << separator << toolName << "\n";
+	//prepare header
+	exportStream << header[0] << separator << QString(SCOPY_VERSION_GIT) << "\n";
+	exportStream << header[1] << separator << QDate::currentDate().toString("dddd MMMM dd/MM/yyyy") << "\n";
+	exportStream << header[2] << separator << "M2K" << "\n";
+	exportStream << header[3] << separator << data.size() << "\n";
+	exportStream << header[4] << separator << sampleRate << "\n";
+	exportStream << header[5] << separator << toolName << "\n";
 	exportStream << header[6] << separator << additionalInfo << "\n";
 
-        //column names row
-        exportStream << "Sample" << separator;
+	//column names row
+	exportStream << "Sample" << separator;
 	bool skipFirstSeparator=true;
-        for (QString columnName : columnNames) {
+	for (QString columnName : columnNames) {
 		if(!skipFirstSeparator)
 			exportStream << separator;
 		exportStream << columnName;
 		skipFirstSeparator = false;
-        }
-        exportStream << "\n";
+	}
+	exportStream << "\n";
 
-        for (int i = 0; i < data.size(); ++i) {
+	for (int i = 0; i < data.size(); ++i) {
 		skipFirstSeparator = true;
-                exportStream << QString::number(i) << separator;
-                for (int j = 0; j < data[i].size(); ++j) {
+		exportStream << QString::number(i) << separator;
+		for (int j = 0; j < data[i].size(); ++j) {
 			if(!skipFirstSeparator)
 				exportStream << separator;
 			exportStream << data[i][j];
 			skipFirstSeparator = false;
-                }
-                exportStream << "\n";
-        }
+		}
+		exportStream << "\n";
+	}
 
-        exportFile.close();
+	exportFile.close();
 }
 
 QStringList FileManager::getAdditionalInformation() const
 {
-        return additionalInformation;
+	return additionalInformation;
 }
 
 void FileManager::setAdditionalInformation(const QString &value)
@@ -324,41 +324,41 @@ void FileManager::setAdditionalInformation(const QString &value)
 
 FileManager::FileFormat FileManager::getFormat() const
 {
-        return format;
+	return format;
 }
 
 void FileManager::setFormat(const FileManager::FileFormat &value)
 {
-        format = value;
+	format = value;
 }
 
 bool ScopyFileHeader::hasValidHeader(QVector<QVector<QString>> data)
 {
 
-        QStringList header_elements = getHeader();
+	QStringList header_elements = getHeader();
 
-        if (data.size() < header_elements.size()) {
-                return false;
-        }
+	if (data.size() < header_elements.size()) {
+		return false;
+	}
 
-        for (int i = 0; i < header_elements.size(); ++i) {
-                if (data[i][0] != header_elements[i]) {
-                        return false;
-                }
-        }
+	for (int i = 0; i < header_elements.size(); ++i) {
+		if (data[i][0] != header_elements[i]) {
+			return false;
+		}
+	}
 
-        return true;
+	return true;
 }
 
 QStringList ScopyFileHeader::getHeader()
 {
 
-        QStringList header_elements = QStringList() << ";Scopy version"
-                                         << ";Exported on"
-                                         << ";Device"
-                                         << ";Nr of samples"
-                                         << ";Sample rate"
-                                         << ";Tool"
-                                         << ";Additional Information";
-        return header_elements;
+	QStringList header_elements = QStringList() << ";Scopy version"
+						    << ";Exported on"
+						    << ";Device"
+						    << ";Nr of samples"
+						    << ";Sample rate"
+						    << ";Tool"
+						    << ";Additional Information";
+	return header_elements;
 }
