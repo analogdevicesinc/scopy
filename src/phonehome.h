3479bace03415f092262f472fe85f1fb461c55e5
Cristi Iacob <cristian.iacob@analog.com>
2019-11-08 13:56:58 +0200

    Added the phonehome mechanism.
    PhoneHome class makes a request to https://analog-applications-versions.herokuapp.com/all
    and takes the latest versions of scopy and m2k-firmware.
    
    The server in deployed on heroku and it checks every day for newer scopy and m2k-fw versions.
    PhoneHome takes data from here and uses it inside scopy, notifying the user in case a newer version is available.
    
    Signed-off-by: Cristi Iacob <cristian.iacob@analog.com>

---
+++ src/phonehome.h
@@ -0,0 +1,39 @@
+#ifndef PHONEHOME_H
+#define PHONEHOME_H
+#include "src/pulseview/pv/devices/device.hpp"
+#include "preferences.h"
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QTextCodec>
+
+namespace adiscope {
+class PhoneHome: public QObject
+{
+	Q_OBJECT
+private:
+	QString m_lastRequestContent;
+	QString m_scopyVersion;
+	QString m_m2kVersion;
+	QString m_scopyLink;
+	QString m_m2kLink;
+	PhoneHome* instance;
+	Preferences* preferences;
+
+public:
+	PhoneHome();
+	void versionsRequest();
+	QString getScopyVersion() { return m_scopyVersion; }
+	QString getM2kVersion() { return m_m2kVersion; }
+	QString getScopyLink() { return m_scopyLink; }
+	QString getM2kLink() { return m_m2kLink; }
+	void setPreferences(Preferences* preferences);
+
+Q_SIGNALS:
+	void scopyVersionChanged();
+	void m2kVersionChanged();
+
+public Q_SLOTS:
+	void onVersionsRequestFinished(QNetworkReply*);
+};
+}
+#endif // PHONEHOME_H
