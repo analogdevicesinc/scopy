9eeb16f84ebfaa0862e08d9ea39b4aa1c5479ea4
Adrian Suciu <adrian.suciu@analog.com>
2019-11-28 14:55:03 +0200

    General: Updated license headers
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- src/debugger.h
+++ src/debugger.h
@@ -1,10 +1,13 @@
 /*
- * Copyright 2017 Analog Devices, Inc.
+ * Copyright (c) 2019 Analog Devices Inc.
  *
- * This program is free software; you can redistribute it and/or modify
+ * This file is part of Scopy
+ * (see http://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,9 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GNU Radio; see the file LICENSE.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef DEBUGGER_H
