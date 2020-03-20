ba21503b0d2834a07ca86135e677ae416111d082
Adrian Suciu <adrian.suciu@analog.com>
2020-03-20 17:01:28 +0200

    appveyor.yml: removed comments and diagnostic messages
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- build_appveyor_mingw.sh
+++ build_appveyor_mingw.sh
@@ -50,16 +50,6 @@ PACMAN_REPO_DEPS="
 
 DLL_DEPS="libmatio-*.dll libhdf5-*.dll libszip*.dll libpcre*.dll libdouble-conversion*.dll libwinpthread-*.dll libgcc_*.dll libstdc++-*.dll libboost_*.dll libglib-*.dll libintl-*.dll libiconv-*.dll libglibmm-2.*.dll libgmodule-2.*.dll libgobject-2.*.dll libffi-*.dll libsigc-2.*.dll libfftw3f-*.dll libicu*.dll zlib*.dll libharfbuzz-*.dll libfreetype-*.dll libbz2-*.dll libpng16-*.dll libgraphite2.dll libjpeg-*.dll libsqlite3-*.dll libwebp-*.dll libxml2-*.dll liblzma-*.dll libxslt-*.dll libzip*.dll libpython3.*.dll libgnutls*.dll libnettle*.dll libhogweed*.dll libgmp*.dll libidn*.dll libp11*.dll libtasn*.dll libunistring*.dll libusb-*.dll libzstd*.dll libgnuradio-*.dll /$MINGW_VERSION/lib/python3.* libiio*.dll libvolk*.dll liblog4cpp*.dll libad9361*.dll liborc*.dll libsigrok*.dll qwt*.dll"
 
-#do we need this ?
-#http://swdownloads.analog.com/cse/build/windres.exe.gz;\
-
-#if [ ${ARCH} == "i686" ]
-#then
-#	RC_COMPILER_OPT="-DCMAKE_RC_COMPILER=/c/windres.exe"
-#else
-#	RC_COMPILER_OPT=""
-#fi
-
 OLD_PATH=$PATH
 DEST_FOLDER=scopy_$ARCH_BIT
 BUILD_FOLDER=build_$ARCH_BIT
