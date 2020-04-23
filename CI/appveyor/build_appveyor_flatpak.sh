31b66256f516a56f3a4073b88d004a547c509407
AlexandraTrifan <Alexandra.Trifan@analog.com>
2020-04-23 17:15:41 +0300

    CI/appveyor/flatpak: Modify the source of the flatpak Docker to tag latest.
    The "latest" is an updated version which has the latest versions for all
    the dependencies.
    
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- CI/appveyor/build_appveyor_flatpak.sh
+++ CI/appveyor/build_appveyor_flatpak.sh
@@ -7,7 +7,7 @@ REPO=${2:-analogdevicesinc/scopy}
 
 sudo apt-get -qq update
 sudo service docker restart
-sudo docker pull alexandratr/scopy-flatpak-bionic:scopy
+sudo docker pull alexandratr/scopy-flatpak-bionic:latest
 
 # Start the docker in detached state
 commit_nb=$(sudo docker run -d \
@@ -16,7 +16,7 @@ commit_nb=$(sudo docker run -d \
 		-v `pwd`:/scopy:rw \
 		-e "BRANCH=$BRANCH" \
 		-e "REPO=$REPO" \
-		alexandratr/scopy-flatpak-bionic:scopy \
+		alexandratr/scopy-flatpak-bionic:latest \
 		/bin/bash -xe /scopy/CI/appveyor/inside_flatpak_docker.sh)
 
 # Attach ourselves to the running docker and wait for it to finish
