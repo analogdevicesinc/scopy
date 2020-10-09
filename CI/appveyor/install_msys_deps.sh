08d925cb1d6320f1d798f7eb4bd0d9e2b69a4779
Alexandra Trifan <Alexandra.Trifan@analog.com>
2020-10-09 17:02:01 +0300

    appveyor: Use the old deps archive provided by scopy-mingw-build-deps instead of trying to get packages from repo.msys2.org.
    Some older packages might be removed from the official repo. Using this
    change, we don't depend on that official repo to build our application.
    
    Also, remove the fast_finish flag, we want to use the MacOS builds whether
    the Mingw build fail or not.
    
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

--- CI/appveyor/install_msys_deps.sh
+++ CI/appveyor/install_msys_deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash.exe
 
 SCOPY_MINGW_BUILD_DEPS_FORK=analogdevicesinc
-SCOPY_MINGW_BUILD_DEPS_BRANCH=master
+SCOPY_MINGW_BUILD_DEPS_BRANCH=test-mirrors #TODO: change before merging to master;
 
 echo "Download and install pre-compiled libraries ... "
 wget "https://ci.appveyor.com/api/projects/$SCOPY_MINGW_BUILD_DEPS_FORK/scopy-mingw-build-deps/artifacts/scopy-$MINGW_VERSION-build-deps-pacman.txt?branch=$SCOPY_MINGW_BUILD_DEPS_BRANCH&job=Environment: MINGW_VERSION=$MINGW_VERSION, ARCH=$ARCH" -O /tmp/scopy-$MINGW_VERSION-build-deps-pacman.txt
@@ -16,10 +16,10 @@ PACMAN_SYNC_DEPS="
 "
 
 PACMAN_REPO_DEPS="
-	http://repo.msys2.org/mingw/$ARCH/mingw-w64-$ARCH-breakpad-git-r1680.70914b2d-1-any.pkg.tar.xz \
-	http://repo.msys2.org/mingw/$ARCH/mingw-w64-$ARCH-libusb-1.0.21-2-any.pkg.tar.xz \
-	http://repo.msys2.org/mingw/$ARCH/mingw-w64-$ARCH-boost-1.72.0-3-any.pkg.tar.zst \
-	http://repo.msys2.org/mingw/$ARCH/mingw-w64-$ARCH-qt5-5.14.2-3-any.pkg.tar.zst \
+	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-breakpad-git-r1680.70914b2d-1-any.pkg.tar.xz \
+	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-libusb-1.0.21-2-any.pkg.tar.xz \
+	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-boost-1.72.0-3-any.pkg.tar.zst \
+	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-qt5-5.14.2-3-any.pkg.tar.zst \
 "
 
 PATH=/c/msys64/$MINGW_VERSION/bin:$PATH
