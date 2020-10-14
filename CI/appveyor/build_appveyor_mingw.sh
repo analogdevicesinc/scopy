33ca72f95a46a12eef8426e4a1abc93a85663718
Alexandra Trifan <Alexandra.Trifan@analog.com>
2020-10-14 18:33:35 +0300

    WindowsInstaller: Use "Tasks" to create checkbox that deletes Scopy.ini.
    The checkbox for a task is checked by default, so we don't need to specify
    another flag.
    Also, the Appveyor job no longer needs to export an empty Scopy.ini.
    
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

--- CI/appveyor/build_appveyor_mingw.sh
+++ CI/appveyor/build_appveyor_mingw.sh
@@ -80,8 +80,6 @@ mv /c/$DEST_FOLDER/Scopy.exe.sym /c/$DEBUG_FOLDER
 mv /c/$DEST_FOLDER/.debug /c/$DEBUG_FOLDER
 
 echo "### Creating archives ... "
-# Create empty ini file for overwrite in setup
-touch /c/$DEST_FOLDER/Scopy.ini
 7z a "/c/scopy-${ARCH_BIT}bit.zip" /c/$DEST_FOLDER
 # appveyor PushArtifact /c/scopy-${ARCH_BIT}bit.zip
 7z a "/c/debug-${ARCH_BIT}bit.zip" /c/$DEBUG_FOLDER
