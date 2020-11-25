641f9efeb55358484d81ed0ed30a853789978128
DanielGuramulta <Daniel.Guramulta@analog.com>
2020-11-25 16:06:06 +0200

    General: when handling exceptions don't introduce undefined behaviour
    Some exceptions would be handled in a thread. Using this macro that creates a QMessageBox would cause undefined behaviour due to the fact that we are creating GUI elements from another thread. If we can identify that this thread does not correspond to the main thread ie the qapplication's thread we invoke the message box. Due to the fact that "msg.exec()" blocks the main thread we also need to block the current thread, to further prevent more undefined behaviour from being introduced, thus the usage of Qt::BlockingQueuedConnection. If we are on the main thread we can simply create the message box like before
    
    Signed-off-by: DanielGuramulta <Daniel.Guramulta@analog.com>

--- src/scopyExceptionHandler.h
+++ src/scopyExceptionHandler.h
@@ -6,25 +6,33 @@
 #include <config.h>
 #include <libm2k/m2kexceptions.hpp>
 #include <scopyApplication.hpp>
+#include <QThread>
 
 using namespace adiscope;
 
 #define HANDLE_EXCEPTION(e) \
-	if(GetScopyApplicationInstance()->getDebugMode()) { \
+if (GetScopyApplicationInstance()->getDebugMode()) { \
+	auto me = dynamic_cast<libm2k::m2k_exception *>(&e); \
+	auto handleLambda = [me, e](){ \
 		QMessageBox msg; \
 		QString str; \
-		auto me = dynamic_cast<libm2k::m2k_exception*>(&e);\
-		if(me) { \
+		if (me) { \
 			str = QString("Exception %1\ne.type() - %2\ne.what()  - %3\ne.iioCode() - %4\nthrown from %5:%6\ncaught in %7:%8\nScopy git tag %9\n") \
 			.arg("m2k_exception").arg(me->type()).arg(me->what()).arg(me->iioCode()).arg(QString::fromStdString(me->file())).arg(QString::number(me->line())).arg(__FILE__).arg(__LINE__).arg(SCOPY_VERSION_GIT); \
 		} else { \
 			str = QString("Exception  %1\ncaught in %2:%3\nScopy git tag %4\n").arg(e.what()).arg(__FILE__).arg(__LINE__).arg(SCOPY_VERSION_GIT); \
 		} \
-		if(WriteScopyMinidump()) { \
+		if (WriteScopyMinidump()) { \
 			str = str + "Created minidump."; \
-		}\
-		msg.setText(str);\
-		msg.exec();\
+		} \
+		msg.setText(str); \
+		msg.exec(); \
+	}; \
+	if (GetScopyApplicationInstance()->thread() != QThread::currentThread()) { \
+		QMetaObject::invokeMethod(GetScopyApplicationInstance(), handleLambda, Qt::BlockingQueuedConnection); \
+	} else { \
+		handleLambda(); \
 	} \
+} \
 
 #endif // SCOPYEXCEPTIONHANDLER_H
