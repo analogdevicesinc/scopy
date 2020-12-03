4984641cc8c57c0260480b1b5dd6eb67d228738f
Adrian Suciu <adrian.suciu@analog.com>
2020-12-03 18:01:13 +0200

    Phonehome: Added caching/timeout mechanism to the downloaded file
    Prevent downloading on every app run, but download every day or so
    
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- src/phonehome.cpp
+++ src/phonehome.cpp
@@ -1,14 +1,21 @@
 #include "phonehome.h"
-
-#include <QJsonDocument>
-#include <QJsonObject>
+#include <QDateTime>
+#include <QTime>
 
 using namespace adiscope;
 
 PHONEHOME_H
 
-PhoneHome::PhoneHome()
+PhoneHome::PhoneHome(QSettings *settings, Preferences *pref) :
+	ApiObject(), m_timestamp(0), m_versionsJson(""),
+	preferences(pref), settings(settings)
 {
+	setObjectName("phonehome");
+	load(*settings);
+}
+
+PhoneHome::~PhoneHome() {
+	save(*settings);
 }
 
 void PhoneHome::setPreferences(Preferences* preferences)
@@ -18,25 +25,71 @@ void PhoneHome::setPreferences(Preferences* preferences)
 
 void PhoneHome::versionsRequest()
 {
-	QNetworkAccessManager* manager = new QNetworkAccessManager();
-	connect(manager, &QNetworkAccessManager::finished, this, &PhoneHome::onVersionsRequestFinished);
-	connect(manager, &QNetworkAccessManager::finished, manager, &QNetworkAccessManager::deleteLater);
+	qint64 timeout = 24 * 60 * 60 * 1000; // 24 hours
+	//qint64 timeout = 5 * 1000;  // 5 seconds
+	qint64 now = QDateTime::currentMSecsSinceEpoch();
 
-	manager->get(QNetworkRequest(QUrl(preferences->getCheck_updates_url())));
+	if(m_timestamp +  timeout < now )
+	{
+		// from swdownloads
+		QNetworkAccessManager* manager = new QNetworkAccessManager();
+		connect(manager, &QNetworkAccessManager::finished, this, &PhoneHome::onVersionsRequestFinished);
+		connect(manager, &QNetworkAccessManager::finished, manager, &QNetworkAccessManager::deleteLater);
+
+		manager->get(QNetworkRequest(QUrl(preferences->getCheck_updates_url())));
+	}
+	else
+	{
+		// from cache
+		if(!m_versionsJson.isEmpty())
+		{
+			QJsonDocument doc = QJsonDocument::fromJson(m_versionsJson.toUtf8());
+			extractVersionStringsFromJson(doc);
+		}
+	}
+}
+
+void PhoneHome::extractVersionStringsFromJson(QJsonDocument doc)
+{
+	auto content = doc.object().toVariantMap();
+	m_scopyVersion = content["scopy"].toMap()["version"].toString();
+	m_m2kVersion = content["m2k-fw"].toMap()["version"].toString();
+	m_scopyLink = content["scopy"].toMap()["link"].toString();
+	m_m2kLink = content["m2k-fw"].toMap()["link"].toString();
+	Q_EMIT scopyVersionChanged();
+	Q_EMIT m2kVersionChanged();
 }
 
 void PhoneHome::onVersionsRequestFinished(QNetworkReply* reply)
 {
-	if (reply->error() == QNetworkReply::NoError) {
-		auto content = QJsonDocument::fromJson(reply->readAll()).object().toVariantMap();
-		m_scopyVersion = content["scopy"].toMap()["version"].toString();
-		m_m2kVersion = content["m2k-fw"].toMap()["version"].toString();
-		m_scopyLink = content["scopy"].toMap()["link"].toString();
-		m_m2kLink = content["m2k-fw"].toMap()["link"].toString();
-		Q_EMIT scopyVersionChanged();
-		Q_EMIT m2kVersionChanged();
+	auto err = reply->error();
+	if (err == QNetworkReply::NoError) {
+		auto data = reply->readAll().trimmed();
+		auto doc = QJsonDocument::fromJson(data);
+		extractVersionStringsFromJson(doc);
+		m_versionsJson = QString(data.trimmed());
+		m_timestamp = QDateTime::currentMSecsSinceEpoch();
+
 	} else {
 		qDebug() << "Wasn't able to access versions database!\n";
 	}
+}
+
+QString PhoneHome::getTimestamp() const
+{
+	return QString::number(m_timestamp);
+}
+void PhoneHome::setTimestamp(QString val)
+{
+	m_timestamp = val.toLongLong();
+}
+QString PhoneHome::getVersionsJson() const
+{
+	return m_versionsJson;
+}
+void PhoneHome::setVersionsJson(QString val)
+{
+	m_versionsJson = val;
+
 }
 
--- src/phonehome.h
+++ src/phonehome.h
@@ -4,9 +4,11 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QTextCodec>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 namespace adiscope {
-class PhoneHome: public QObject
+class PhoneHome : public ApiObject
 {
 	Q_OBJECT
 private:
@@ -15,17 +17,30 @@ private:
 	QString m_m2kVersion;
 	QString m_scopyLink;
 	QString m_m2kLink;
+	qint64 m_timestamp;
+	QString m_versionsJson;
 	PhoneHome* instance;
 	Preferences* preferences;
+	QSettings *settings;
+
+
+	Q_PROPERTY(QString timestamp READ getTimestamp WRITE setTimestamp)
+	Q_PROPERTY(QString versionsJson READ getVersionsJson WRITE setVersionsJson)
 
 public:
-	PhoneHome();
+	PhoneHome(QSettings *settings, Preferences *preferences);
+	~PhoneHome();
 	void versionsRequest();
+	void extractVersionStringsFromJson(QJsonDocument doc);
 	QString getScopyVersion() { return m_scopyVersion; }
 	QString getM2kVersion() { return m_m2kVersion; }
 	QString getScopyLink() { return m_scopyLink; }
 	QString getM2kLink() { return m_m2kLink; }
 	void setPreferences(Preferences* preferences);
+	QString getTimestamp() const;
+	void setTimestamp(QString);
+	QString getVersionsJson() const;
+	void setVersionsJson(QString);
 
 Q_SIGNALS:
 	void scopyVersionChanged();
