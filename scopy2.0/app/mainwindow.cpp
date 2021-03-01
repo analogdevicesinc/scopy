6235849e375c7bad8118319701e20edc1e943c8a
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-03-01 17:47:42 +0200

    Custom Widgets: Added RunSingleWidget
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

--- scopy2.0/app/mainwindow.cpp
+++ scopy2.0/app/mainwindow.cpp
@@ -1,14 +1,14 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <scopy/gui/twobuttonswidget.h>
-//#include <twobuttonswidget.h>
+#include <scopy/gui/two_buttons_widget.hpp>
+#include <scopy/gui/run_single_widget.hpp>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
 
-  ui->centralLayout->addWidget(new scopy::gui::TwoButtonsWidget("hello"));
+  ui->centralLayout->addWidget(new scopy::gui::RunSingleWidget);
 }
 
 MainWindow::~MainWindow() { delete ui; }
