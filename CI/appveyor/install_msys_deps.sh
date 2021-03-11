263523b18c82dd1c7a85a9fc9868299a1f860edb
Adrian Suciu <adrian.suciu@analog.com>
2021-03-11 18:32:21 +0200

    ci: windows: install libusb v1.0.21
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- CI/appveyor/install_msys_deps.sh
+++ CI/appveyor/install_msys_deps.sh
@@ -18,10 +18,10 @@ PACMAN_SYNC_DEPS="
 	mingw-w64-$ARCH-matio \
 "
 
-#	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-libusb-1.0.21-2-any.pkg.tar.xz \
 #	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-boost-1.72.0-3-any.pkg.tar.zst \
 #	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-qt5-5.14.2-3-any.pkg.tar.zst \
 PACMAN_REPO_DEPS="
+	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-libusb-1.0.21-2-any.pkg.tar.xz \
 	/c/old_msys_deps_$MINGW_VERSION/mingw-w64-$ARCH-breakpad-git-r1680.70914b2d-1-any.pkg.tar.xz \
 "
 
