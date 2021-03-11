9bd48409f6e51cc1bc2ccd35ab56a5b692927bbb
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-03-11 18:07:06 +0200

    Custom Widgets: Added CustomPlotPositionButton
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -2,18 +2,19 @@
 
 #include "./ui_mainwindow.h"
 
+#include <scopy/gui/channel_widget.hpp>
+#include <scopy/gui/connect_dialog.hpp>
+#include <scopy/gui/custom_menu_button.hpp>
+#include <scopy/gui/custom_plot_position_button.hpp>
+#include <scopy/gui/custom_switch.hpp>
+#include <scopy/gui/homepage_controls.hpp>
+#include <scopy/gui/linked_button.hpp>
 #include <scopy/gui/run_single_widget.hpp>
 #include <scopy/gui/settings_pair_widget.hpp>
+#include <scopy/gui/small_on_off_switch.hpp>
+#include <scopy/gui/spinbox_a.hpp>
 #include <scopy/gui/subsection_separator.hpp>
 #include <scopy/gui/two_buttons_widget.hpp>
-#include <scopy/gui/spinbox_a.hpp>
-#include <scopy/gui/custom_menu_button.hpp>
-#include <scopy/gui/homepage_controls.hpp>
-#include <scopy/gui/connect_dialog.hpp>
-#include <scopy/gui/linked_button.hpp>
-#include <scopy/gui/small_on_off_switch.hpp>
-#include <scopy/gui/channel_widget.hpp>
-#include <scopy/gui/custom_switch.hpp>
 
 MainWindow::MainWindow(QWidget* parent)
 	: QMainWindow(parent)
@@ -28,8 +29,9 @@ MainWindow::MainWindow(QWidget* parent)
 
 	ui->vLayout->addWidget(new scopy::gui::SettingsPairWidget);
 
-	scopy::gui::ScaleSpinButton* scaleSpinBox = new scopy::gui::ScaleSpinButton({{"sps", 1E0}, {"ksps", 1E+3}, {"Msps", 1E+6}},
-							    tr("Sample Rate"), 1, 10e7, true, false, ui->centralwidget, {1, 2, 5});
+	scopy::gui::ScaleSpinButton* scaleSpinBox =
+		new scopy::gui::ScaleSpinButton({{"sps", 1E0}, {"ksps", 1E+3}, {"Msps", 1E+6}}, tr("Sample Rate"), 1,
+						10e7, true, false, ui->centralwidget, {1, 2, 5});
 
 	ui->vLayout->addWidget(scaleSpinBox);
 
@@ -54,6 +56,7 @@ MainWindow::MainWindow(QWidget* parent)
 
 	ui->vLayout1->addWidget(new scopy::gui::CustomSwitch);
 
+	ui->vLayout1->addWidget(new scopy::gui::CustomPlotPositionButton);
 }
 
 MainWindow::~MainWindow() { delete ui; }
