5abd4dd9b30ede53455703a94efc5d2475916fd0
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-03-17 12:34:47 +0200

    Custom Widgets: Added InstrumentNotes
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ gui/src/instrument_notes.cpp
@@ -0,0 +1,21 @@
+#include "ui_instrument_notes.h"
+
+#include <scopy/gui/instrument_notes.hpp>
+
+using namespace scopy::gui;
+
+InstrumentNotes::InstrumentNotes(QWidget* parent)
+	: QWidget(parent)
+	, m_ui(new Ui::InstrumentNotes)
+{
+	m_ui->setupUi(this);
+
+	m_ui->widgetSubsectionSeparator->setLabel("Note");
+	m_ui->widgetSubsectionSeparator->setButton(false);
+}
+
+InstrumentNotes::~InstrumentNotes() { delete m_ui; }
+
+QString InstrumentNotes::getNotes() { return m_ui->textEdit->toPlainText(); }
+
+void InstrumentNotes::setNotes(QString str) { m_ui->textEdit->setPlainText(str); }
--- gui/src/subsection_separator.cpp
+++ gui/src/subsection_separator.cpp
@@ -6,18 +6,29 @@
 
 using namespace scopy::gui;
 
+SubsectionSeparator::SubsectionSeparator(QWidget* parent)
+	: QWidget(parent)
+	, m_ui(new Ui::SubsectionSeparator)
+{
+	m_ui->setupUi(this);
+}
+
 SubsectionSeparator::SubsectionSeparator(const QString& text, const bool buttonVisible, QWidget* parent)
 	: QWidget(parent)
 	, m_ui(new Ui::SubsectionSeparator)
 {
 	m_ui->setupUi(this);
 
-	m_ui->lblSubsectionSeparator->setText(text);
-	m_ui->btnSubsectionSeparator->setVisible(buttonVisible);
+	setLabel(text);
+	setButton(buttonVisible);
 }
 
 SubsectionSeparator::~SubsectionSeparator() { delete m_ui; }
 
 QPushButton* SubsectionSeparator::getButton() { return m_ui->btnSubsectionSeparator; }
 
+void SubsectionSeparator::setButton(bool buttonVisible) { m_ui->btnSubsectionSeparator->setVisible(buttonVisible); }
+
 QLabel* SubsectionSeparator::getLabel() { return m_ui->lblSubsectionSeparator; }
+
+void SubsectionSeparator::setLabel(const QString& text) { m_ui->lblSubsectionSeparator->setText(text); }
