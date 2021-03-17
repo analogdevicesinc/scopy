4a4d15949fb37caeeee03bf22be443188e8bdfd3
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-03-17 12:25:40 +0200

    Custom Widgets: Added InfoWidget
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ gui/include/scopy/gui/info_widget.hpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see http://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef INFO_WIDGET_H
+#define INFO_WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class InfoWidget;
+}
+namespace scopy {
+namespace gui {
+
+class InfoWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit InfoWidget(QWidget* parent = 0);
+	~InfoWidget();
+
+private:
+	Ui::InfoWidget* m_ui;
+	QTimer* m_timer;
+
+public Q_SLOTS:
+	void enable(bool);
+	void setText(QString);
+
+private Q_SLOTS:
+	void updatePosition();
+};
+} // namespace gui
+} // namespace scopy
+
+#endif // INFO_WIDGET_H
---
+++ gui/src/info_widget.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2019 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see http://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ui_info_widget.h"
+
+#include <QTimer>
+
+#include <scopy/gui/info_widget.hpp>
+
+using namespace scopy::gui;
+
+InfoWidget::InfoWidget(QWidget* parent)
+	: QWidget(parent)
+	, m_ui(new Ui::InfoWidget)
+{
+	m_ui->setupUi(this);
+	m_timer = new QTimer(this);
+	connect(m_timer, SIGNAL(timeout()), this, SLOT(updatePosition()));
+	this->hide();
+}
+
+InfoWidget::~InfoWidget() { delete m_ui; }
+
+void InfoWidget::updatePosition()
+{
+	QPoint p = this->parentWidget()->mapFromGlobal(QCursor::pos());
+
+	// Position the widget in the bottom right corner of the pixmap
+	// that is dragged
+	this->move(p.x() + 159, p.y() + 54);
+}
+
+void InfoWidget::setText(QString text) { m_ui->lbl->setText(text); }
+
+void InfoWidget::enable(bool on)
+{
+	if (on) {
+		m_timer->start(1);
+		this->show();
+	} else {
+		m_timer->stop();
+		this->hide();
+	}
+}
