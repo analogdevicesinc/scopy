c2f96958d8288af9251fea1b859f2cbd92838458
DanielGuramulta <Daniel.Guramulta@analog.com>
2021-03-18 14:23:30 +0200

    AzurePipelines: add cmake-format check
    This check will verify that the coding style for the cmake files is respected. Otherwise the job will fail and a patch to fix the style issue will be found in the artifacts section
    
    Signed-off-by: DanielGuramulta <Daniel.Guramulta@analog.com>

--- ci/azure/run_build.sh
+++ ci/azure/run_build.sh
@@ -12,4 +12,8 @@ build_clang_format() {
     check_clangformat
 }
 
+build_cmake_format() {
+    check_cmakeformat
+}
+
 build_${BUILD_TYPE:-default}
--- cmake/codeStyle.cmake
+++ cmake/codeStyle.cmake
@@ -4,6 +4,15 @@ if(ENABLE_CMAKE_FORMAT)
     find_program(CMAKEFORMAT cmake-format)
     if(CMAKEFORMAT)
         file(GLOB_RECURSE CMAKE_FILES *.cmake CMakeLists.txt)
+
+        # ignore old source code files
+        foreach(element ${CMAKE_FILES})
+            string(REGEX MATCH ".*old.*" result ${element})
+            if(result)
+                list(REMOVE_ITEM CMAKE_FILES ${element})
+            endif()
+        endforeach()
+
         add_custom_target(
             cmake-format COMMAND ${CMAKEFORMAT} -i ${CMAKE_FILES}
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
