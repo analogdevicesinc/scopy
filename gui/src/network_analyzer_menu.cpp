db8a1bf8ffa73167c512553419af0d19f81f8a34
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-03-31 19:16:46 +0300

    Custom Widgets: Added NetworkAnalyzerMenu
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ gui/src/network_analyzer_menu.cpp
@@ -0,0 +1,52 @@
+#include <scopy/gui/network_analyzer_menu.hpp>
+#include "ui_network_analyzer_menu.h"
+
+using namespace scopy::gui;
+
+NetworkAnalyzerMenu::NetworkAnalyzerMenu(QWidget *parent) :
+	QWidget(parent),
+	m_ui(new Ui::NetworkAnalyzerMenu)
+{
+	m_ui->setupUi(this);
+
+	m_ui->widgetCursorsMenuHeader->setLabel("Cursors");
+	m_ui->widgetCursorsMenuHeader->setLineColor(new QColor("#4A64FF"));
+
+	m_ui->widgetSettingsMenuHeader->setLabel("Settings");
+	m_ui->widgetSettingsMenuHeader->setLineColor(new QColor("#4A64FF"));
+
+	m_ui->widgetGeneralSettingsMenuHeader->setLabel("General Settings");
+	m_ui->widgetGeneralSettingsMenuHeader->setLineColor(new QColor("#4A64FF"));
+
+	m_ui->widgetPlotSubsSep->setLabel("PLOT");
+	m_ui->widgetPlotSubsSep->setButton(false);
+
+	m_ui->widgetSweepSubsSep->setLabel("SWEEP");
+	m_ui->widgetSweepSubsSep->setButton(false);
+
+	m_ui->widgetExportSubsSep->setLabel("EXPORT");
+	m_ui->widgetExportSubsSep->setButton(false);
+
+	m_ui->widgetDisplaySubsSep->setLabel("DISPLAY");
+	m_ui->widgetDisplaySubsSep->setButton(false);
+
+	m_ui->widgetResponseSubsSep->setLabel("RESPONSE");
+	m_ui->widgetResponseSubsSep->setButton(false);
+
+	m_ui->widgetReferenceSubsSep->setLabel("REFRENCE");
+	m_ui->widgetReferenceSubsSep->setButton(false);
+
+	m_ui->widgetReferenceSubsSep_2->setLabel("REFRENCE");
+	m_ui->widgetReferenceSubsSep_2->setButton(false);
+
+	m_ui->widgetCursorReadoutsSubsSep->setLabel("CURSOR READOUTS");
+	m_ui->widgetCursorReadoutsSubsSep->setButton(false);
+
+	m_ui->widgetBufferPreviewerSubsSep->setLabel("BUFFER PREVIEWER");
+	m_ui->widgetBufferPreviewerSubsSep->setButton(false);
+}
+
+NetworkAnalyzerMenu::~NetworkAnalyzerMenu()
+{
+	delete m_ui;
+}
