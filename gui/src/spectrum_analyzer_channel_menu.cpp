f97432da8ac1c03c0ea74c2f15d6f85670b774a3
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-04-22 15:21:23 +0300

    Spectrum Analyzer: Added Channel menu
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ gui/src/spectrum_analyzer_channel_menu.cpp
@@ -0,0 +1,65 @@
+#include "ui_spectrum_analyzer_channel_menu.h"
+
+#include <scopy/gui/spectrum_analyzer_channel_menu.hpp>
+
+using namespace scopy::gui;
+
+SpectrumAnalyzerChannelMenu::SpectrumAnalyzerChannelMenu(QWidget* parent)
+	: QWidget(parent)
+	, m_ui(new Ui::SpectrumAnalyzerChannelMenu)
+{
+	m_ui->setupUi(this);
+
+	initUi();
+}
+
+SpectrumAnalyzerChannelMenu::~SpectrumAnalyzerChannelMenu() { delete m_ui; }
+
+void SpectrumAnalyzerChannelMenu::initUi()
+{
+	// Temporary: each channel will set its name and line color
+	m_ui->widgetMenuHeader->setLabel("Channel");
+	m_ui->widgetMenuHeader->setLineColor(new QColor("#4A64FF"));
+
+	// Initialize Type ComboBox
+	m_avgTypes = {
+		{tr("Sample"), FftDisplayPlot::AverageType::SAMPLE},
+		{tr("Peak Hold"), FftDisplayPlot::AverageType::PEAK_HOLD},
+		{tr("Peak Hold Continous"), FftDisplayPlot::AverageType::PEAK_HOLD_CONTINUOUS},
+		{tr("Min Hold"), FftDisplayPlot::AverageType::MIN_HOLD},
+		{tr("Min Hold Continous"), FftDisplayPlot::AverageType::MIN_HOLD_CONTINUOUS},
+		{tr("Linear RMS"), FftDisplayPlot::AverageType::LINEAR_RMS},
+		{tr("Linear dB"), FftDisplayPlot::AverageType::LINEAR_DB},
+		{tr("Exponential RMS"), FftDisplayPlot::AverageType::EXPONENTIAL_RMS},
+		{tr("Exponential dB"), FftDisplayPlot::AverageType::EXPONENTIAL_DB},
+	};
+
+	m_ui->cmbBoxType->blockSignals(true);
+	m_ui->cmbBoxType->clear();
+
+	for (auto it = m_avgTypes.begin(); it != m_avgTypes.end(); ++it) {
+		m_ui->cmbBoxType->addItem(it->first);
+	}
+
+	m_ui->cmbBoxType->blockSignals(false);
+
+	// Initialize Window ComboBox
+	m_winTypes = {
+		{tr("Flat top"), FftWinType::FLAT_TOP},
+		{tr("Rectangular"), FftWinType::RECTANGULAR},
+		{tr("Triangular (Bartlett)"), FftWinType::TRIANGULAR},
+		{tr("Hamming"), FftWinType::HAMMING},
+		{tr("Hann"), FftWinType::HANN},
+		{tr("Blackman-Harris"), FftWinType::BLACKMAN_HARRIS},
+		{tr("Kaiser"), FftWinType::KAISER},
+	};
+
+	m_ui->cmbBoxWindow->blockSignals(true);
+	m_ui->cmbBoxWindow->clear();
+
+	for (auto it = m_winTypes.begin(); it != m_winTypes.end(); ++it) {
+		m_ui->cmbBoxWindow->addItem(it->first);
+	}
+
+	m_ui->cmbBoxWindow->blockSignals(false);
+}
