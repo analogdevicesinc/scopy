c1f7d09555c331e3d4c73943f7023cb50c1eb0ed
AlexandraTrifan <Alexandra.Trifan@analog.com>
2021-05-20 20:07:16 +0300

    scopyExceptions: Don't create a pop up to handle an exception, since that might cause a hang if thrown from another thread.
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- src/scopyExceptionHandler.h
+++ src/scopyExceptionHandler.h
@@ -1,38 +1,34 @@
 #ifndef SCOPYEXCEPTIONHANDLER_H
 #define SCOPYEXCEPTIONHANDLER_H
 
-#include <QMessageBox>
 #include <QApplication>
 #include <config.h>
 #include <libm2k/m2kexceptions.hpp>
 #include <scopyApplication.hpp>
 #include <QThread>
+#include <QDebug>
 
 using namespace adiscope;
+
+// Debug messages are currently disabled for users, can be used for development.
+// The message box was removed because exceptions might occur both from the control flow
+// and for signaling problems. Until those are handled separately, we can't use
+// QMessageBox to show the exception, since those can be thrown from different threads.
 
 #define HANDLE_EXCEPTION(e) \
 if (GetScopyApplicationInstance()->getDebugMode()) { \
 	auto me = dynamic_cast<libm2k::m2k_exception *>(&e); \
-	auto handleLambda = [me, e](){ \
-		QMessageBox msg; \
-		QString str; \
-		if (me) { \
-			str = QString("Exception %1\ne.type() - %2\ne.what()  - %3\ne.iioCode() - %4\nthrown from %5:%6\ncaught in %7:%8\nScopy git tag %9\n") \
-			.arg("m2k_exception").arg(me->type()).arg(me->what()).arg(me->iioCode()).arg(QString::fromStdString(me->file())).arg(QString::number(me->line())).arg(__FILE__).arg(__LINE__).arg(SCOPY_VERSION_GIT); \
-		} else { \
-			str = QString("Exception  %1\ncaught in %2:%3\nScopy git tag %4\n").arg(e.what()).arg(__FILE__).arg(__LINE__).arg(SCOPY_VERSION_GIT); \
-		} \
-		if (WriteScopyMinidump()) { \
-			str = str + "Created minidump."; \
-		} \
-		msg.setText(str); \
-		msg.exec(); \
-	}; \
-	if (GetScopyApplicationInstance()->thread() != QThread::currentThread()) { \
-		QMetaObject::invokeMethod(GetScopyApplicationInstance(), handleLambda, Qt::BlockingQueuedConnection); \
+	QString str; \
+	if (me) { \
+		str = QString("Exception %1\ne.type() - %2\ne.what()  - %3\ne.iioCode() - %4\nthrown from %5:%6\ncaught in %7:%8\nScopy git tag %9\n") \
+		.arg("m2k_exception").arg(me->type()).arg(me->what()).arg(me->iioCode()).arg(QString::fromStdString(me->file())).arg(QString::number(me->line())).arg(__FILE__).arg(__LINE__).arg(SCOPY_VERSION_GIT); \
 	} else { \
-		handleLambda(); \
+		str = QString("Exception  %1\ncaught in %2:%3\nScopy git tag %4\n").arg(e.what()).arg(__FILE__).arg(__LINE__).arg(SCOPY_VERSION_GIT); \
 	} \
+	if (WriteScopyMinidump()) { \
+		str = str + "Created minidump."; \
+	} \
+	qDebug() << str << "\n"; \
 } \
 
 #endif // SCOPYEXCEPTIONHANDLER_H
