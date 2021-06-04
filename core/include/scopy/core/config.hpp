dd759234b250a42c2a2cd570f6b576327ce57dde
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-06-04 15:06:32 +0300

    Added config file
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ core/include/scopy/core/config.hpp
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H_CMAKEIN
+#define CONFIG_H_CMAKEIN
+
+#define PROJECT_VERSION	       ""
+#define SCOPY_VERSION_GIT      "8e2ef8d7"
+#define BREAKPAD_HANDLER       0
+#define PYTHON_VERSION		"python."
+
+#endif // CONFIG_H_CMAKEIN
