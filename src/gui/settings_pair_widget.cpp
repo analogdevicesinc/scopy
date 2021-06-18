eea69c580cfdbf29970bd26f9798ac1c4e8da8a8
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-06-18 18:03:38 +0300

    gui: added SettingsPairWidget
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ src/gui/settings_pair_widget.cpp
@@ -0,0 +1,18 @@
+#include "ui_settings_pair_widget.h"
+
+#include "settings_pair_widget.hpp"
+
+using namespace scopy::gui;
+
+SettingsPairWidget::SettingsPairWidget(QWidget* parent)
+	: QWidget(parent)
+	, m_ui(new Ui::SettingsPairWidget)
+{
+	m_ui->setupUi(this);
+}
+
+SettingsPairWidget::~SettingsPairWidget() { delete m_ui; }
+
+CustomPushButton* SettingsPairWidget::getGeneralSettingsBtn() { return m_ui->btnGenSettings; }
+
+QPushButton* SettingsPairWidget::getSettingsBtn() { return m_ui->btnSettings; }
