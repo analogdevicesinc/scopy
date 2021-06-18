6a3c8b21e6e6bfa31ad1f8a173ef76d5f63a3be6
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-06-18 18:03:38 +0300

    gui: added SettingsPairWidget
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ src/gui/settings_pair_widget.hpp
@@ -0,0 +1,34 @@
+#ifndef SETTINGSCUSTOMWIDGET_H
+#define SETTINGSCUSTOMWIDGET_H
+
+#include <QButtonGroup>
+#include <QWidget>
+
+#include "customPushButton.hpp"
+
+using namespace adiscope;
+
+namespace Ui {
+class SettingsPairWidget;
+}
+
+namespace scopy {
+namespace gui {
+class SettingsPairWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit SettingsPairWidget(QWidget* parent = nullptr);
+	~SettingsPairWidget();
+
+	CustomPushButton* getGeneralSettingsBtn();
+	QPushButton* getSettingsBtn();
+
+private:
+	Ui::SettingsPairWidget* m_ui;
+};
+} // namespace gui
+} // namespace scopy
+
+#endif // SETTINGSPAIRWIDGET_H
