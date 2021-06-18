0f707bd460a23726815485c9fbdc4e929d920d01
Ioana Chelaru <Ioana.Chelaru@analog.com>
2021-06-18 18:02:37 +0300

    gui: added SubsectionSeparator
    Signed-off-by: Ioana Chelaru <Ioana.Chelaru@analog.com>

---
+++ src/gui/subsection_separator.hpp
@@ -0,0 +1,44 @@
+#ifndef SUBSECTIONSEPARATOR_H
+#define SUBSECTIONSEPARATOR_H
+
+#include <QLabel>
+#include <QPushButton>
+#include <QWidget>
+
+namespace Ui {
+class SubsectionSeparator;
+}
+
+namespace scopy {
+namespace gui {
+class SubsectionSeparator : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit SubsectionSeparator(QWidget* parent = nullptr);
+	explicit SubsectionSeparator(const QString& text, const bool buttonVisible = false, QWidget* parent = nullptr);
+	~SubsectionSeparator();
+
+private:
+	Ui::SubsectionSeparator* m_ui;
+
+public:
+	QPushButton* getButton();
+	void setButtonVisible(bool buttonVisible);
+
+	bool getButtonChecked();
+	void setButtonChecked(bool checked);
+
+	QLabel* getLabel();
+	void setLabel(const QString& text);
+
+	void setLabelVisible(bool visible);
+	void setLineVisible(bool visible);
+
+	void setContent(QWidget* content);
+};
+} // namespace gui
+} // namespace scopy
+
+#endif // SUBSECTIONSEPARATOR_H
