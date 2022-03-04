ab181450642effb1fec58d1f66e6c301a219b5b6
AlexandraTrifan <Alexandra.Trifan@analog.com>
2022-03-04 14:47:16 +0200

    azure-pipelines.yml: Move MacOS builds to Azure Pipelines.
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- CI/appveyor/build_appveyor_macos.sh
+++ CI/appveyor/build_appveyor_macos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+STAGINGDIR="${PWD}/staging"
 
 # Use the /usr/local/bin/pkg-config instead of /Mono/../pkg-config
 export PATH="/usr/local/bin:$PATH"
@@ -15,22 +16,17 @@ if command -v brew ; then
 fi
 
 NUM_JOBS=4
-
 mkdir -p build
-
 cd build
 
-if [ "$APPVEYOR" == "true" ] ; then
-	MACOS_VERSION=$(/usr/libexec/PlistBuddy -c "Print:ProductVersion" /System/Library/CoreServices/SystemVersion.plist)
-	if [[ "$MACOS_VERSION" == "10.14."* ]] ; then
-		export MACOSX_DEPLOYMENT_TARGET=10.13
-	fi
-	cmake ..
-	make -j${NUM_JOBS}
-	otool -l ./Scopy.app/Contents/MacOS/Scopy # for debugging
-else
-	cmake -DCMAKE_PREFIX_PATH="$STAGINGDIR;${QT_PATH}/lib/cmake" -DCMAKE_INSTALL_PREFIX="$STAGINGDIR" \
-		-DCMAKE_EXE_LINKER_FLAGS="-L${STAGINGDIR}/lib" ..
-	CFLAGS=-I${STAGINGDIR}/include LDFLAGS=-L${STAGINGDIR}/lib make -j${NUM_JOBS}
+MACOS_VERSION=$(/usr/libexec/PlistBuddy -c "Print:ProductVersion" /System/Library/CoreServices/SystemVersion.plist)
+if [[ "$MACOS_VERSION" == "10.14."* ]] ; then
+	export MACOSX_DEPLOYMENT_TARGET=10.13
 fi
+cmake -DCMAKE_STAGING_PREFIX="${STAGINGDIR}" \
+	-DCMAKE_PREFIX_PATH="${STAGINGDIR};${STAGINGDIR}/lib/cmake;${STAGINGDIR}/lib/pkgconfig;${STAGINGDIR}/lib/cmake/gnuradio;${STAGINGDIR}/lib/cmake/iio;${QT_PATH}/lib/cmake" \
+	-DCMAKE_INSTALL_PREFIX="${STAGINGDIR}" \
+	-DCMAKE_EXE_LINKER_FLAGS="-L${STAGINGDIR}/lib" ..
+CFLAGS=-I${STAGINGDIR}/include LDFLAGS=-L${STAGINGDIR}/lib make -j${NUM_JOBS}
 
+otool -l ./Scopy.app/Contents/MacOS/Scopy
