c3fb38125649b400d68d56e5aa103be380726a05
Adrian Suciu <adrian.suciu@analog.com>
2022-07-05 14:16:27 +0300

    android: added scripts that can be run by ci/user to deploy apk/aab
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- CI/appveyor/build_scopy_apk.sh
+++ CI/appveyor/build_scopy_apk.sh
@@ -4,50 +4,74 @@ source ./android_toolchain.sh $1 $2
 
 ARTIFACT_LOCATION=$GITHUB_WORKSPACE
 
-build_scopy() {
 
+clone_scopy() {
 	git clone $REPO_URL
 	pushd scopy
 	git submodule update --init --recursive iio-emu
 
 	git fetch origin $BRANCH
 	git checkout FETCH_HEAD
-
-	rm -rf build*
-
-	cp $BUILD_STATUS_FILE .
-	cp $BUILD_ROOT/android_cmake.sh .
-	cp $SCRIPT_HOME_DIR/android_deploy_qt.sh .
-
-	./android_cmake.sh .
-
-	cd $BUILDDIR
-	make -j$JOBS
-	cd ..
-
-	./android_deploy_qt.sh
-	cd $BUILDDIR
-	make apk
-	make aab
-	cd ..
-	
 	popd
 }
 
+
+#cp libs
+cp_scripts() {
+	pushd scopy
+	cp $BUILD_STATUS_FILE .
+	cp $BUILD_ROOT/android_cmake.sh .
+	cp ./CI/android/* .
+	popd
+}
+
+make_apk_aab() {
+	source ./android_toolchain.sh arm
+	pushd scopy
+	rm -rf build*
+
+	./android_cmake.sh .
+	cd $BUILDDIR
+	make -j$JOBS iio-emu
+	make -j$JOBS scopy
+	cd ..
+	./android_deploy_libs.sh
+	./android_deploy_qt.sh apk
+
+	pushd ../
+	source ./android_toolchain.sh aarch64
+	popd
+
+	./android_cmake.sh .
+	cd $BUILDDIR
+	make -j$JOBS iio-emu
+	make -j$JOBS scopy
+	cd ..
+	./android_deploy_libs.sh
+	./android_deploy_qt.sh apk #sign
+
+	./android_deploy_qt.sh aab #sign
+	popd
+}
+
+
+
 move_artifact() {
 	pushd scopy
-	sudo cp ./$BUILDDIR/android-build/build/outputs/apk/debug/android-build-debug.apk $ARTIFACT_LOCATION/
-	sudo cp ./$BUILDDIR/android-build/build/outputs/bundle/debug/android-build-debug.aab $ARTIFACT_LOCATION/
-	sudo cp ./$BUILDDIR/android-build/build/outputs/bundle/release/android-build-release.aab $ARTIFACT_LOCATION/
+	sudo cp *.apk $ARTIFACT_LOCATION
+	sudo cp *.aab $ARTIFACT_LOCATION
+
 	pushd $ARTIFACT_LOCATION
-	sudo chmod 644 ./android-build-debug.apk
-	sudo chmod 644 ./android-build-debug.aab
-	sudo chmod 644 ./android-build-release.aab
+	sudo chmod 644 *.apk
+	sudo chmod 644 *.aab
+
 	ls -la $ARTIFACT_LOCATION
 	popd
 
 	popd
 }
 
-build_scopy
-move_artifact
+ clone_scopy
+ cp_scripts
+ make_apk_aab
+ move_artifact
