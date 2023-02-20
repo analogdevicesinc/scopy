b552d84a6257bc9f4687ae65777cb2a40d5caa5f
Adrian Suciu <adrian.suciu@analog.com>
2023-02-20 20:57:19 +0200

    Created folders and CMake for all plugins and scopymodules
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

---
+++ plugins/testplugin2/include/testplugin2/testpluginip.h
@@ -0,0 +1,31 @@
+#ifndef TESTPLUGINIP_H
+#define TESTPLUGINIP_H
+
+#include <QObject>
+#include <pluginbase/plugin.h>
+#include "scopytestplugin2_export.h"
+
+namespace adiscope {
+class SCOPYTESTPLUGIN2_EXPORT TestPluginIp : public Plugin
+{
+	Q_OBJECT
+public:
+	TestPluginIp(QObject *parent = nullptr);
+	~TestPluginIp();
+
+
+	// Plugin interface
+public:
+	bool compatible(QString uri) override;
+	bool load(QString uri) override;
+	void unload() override;
+
+	bool connectDev() override;
+	bool disconnectDev() override;
+
+private:
+	QWidget *m_tool;
+
+};
+}
+#endif // TESTPLUGINIP_H
