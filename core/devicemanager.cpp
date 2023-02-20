5b47238fc16fd4fa3d3503850872cb88d04dd27b
Adrian Suciu <adrian.suciu@analog.com>
2023-02-20 16:07:14 +0200

    Fixed device remove lifecycle and added requestTool from plugin
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- core/devicemanager.cpp
+++ core/devicemanager.cpp
@@ -23,6 +23,8 @@ Device* DeviceManager::getDevice(QString uri) {
 void DeviceManager::addDevice(QString uri)
 {	
 	qInfo(CAT_DEVICEMANAGER) << "device" << uri << "added";
+	Q_EMIT deviceAddStarted(uri);
+
 	Device *d = nullptr;
 	if(map.contains(uri)) {
 //		d = map.value(uri,nullptr);
@@ -33,14 +35,49 @@ void DeviceManager::addDevice(QString uri)
 	d = new DeviceImpl(uri, this);//DeviceFactory::newDevice(uri);
 	d->loadPlugins();
 
-	connect(dynamic_cast<QObject*>(d),SIGNAL(connected()),this,SLOT(connectDevice()));
-	connect(dynamic_cast<QObject*>(d),SIGNAL(disconnected()),this,SLOT(disconnectDevice()));
-	connect(dynamic_cast<QObject*>(d),SIGNAL(toolListChanged()),this,SLOT(changeToolListDevice()));
+	connect(dynamic_cast<DeviceImpl*>(d),SIGNAL(connected()),this,SLOT(connectDevice()));
+	connect(dynamic_cast<DeviceImpl*>(d),SIGNAL(disconnected()),this,SLOT(disconnectDevice()));
+	connect(dynamic_cast<DeviceImpl*>(d),SIGNAL(requestedRestart()), this,SLOT(restartDevice()));
+	connect(dynamic_cast<DeviceImpl*>(d),SIGNAL(toolListChanged()),this,SLOT(changeToolListDevice()));
+	connect(dynamic_cast<DeviceImpl*>(d),SIGNAL(requestTool(QString)),this,SIGNAL(requestTool(QString)));
 
 	map[uri] = d;
 
 	if(d)
 		Q_EMIT deviceAdded(uri, d);
+}
+
+
+void DeviceManager::removeDevice(QString uri)
+{
+	Device *d = nullptr;
+
+	if(connectedDev.contains(uri)) {
+		getDevice(uri)->disconnectDev();
+	}
+
+	if(!map.contains(uri)) {
+		qWarning(CAT_DEVICEMANAGER) << uri <<"Device does not exist";
+		return;
+	}
+	d = map.take(uri);
+	Q_EMIT deviceRemoveStarted(uri, d);
+	disconnect(dynamic_cast<QObject*>(d),SIGNAL(connected()),this,SLOT(connectDevice()));
+	disconnect(dynamic_cast<QObject*>(d),SIGNAL(disconnected()),this,SLOT(disconnectDevice()));
+	disconnect(dynamic_cast<QObject*>(d),SIGNAL(toolListChanged()),this,SLOT(changeToolListDevice()));
+
+	d->unloadPlugins();
+	delete(d);
+
+	qInfo(CAT_DEVICEMANAGER) << "device" << uri << "removed";
+
+	Q_EMIT deviceRemoved(uri);
+}
+
+void DeviceManager::restartDevice(QString uri)
+{
+	removeDevice(uri);
+	addDevice(uri);
 }
 
 void DeviceManager::changeToolListDevice() {
@@ -81,13 +118,10 @@ bool DeviceManager::getExclusive() const {
 	return exclusive;
 }
 
-void DeviceManager::removeDevice(QString uri)
-{	
-	Device *d = nullptr;
-	if(map.contains(uri)) {
-		d = map.take(uri);
-		delete(d);
-	}
-	qInfo(CAT_DEVICEMANAGER) << "device" << uri << "removed";
-	Q_EMIT deviceRemoved(uri);
+void DeviceManager::restartDevice() {
+	QString uri = dynamic_cast<Device*>(QObject::sender())->uri();
+	qDebug(CAT_DEVICEMANAGER)<<"restarting "<< uri << "...";
+	restartDevice(uri);
+	Q_EMIT requestDevice(uri);
 }
+
--- core/testpluginip.cpp
+++ core/testpluginip.cpp
@@ -1,5 +1,6 @@
 #include "testpluginip.h"
 #include "qlabel.h"
+#include "qpushbutton.h"
 #include <QLoggingCategory>
 #include <QUuid>
 
@@ -22,33 +23,61 @@ bool TestPluginIp::compatible(QString uri) {
 }
 
 bool TestPluginIp::load(QString uri) {
+	static int count = 0;
 	m_uri = uri;
-	m_icon = new QLabel("");
+	m_icon = new QLabel(QString::number(count));
+	count++;
 	m_icon->setStyleSheet("border-image: url(:/icons/adalm.svg);");
 	m_page = new QLabel("TestPageIP");
 	m_toolList.append(new ToolMenuEntry(QUuid::createUuid().toString(),"IP","", this));
 	return true;
 }
 
+
+void TestPluginIp::unload() {
+	for(auto &tool : m_toolList) {
+		delete tool;
+	}
+
+}
+
 bool TestPluginIp::connectDev()
 {
 	qDebug(CAT_TESTPLUGINIP)<<"connect";
-	for(auto &&tool:m_toolList) {
-	qDebug(CAT_TESTPLUGINIP)<<tool->id()<<tool->name();
-	tool->setEnabled(true);
-	tool->setName("boo");
-	tool->setRunBtnVisible(true);	
-	}
-	m_toolList.append(new ToolMenuEntry(QUuid::createUuid().toString(),"IP222","", this));
-	m_toolList.last()->setEnabled(true);
+	qDebug(CAT_TESTPLUGINIP)<<m_toolList[0]->id()<<m_toolList[0]->name();
+	m_toolList[0]->setEnabled(true);
+	m_toolList[0]->setName("IP tool1");
+	m_toolList[0]->setRunBtnVisible(true);
+	m_toolList[0]->setTool(new QLabel("TestPage IP Renamed"));
+	QString iptool2UUID = QUuid::createUuid().toString();
+
+	m_toolList.append(new ToolMenuEntry(iptool2UUID,"IP tool2","", this));
+	m_toolList[1]->setEnabled(true);
+	QPushButton *btn = new QPushButton("LASTTOOOL testpage");
+	m_tool = btn;
+
+	connect(btn,&QPushButton::clicked,this,[=]() { Q_EMIT requestTool(m_toolList[0]->id());});
+
 	Q_EMIT toolListChanged();
-	m_toolList.last()->setTool(new QLabel("LASTTOOOL"));
+	m_toolList[1]->setTool(m_tool);
 
 	return true;
 }
 
 bool TestPluginIp::disconnectDev()
 {
+	for(auto &tool : m_toolList) {
+		tool->setEnabled(false);
+		tool->setRunBtnVisible(false);
+		if(tool->tool()) {
+			delete tool->tool();
+			tool->setTool(nullptr);
+		}
+	}
+	m_toolList.removeLast();
+	Q_EMIT toolListChanged();
+	m_toolList[0]->setName("IP");
+
 	qDebug(CAT_TESTPLUGINIP)<<"disconnect";
 	return true;
 }
