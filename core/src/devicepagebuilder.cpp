034adfdafb2882bd3c3b9b76eb85d986ad0b2858
Adrian Suciu <adrian.suciu@analog.com>
2023-03-07 14:39:07 +0200

    core: fixed deviceicon and devicepage builder
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- core/src/devicepagebuilder.cpp
+++
@@ -1,9 +0,0 @@
-#include "devicepagebuilder.h"
-
-DevicePageBuilder::DevicePageBuilder(QObject *parent) // creates buttons and subsequent pages ..
-	: QObject{parent}
-{
-
-}
-
-#include "moc_devicepagebuilder.cpp"
