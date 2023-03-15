6b8602bad6e8c5b47f66871b955c76f06d4e9ee9
Adrian Suciu <adrian.suciu@analog.com>
2023-03-15 10:53:45 +0200

    core: added preferences, added toolmenuentry run button interface and various fixes
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

---
+++ pluginbase/src/scopyenvironment.cpp
@@ -0,0 +1,38 @@
+#include "scopyenvironment.h"
+#include <QApplication>
+
+using namespace adiscope;
+ScopyEnvironment* ScopyEnvironment::pinstance_{nullptr};
+
+ScopyEnvironment::ScopyEnvironment(QObject *parent) : QObject(parent)
+{
+
+}
+
+ScopyEnvironment::~ScopyEnvironment()
+{
+
+}
+
+QVariant ScopyEnvironment::get(QString k)
+{
+	QVariant v = QVariant();
+	if(p.contains(k))
+		v = p[k];
+	return v;
+}
+
+void ScopyEnvironment::set(QString k, QVariant v)
+{
+	QVariant oldVal = p[k];
+	p[k] = v;
+}
+
+ScopyEnvironment *ScopyEnvironment::GetInstance()
+{
+	if (pinstance_ == nullptr)
+	{
+		pinstance_ = new ScopyEnvironment(QApplication::instance()); // singleton has the app as parent
+	}
+	return pinstance_;
+}
