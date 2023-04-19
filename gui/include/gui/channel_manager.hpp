945b560cde0ff5fe9b8be21b202633eb25ccd5cf
Adrian Suciu <adrian.suciu@analog.com>
2023-04-19 17:41:01 +0300

    gui: added toolview
    also removed obsolete files
    
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

---
+++ gui/include/gui/channel_manager.hpp
@@ -0,0 +1,73 @@
+#ifndef CHANNEL_MANAGER_HPP
+#define CHANNEL_MANAGER_HPP
+
+#include <QScrollArea>
+#include <QStackedWidget>
+#include <QWidget>
+#include <QLabel>
+
+#include "channel_widget.hpp"
+#include "customPushButton.h"
+
+namespace scopy {
+namespace gui {
+
+enum class ChannelsPositionEnum
+{
+	VERTICAL,
+	HORIZONTAL
+};
+
+class ChannelManager : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit ChannelManager(ChannelsPositionEnum position = ChannelsPositionEnum::HORIZONTAL,
+				QWidget* parent = nullptr);
+	~ChannelManager();
+
+	void build(QWidget* parent);
+	void insertAddBtn(QWidget* menu, bool dockable);
+
+	ChannelWidget* buildNewChannel(int chId, bool deletable, bool simplefied, QColor color, const QString& fullName,
+				       const QString& shortName);
+	void removeChannel(ChannelWidget* ch);
+	QList<ChannelWidget*> getEnabledChannels();
+
+	CustomPushButton* getAddChannelBtn();
+	QList<ChannelWidget*> getChannelsList();
+
+	void setChannelAlignment(ChannelWidget* ch,Qt::Alignment alignment);
+	void setChannelIdVisible(bool visible);
+	const QString getToolStatus() const;
+	void setToolStatus(const QString &newToolStatus);
+
+public Q_SLOTS:
+	void changeParent(QWidget* newParent);
+	void toggleChannelManager(bool toggled);
+
+Q_SIGNALS:
+	void configureAddBtn(QWidget* menu, bool dockable);
+	void positionChanged(ChannelsPositionEnum position);
+	void channelManagerToggle(bool toggled);
+
+private:
+	QWidget* m_parent;
+	QScrollArea* m_scrollArea;
+	QWidget* m_channelsWidget;
+	bool m_hasAddBtn;
+	CustomPushButton* m_addChannelBtn;
+	ChannelsPositionEnum m_position;
+	QList<ChannelWidget*> m_channelsList;
+	bool m_channelIdVisible;
+	QPushButton *toggleChannels;
+	bool channelManagerToggled;
+	QLabel *toolStatus;
+	int m_maxChannelWidth;
+	int m_minChannelWidth;
+};
+} // namespace gui
+} // namespace scopy
+
+#endif // CHANNEL_MANAGER_HPP
---
+++ gui/src/custom_menu_button.cpp
@@ -0,0 +1,54 @@
+#include "ui_custom_menu_button.h"
+
+#include "custom_menu_button.hpp"
+
+using namespace scopy::gui;
+
+CustomMenuButton::CustomMenuButton(QString labelText, bool checkboxVisible, bool checkBoxChecked, QWidget* parent)
+	: CustomMenuButton(parent)
+{
+
+	m_ui->lblCustomMenuButton->setText(labelText);
+	m_ui->checkBoxCustomMenuButton->setVisible(checkboxVisible);
+
+	if (checkboxVisible) {
+		checkBoxToggled(checkBoxChecked);
+		m_ui->checkBoxCustomMenuButton->setChecked(checkBoxChecked);
+	}
+}
+
+CustomMenuButton::CustomMenuButton(QWidget* parent)
+	: QWidget(parent)
+	, m_ui(new Ui::CustomMenuButton)
+	, m_floatingMenu(false)
+{
+	m_ui->setupUi(this);
+	connect(m_ui->checkBoxCustomMenuButton, &QCheckBox::toggled, this, &CustomMenuButton::checkBoxToggled);
+}
+
+CustomMenuButton::~CustomMenuButton() { delete m_ui; }
+
+void CustomMenuButton::setLabel(const QString& text) { m_ui->lblCustomMenuButton->setText(text); }
+
+void CustomMenuButton::setCheckboxVisible(bool visible) { m_ui->checkBoxCustomMenuButton->setVisible(visible); }
+
+CustomPushButton* CustomMenuButton::getBtn() { return m_ui->btnCustomMenuButton; }
+
+QCheckBox* CustomMenuButton::getCheckBox() { return m_ui->checkBoxCustomMenuButton; }
+
+bool CustomMenuButton::getCheckBoxState() { return getCheckBox()->isChecked(); }
+
+void CustomMenuButton::setCheckBoxState(bool checked) { m_ui->checkBoxCustomMenuButton->setChecked(checked); }
+
+void CustomMenuButton::setMenuFloating(bool floating) { m_floatingMenu = floating; }
+
+void CustomMenuButton::checkBoxToggled(bool toggled)
+{
+	if (!toggled) {
+		m_ui->btnCustomMenuButton->setChecked(false);
+	}
+
+	if (!m_floatingMenu) {
+		m_ui->btnCustomMenuButton->setEnabled(toggled);
+	}
+}
