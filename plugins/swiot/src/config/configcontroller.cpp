ef5034dcb878808f35b570888a1b8c785a4aa69c
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-05-25 11:29:08 +0300

    swiot: updated config mode to match the new context
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- plugins/swiot/src/config/configcontroller.cpp
+++ plugins/swiot/src/config/configcontroller.cpp
@@ -3,19 +3,19 @@
 
 using namespace scopy::swiot;
 
-ConfigController::ConfigController(ConfigChannelView* chnlsView,
-				   ConfigModel* model):
-	m_chnlsView(chnlsView)
-      ,m_model(model)
+ConfigController::ConfigController(ConfigChannelView *chnlsView,
+				   ConfigModel *model,
+				   int channelId) :
+	m_channelsView(chnlsView), m_model(model), m_channelId(channelId)
 {
-	createConnections();
+	this->initChannelView();
+	this->connectSignalsAndSlots();
 }
 
-ConfigController::~ConfigController()
-{
-	if (m_chnlsView) {
-		delete m_chnlsView;
-		m_chnlsView = nullptr;
+ConfigController::~ConfigController() {
+	if (m_channelsView) {
+		delete m_channelsView;
+		m_channelsView = nullptr;
 	}
 
 	if (m_model) {
@@ -24,28 +24,71 @@ ConfigController::~ConfigController()
 	}
 }
 
-void ConfigController::addMenuAttrValues()
-{
-	//get all the attributes from the iio_channel
-	QVector<QMap<QString, QStringList>> chnlsValues = m_model->getChnlsAttrValues();
+void ConfigController::connectSignalsAndSlots() {
+	QObject::connect(m_channelsView, &ConfigChannelView::enabledChanged, this, [this] (int index, bool value) {
+		if (m_channelId == index) { // TODO: find a better alternative
+			if (value) {
+				m_model->writeEnabled("1");
+			} else {
+				m_model->writeEnabled("0");
+			}
+		}
+	});
 
-	if (!chnlsValues.empty()) {
-		m_chnlsView->setChnlsAttr(chnlsValues);
-	}
+	QObject::connect(m_channelsView, &ConfigChannelView::deviceChanged, this, [this] (int index, const QString& device) {
+		if (m_channelId == index) {
+			m_model->writeDevice(device);
+
+			QStringList functionsAvailable = m_model->readFunctionAvailable();
+			m_channelsView->setFunctionAvailable(functionsAvailable);
+
+			QString selectedFunction = m_model->readFunction();
+			m_channelsView->setSelectedFunction(selectedFunction);
+		}
+	});
+
+	QObject::connect(m_channelsView, &ConfigChannelView::functionChanged, this, [this] (int index, const QString& function) {
+		if (m_channelId == index) {
+			m_model->writeFunction(function);
+		}
+	});
 }
 
-void ConfigController::createConnections()
-{
-	connect(m_chnlsView, &scopy::swiot::ConfigChannelView::attrValueChanged, this, &ConfigController::attrChanged);
+void ConfigController::initChannelView() {
+	QString enabled = m_model->readEnabled();
+	m_channelsView->setChannelEnabled((enabled == "1"));
+
+	QStringList devicesAvailable = m_model->readDeviceAvailable();
+	m_channelsView->setDeviceAvailable(devicesAvailable);
+
+	QString selectedDevice = m_model->readDevice();
+	m_channelsView->setSelectedDevice(selectedDevice);
+
+	QStringList functionsAvailable = m_model->readFunctionAvailable();
+	m_channelsView->setFunctionAvailable(functionsAvailable);
+
+	QString selectedFunction = m_model->readFunction();
+	m_channelsView->setSelectedFunction(selectedFunction);
 }
 
-void ConfigController::attrChanged(QString attrName, int deviceIdx)
-{
-	QVector<QMap<QString, QStringList>> values = m_chnlsView->getChnlsAttr();
-	m_model->updateChnlAttributes(values, attrName, deviceIdx);
-}
-
-QStringList ConfigController::getActiveFunctions()
-{
-	return m_model->getActiveFunctions();
-}
+//void ConfigController::addMenuAttrValues() {
+//	//get all the attributes from the iio_channel
+//	QStringList chnlsValues = m_model->getMFunctionAvailable();
+//
+//	if (!chnlsValues.empty()) {
+//		m_chnlsView->setChnlsAttr(chnlsValues);
+//	}
+//}
+//
+//void ConfigController::createConnections() {
+//	connect(m_chnlsView, &scopy::swiot::ConfigChannelView::attrValueChanged, this, &ConfigController::attrChanged);
+//}
+//
+//void ConfigController::attrChanged(const QString& attrName, const QString& attrValue) {
+////	QMap<QString, QString> values = m_chnlsView->getChnlsAttr();
+//	m_model->updateChnlAttributes(attrName, attrValue);
+//}
+//
+//QStringList ConfigController::getActiveFunctions() {
+//	return m_model->getActiveFunctions();
+//}
