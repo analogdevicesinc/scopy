91d4c575ba409b116cf81036f5c1a044c495fbe9
AlexandraTrifan <Alexandra.Trifan@analog.com>
2023-05-25 12:39:56 +0300

    IioUtil: Add a Command Queue structure to handle IIO commands.
    If tinyiiod contexts are used, the IIO calls cannot be done in parallel,
    since the daemon can't handle multiple commands at once. This command
    queue structure would allow the creation of any Commands and execute them
    in the desired manner. The Command Queue can be created to run in the main
    thread, run in a work thread and execute commands in place or run in a
    work thread and execute each command on a separate thread.
    
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

---
+++ iioutil/include/iioutil/iiocommand/iiochannelattributewrite.h
@@ -0,0 +1,39 @@
+#ifndef IIOCHANNELATTRIBUTEWRITE_H
+#define IIOCHANNELATTRIBUTEWRITE_H
+
+#include "../command.h"
+#include <iio.h>
+
+namespace scopy {
+class SCOPY_IIOUTIL_EXPORT IioChannelAttributeWrite : public Command {
+	Q_OBJECT
+public:
+	explicit IioChannelAttributeWrite(struct iio_channel *channel,
+					  const char *attribute,
+					  const char *value,
+					  QObject *parent,
+					  bool overwrite = false)
+		: m_channel(channel)
+		, m_attribute_name(std::string(attribute))
+		, m_value(std::string(value))
+	{
+		setOverwrite(overwrite);
+		this->setParent(parent);
+		m_cmdResult = new CommandResult();
+	}
+
+	virtual void execute() override {
+		Q_EMIT started(this);
+		ssize_t ret = iio_channel_attr_write(m_channel, m_attribute_name.c_str(), m_value.c_str());
+		m_cmdResult->errorCode = ret;
+		Q_EMIT finished(this);
+	}
+
+private:
+	struct iio_channel *m_channel;
+	std::string m_attribute_name;
+	std::string m_value;
+};
+}
+
+#endif // IIOCHANNELATTRIBUTEWRITE_H
