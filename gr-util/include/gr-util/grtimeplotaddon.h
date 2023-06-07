6e143ab5ba4aed6f4ba2680306dcc719a2647a8f
Adrian Suciu <adrian.suciu@analog.com>
2023-06-07 17:52:30 +0300

    gr-util: implement gr time domain addons
    Addons are modules that can be added to a dynamic instrument
    
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

---
+++ gr-util/include/gr-util/grtimeplotaddon.h
@@ -0,0 +1,48 @@
+#ifndef GRTIMEPLOTADDON_H
+#define GRTIMEPLOTADDON_H
+
+#include "scopy-gr-util_export.h"
+#include "tooladdon.h"
+#include <gui/oscilloscope_plot.hpp>
+#include <QGridLayout>
+#include <gr-gui/scope_sink_f.h>
+
+namespace scopy::grutil {
+using namespace scopy;
+class GRTopBlock;
+
+class SCOPY_GR_UTIL_EXPORT GRTimePlotAddon : public QObject, public ToolAddon {
+	Q_OBJECT
+public:
+	GRTimePlotAddon(QString name, GRTopBlock *top, QObject *parent = nullptr);
+	~GRTimePlotAddon();
+
+	QString getName() override;
+	QWidget *getWidget() override;
+	CapturePlot *plot();
+
+public Q_SLOTS:
+	void enable() override;
+	void disable() override;
+	void onStart() override;
+	void onStop() override;
+	void onAdd() override;
+	void onRemove() override;
+	void onChannelAdded(ToolAddon* t) override;
+	void onChannelRemoved(ToolAddon*) override;
+
+
+private slots:
+	void connectSignalPaths();
+	void tearDownSignalPaths();
+	void onNewData();
+private:
+	QString name;
+	QWidget *widget;
+	CapturePlot *m_plot;
+	GRTopBlock *m_top;
+	QList<scope_sink_f::sptr> sinks;
+};
+}
+
+#endif // GRTIMEPLOTADDON_H
