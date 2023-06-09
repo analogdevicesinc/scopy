7c127336d94921d6b6ec6c53065c359d233b9128
andrei.danila <andrei.danila@analog.com>
2023-06-09 10:47:21 +0300

    swiot: Added ExternalPsReaderThred to SwiotController.
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

--- plugins/swiot/src/externalpsreaderthread.cpp
+++ plugins/swiot/src/externalpsreaderthread.cpp
@@ -1,28 +1,38 @@
 #include "externalpsreaderthread.h"
+#include <iioutil/contextprovider.h>
 #include "src/swiot_logging_categories.h"
 
 #include <utility>
 
 using namespace scopy::swiot;
 
-ExternalPsReaderThread::ExternalPsReaderThread(struct iio_device* device, QString attribute, QObject *parent) :
-	QThread(parent),
-	m_device(device),
-	m_attribute(std::move(attribute))
-{
-
-}
+ExternalPsReaderThread::ExternalPsReaderThread(QString uri, QString attr)
+	: QThread()
+	, m_uri(uri)
+	, m_attribute(attr)
+{}
 
 void ExternalPsReaderThread::run() {
 	bool value = false;
-	ssize_t res = iio_device_attr_read_bool(m_device, m_attribute.toStdString().c_str(), &value);
-	if (res < 0) {
-		qCritical(CAT_SWIOT) << "Error, could not read ext_psu attribute from swiot device, error code" << res;
-	} else {
-		qCritical(CAT_SWIOT) << "testing ps" << res;
-
-		Q_EMIT hasConnectedPowerSupply(value);
+	iio_context *ctx = ContextProvider::GetInstance()->open(m_uri);
+	if (!ctx) {
+		return;
 	}
+	if (isInterruptionRequested()) {
+		ContextProvider::GetInstance()->close(m_uri);
+		return;
+	}
+	iio_device *swiotDevice = iio_context_find_device(ctx, "swiot");
+	if (swiotDevice) {
+		ssize_t res = iio_device_attr_read_bool(swiotDevice, m_attribute.toStdString().c_str(), &value);
+		if (res < 0) {
+			qCritical(CAT_SWIOT) << "Error, could not read ext_psu attribute from swiot device, error code" << res;
+		} else {
+			qCritical(CAT_SWIOT) << "testing ps" << res;
+			Q_EMIT hasConnectedPowerSupply(value);
+		}
+	}
+	ContextProvider::GetInstance()->close(m_uri);
 }
 
 #include "moc_externalpsreaderthread.cpp"
