05cc0a6b782485850b7c9cf5be30d30a14442660
andrei.danila <andrei.danila@analog.com>
2023-06-13 09:13:48 +0300

    gui/InfoPage: Mapped the name of iio context attributes.
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

--- gui/src/infopagekeyvaluewidget.cpp
+++ gui/src/infopagekeyvaluewidget.cpp
@@ -14,6 +14,8 @@ InfoPageKeyValueWidget::InfoPageKeyValueWidget(QString key, QString value, QWidg
 	valueWidget = new QLabel(value, this);
 	lay->addWidget(keyWidget);
 	lay->addWidget(valueWidget);
+	lay->setStretch(0, 1);
+	lay->setStretch(1, 3);
 }
 
 void InfoPageKeyValueWidget::updateValue(QString value) {
