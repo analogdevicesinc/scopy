438d27c4ef1a4d6d8e8d31f28d721a60694f899d
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-06-13 14:26:43 +0300

    swiot: updated icon and text colour for status label
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- plugins/swiot/src/runtime/faults/faults.cpp
+++ plugins/swiot/src/runtime/faults/faults.cpp
@@ -57,9 +57,9 @@ void Faults::setupDynamicUi(QWidget *parent) {
 	m_statusContainer->setLayout(new QHBoxLayout(m_statusContainer));
 	m_statusContainer->layout()->setSpacing(0);
 	m_statusContainer->layout()->setContentsMargins(0,0,0,0);
-	m_statusLabel->setText("The external power supply is not connected. The MAX14906 chip will not be used at full capacity.");
+	m_statusLabel->setText("The system is powered at limited capacity.");
 	m_statusLabel->setWordWrap(true);
-	m_statusContainer->setStyleSheet("color: red; background-color: rgba(0, 0, 0, 60); border: 1px solid rgba(0, 0, 0, 30); font-size: 11pt");
+	m_statusContainer->setStyleSheet("color: ffc904; background-color: rgba(0, 0, 0, 60); border: 1px solid rgba(0, 0, 0, 30); font-size: 11pt");
 
 	auto exclamationLabel = new QPushButton(m_statusContainer);
 	exclamationLabel->setIcon(QIcon::fromTheme(":/swiot/warning.svg"));
