c83359685f8ec650b5d5bd43dca782d7d32d732e
AlexandraTrifan <Alexandra.Trifan@analog.com>
2023-06-28 17:04:46 +0300

    SwiotPingTask: Add a specific ping task for SWIOT, using the command queue.
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

---
+++ plugins/swiot/src/swiotpingtask.cpp
@@ -0,0 +1,49 @@
+#include "swiotpingtask.h"
+#include <QDebug>
+#include "src/swiot_logging_categories.h"
+#include <iioutil/commandqueueprovider.h>
+#include <iioutil/iiocommand/iiodevicegettrigger.h>
+
+using namespace scopy;
+using namespace scopy::swiot;
+SwiotPingTask::SwiotPingTask(iio_context *c ,QObject *parent) : QThread(parent), c(c) {}
+
+SwiotPingTask::~SwiotPingTask() {}
+
+void SwiotPingTask::run() {
+
+	enabled = true;
+	CommandQueue *commandQueue = CommandQueueProvider::GetInstance()->open(c);
+	if (!commandQueue) {
+		Q_EMIT pingFailed();
+		return;
+	}
+
+	auto dev = iio_context_find_device(c, "sw_trig");
+
+	if (dev) {
+		Command *getTriggerCommand = new IioDeviceGetTrigger(dev, nullptr);
+		connect(getTriggerCommand, &scopy::Command::finished, this, &SwiotPingTask::getTriggerCommandFinished, Qt::QueuedConnection);
+		commandQueue->enqueue(getTriggerCommand);
+	} else {
+		CommandQueueProvider::GetInstance()->close(c);
+	}
+}
+
+void SwiotPingTask::getTriggerCommandFinished(scopy::Command *cmd)
+{
+	IioDeviceGetTrigger *tcmd = dynamic_cast<IioDeviceGetTrigger*>(cmd);
+	if (!tcmd) {
+		CommandQueueProvider::GetInstance()->close(c);
+		return;
+	}
+	int ret = tcmd->getReturnCode();
+	if (tcmd->getReturnCode() >= 0 || tcmd->getReturnCode() == -ENOENT) {
+		Q_EMIT pingSuccess();
+	} else {
+		Q_EMIT pingFailed();
+	}
+	CommandQueueProvider::GetInstance()->close(c);
+}
+
+#include "moc_swiotpingtask.cpp"
