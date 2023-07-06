52be7023e524a23678035f111f79915faba7b48d
AlexandraTrifan <Alexandra.Trifan@analog.com>
2023-07-06 17:20:58 +0300

    BufferLogic: Fix clazy-range-loop-detach warning.
    Range loop might detach Qt container.
    
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- plugins/swiot/src/runtime/ad74413r/bufferlogic.cpp
+++ plugins/swiot/src/runtime/ad74413r/bufferlogic.cpp
@@ -186,7 +186,7 @@ QVector<QString> BufferLogic::getPlotChnlsUnitOfMeasure()
 {
 	QVector<QString> chnlsUnitOfMeasure;
 
-	for (ChnlInfo* chnl : m_chnlsInfo) {
+	for (ChnlInfo* chnl : qAsConst(m_chnlsInfo)) {
 		if (chnl->isScanElement() && !chnl->isOutput()) {
 			QString unitOfMeasure = chnl->unitOfMeasure();
 			chnlsUnitOfMeasure.push_back(unitOfMeasure);
@@ -198,7 +198,7 @@ QVector<QString> BufferLogic::getPlotChnlsUnitOfMeasure()
 QVector<std::pair<int, int>> BufferLogic::getPlotChnlsRangeValues()
 {
 	QVector<std::pair<int, int>> chnlsRangeValues;
-	for (ChnlInfo* chnl : m_chnlsInfo) {
+	for (ChnlInfo* chnl : qAsConst(m_chnlsInfo)) {
 		if (chnl->isScanElement() && !chnl->isOutput()) {
 			std::pair<int, int> rangeValues = chnl->rangeValues();
 			chnlsRangeValues.push_back(rangeValues);
