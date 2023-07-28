36fb02c69f053567bbc8b53c292254cb648461ff
Adrian Suciu <adrian.suciu@analog.com>
2023-07-28 18:48:11 +0300

    adc: implement Y-Mode - counts/percent/volts on Y-Axis and Y-Autoscale
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- gr-util/src/grtimechanneladdon.cpp
+++ gr-util/src/grtimechanneladdon.cpp
@@ -1,6 +1,5 @@
 #include "grtimechanneladdon.h"
 #include "grdeviceaddon.h"
-#include "griiofloatchannelsrc.h"
 #include <gui/stylehelper.h>
 #include <QDebug>
 #include <QVBoxLayout>
@@ -14,25 +13,22 @@
 #include <gui/widgets/menucollapsesection.h>
 #include <gui/widgets/menubigswitch.h>
 #include <gui/widgets/menulineedit.h>
+#include <QLoggingCategory>
 
+Q_LOGGING_CATEGORY(CAT_GR_TIME_CHANNEL, "GRTimeChannel");
 using namespace scopy::grutil;
 
 QWidget* GRTimeChannelAddon::createYAxisMenu(QWidget* parent) {
 	MenuSectionWidget *yaxiscontainer = new MenuSectionWidget(parent);
 	MenuCollapseSection *yaxis = new MenuCollapseSection("Y-AXIS",MenuCollapseSection::MHCW_NONE, yaxiscontainer);
-
 	MenuCombo *cbb = new MenuCombo("", yaxis);
 	auto cb = cbb->combo();
-	cb->addItem("ADC Counts", 1);
-	cb->addItem("% Full Scale", 1.0/(1<<12));
+	cb->addItem("ADC Counts", YMODE_COUNT);
+	cb->addItem("% Full Scale", YMODE_FS);
+
 	if(m_scaleAvailable) {
-		cb->addItem(m_unit, 2.2);
+		cb->addItem(m_unit, YMODE_SCALE);
 	}
-
-	connect(cb, qOverload<int>(&QComboBox::currentIndexChanged), this, [=](int idx) {
-		m_scOff->setScale(cb->itemData(idx).toDouble());
-
-	});
 
 	QWidget *startStop = new QWidget(yaxis);
 	QHBoxLayout *startStopLay = new QHBoxLayout(startStop);
@@ -40,53 +36,67 @@ QWidget* GRTimeChannelAddon::createYAxisMenu(QWidget* parent) {
 	startStopLay->setSpacing(10);
 	startStop->setLayout(startStopLay);
 
-	ScaleSpinButton *ymin = new ScaleSpinButton(
+	m_ymin = new ScaleSpinButton(
 		{
-		 {" ",1e0},
+		 {"V",1e0},
 		 {"k",1e3},
 		 {"M",1e6},
 		 {"G",1e9},
 		 },"YMin",(double)((long)(-1<<31)),(double)((long)1<<31),false,false,startStop);
-//	connect(ymin, &ScaleSpinButton::valueChanged, m_plotAxis, &PlotAxis::setMin);
-//	connect(m_plotAxis, &PlotAxis::minChanged, this, [=](){
-////		QSignalBlocker b(ymin);
-//		ymin->setValue(m_plotAxis->min());
-//	});
 
-	ScaleSpinButton *ymax = new ScaleSpinButton(
+
+	m_ymax = new ScaleSpinButton(
 	    {
-	     {" ",1e0},
+	     {"V",1e0},
 	     {"k",1e3},
 	     {"M",1e6},
 	     {"G",1e9},
 	     },"YMax",(double)((long)(-1<<31)),(double)((long)1<<31),false,false,startStop);
 
-//	connect(ymax, &ScaleSpinButton::valueChanged, m_plotAxis, &PlotAxis::setMax);
-//	connect(m_plotAxis, &PlotAxis::maxChanged, this, [=](){
-////		QSignalBlocker b(ymax);
-//		ymax->setValue(m_plotAxis->min());
-//	});
+	startStopLay->addWidget(m_ymin);
+	startStopLay->addWidget(m_ymax);
 
-	connect(ymin, &ScaleSpinButton::valueChanged, m_plotAxis, &PlotAxis::setMin);
-
-	ymin->setValue(-1.0);
-	ymax->setValue(1.0);
-
-	startStopLay->addWidget(ymin);
-	startStopLay->addWidget(ymax);
-
+	m_autoScaleTimer = new QTimer(this);
+	m_autoScaleTimer->setInterval(1000);
+	connect(m_autoScaleTimer, &QTimer::timeout, this, &GRTimeChannelAddon::autoscale);
 	MenuOnOffSwitch *autoscale = new MenuOnOffSwitch(tr("AUTOSCALE"), yaxis, false);
-	connect(autoscale->onOffswitch(), &QAbstractButton::toggled, this, [=](bool b){
-		ymin->setEnabled(!b);
-		ymax->setEnabled(!b);
-		m_plotAddon->plot()->plot()->setAxisAutoScale(m_plotAxis->axisId(), b);
-	} );
 
 	yaxis->add(startStop);
 	yaxis->add(autoscale);
 	yaxis->add(cbb);
 
 	yaxiscontainer->contentLayout()->addWidget(yaxis);
+
+	// Connects
+	connect(m_ymin, &ScaleSpinButton::valueChanged, m_plotAxis, &PlotAxis::setMin);
+	connect(m_plotAxis, &PlotAxis::minChanged, this, [=](){
+		QSignalBlocker b(m_ymin);
+		m_ymin->setValue(m_plotAxis->min());
+	});
+
+	connect(m_ymax, &ScaleSpinButton::valueChanged, m_plotAxis, &PlotAxis::setMax);
+	connect(m_plotAxis, &PlotAxis::maxChanged, this, [=](){
+		QSignalBlocker b(m_ymax);
+		m_ymax->setValue(m_plotAxis->max());
+	});
+
+	connect(autoscale->onOffswitch(), &QAbstractButton::toggled, this, [=](bool b){
+		m_ymin->setEnabled(!b);
+		m_ymax->setEnabled(!b);
+		m_autoscaleEnabled = b;
+		toggleAutoScale();
+
+	} );
+
+	connect(cb, qOverload<int>(&QComboBox::currentIndexChanged), this, [=](int idx) {
+		auto mode = cb->itemData(idx).toInt();
+		setYMode(static_cast<YMode>(mode));
+	});
+
+	// Defaults
+	m_ymin->setValue(-1.0);
+	m_ymax->setValue(1.0);
+	cb->setCurrentIndex(1);
 
 	return yaxiscontainer;
 }
@@ -158,6 +168,8 @@ QWidget* GRTimeChannelAddon::createMenu(QWidget* parent) {
 GRTimeChannelAddon::GRTimeChannelAddon(QString ch, GRDeviceAddon *dev, GRTimePlotAddon *plotAddon, QPen pen, QObject *parent)
     : QObject(parent), m_channelName(ch), m_dev(dev), m_plotAddon(plotAddon), m_pen(pen) {
 
+	m_running = false;
+	m_autoscaleEnabled = false;
 	m_signalPath = new GRSignalPath("time_" + dev->getName() + ch, this);
 	m_grch = new GRIIOFloatChannelSrc(dev->src(),ch,m_signalPath);
 	m_signalPath->append(m_grch);
@@ -176,20 +188,14 @@ GRTimeChannelAddon::GRTimeChannelAddon(QString ch, GRDeviceAddon *dev, GRTimePlo
 	auto plot = plotAddon->plot();;
 
 	m_plotAxis = new PlotAxis(QwtAxis::YLeft, plot, pen, this);
-	m_plotAxis->setInterval(-1<<11,1<<11);
-//	m_plotAxis->setInterval(0,1<<24);
 	m_plotCh = new PlotChannel(name, pen, plot, plot->xAxis(), m_plotAxis,this);
 	m_plotAxisHandle = new PlotAxisHandle(pen, m_plotAxis,plot,this);
 	m_plotCh->setHandle(m_plotAxisHandle);
 	plot->addPlotAxisHandle(m_plotAxisHandle);
 
-	// create menu widget
 	widget = createMenu();
 
 	// - create ADCCount/VFS/V
-	// - create VDIV scale
-
-	// - create thickness/render mode / Color (?)  / match to Curve
 }
 
 GRTimeChannelAddon::~GRTimeChannelAddon() {}
@@ -203,26 +209,102 @@ void GRTimeChannelAddon::setDevice(GRDeviceAddon *d) { m_dev = d; d->registerCha
 GRDeviceAddon* GRTimeChannelAddon::getDevice() { return m_dev;}
 
 void GRTimeChannelAddon::enable() {
-	qInfo()<<name<<" enabled";
-	m_plotCh->attach();
+	qInfo(CAT_GR_TIME_CHANNEL)<<name<<" enabled";
+	m_plotCh->enable();
 	m_plotAxisHandle->handle()->setVisible(true);
 	m_plotAxisHandle->handle()->raise();
 	m_signalPath->setEnabled(true);
 	m_grch->setEnabled(true);
+	m_plotAddon->plot()->replot();
 }
 
 
 void GRTimeChannelAddon::disable() {
-	qInfo()<<name<<" disabled";
-	m_plotCh->detach();
+	qInfo(CAT_GR_TIME_CHANNEL())<<name<<" disabled";
+	m_plotCh->disable();
 	m_plotAxisHandle->handle()->setVisible(false);
 	m_signalPath->setEnabled(false);
 	m_grch->setEnabled(false);
+	m_plotAddon->plot()->replot();
 }
 
-void GRTimeChannelAddon::onStart() {}
+void GRTimeChannelAddon::onStart() {
+	m_running = true;
+	toggleAutoScale();
+}
 
-void GRTimeChannelAddon::onStop() {}
+void GRTimeChannelAddon::onStop() {
+	m_running = false;
+	toggleAutoScale();
+}
+
+void GRTimeChannelAddon::toggleAutoScale() {
+	if(m_running && m_autoscaleEnabled) {
+		m_autoScaleTimer->start();
+	} else {
+		m_autoScaleTimer->stop();
+	}
+}
+
+void GRTimeChannelAddon::autoscale() {
+	double max = -1000000.0;
+	double min = 1000000.0;
+
+	auto data =  m_plotCh->curve()->data();
+	for(int i = 0; i < data->size(); i++) {
+		auto sample = data->sample(i).y();
+		if(max < sample) max = sample;
+		if(min > sample) min = sample;
+	}
+
+	qInfo(CAT_GR_TIME_CHANNEL)<<"Autoscaling channel " << m_channelName << "to (" <<min <<", " << max<<")";
+
+	m_plotAxis->setMin(min);
+	m_plotAxis->setMax(max);
+}
+
+void GRTimeChannelAddon::setYMode(YMode mode)
+{
+	double scale = 1;
+	double offset = 0;
+	double ymin = -1;
+	double ymax = 1;
+	const iio_data_format* fmt = m_grch->getFmt();
+
+	switch(mode) {
+	case YMODE_COUNT:
+		scale = 1;
+		if(fmt->is_signed) {
+			ymin = - 1<<(fmt->bits-1);
+			ymax =   1<<(fmt->bits-1);
+		} else {
+			ymin =   0;
+			ymax =   1<<(fmt->bits);
+		}
+		break;
+	case YMODE_FS:
+		scale = 1.0/((1<<fmt->bits));
+		if(fmt->is_signed) {
+			ymin = -0.5;
+			ymax = 0.5;
+		} else {
+			ymin = 0;
+			ymax = 1;
+		}
+		break;
+	case YMODE_SCALE:
+		break;
+	default:
+		break;
+
+	}
+	m_ymin->setValue(ymin);
+	m_ymax->setValue(ymax);
+	m_scOff->setScale(scale);
+	m_scOff->setOffset(offset);
+
+}
+
 
 void GRTimeChannelAddon::onAdd() {  }
 
