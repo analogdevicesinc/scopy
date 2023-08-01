384026fdfb17bdfac62ec12eacdec74cc57aecee
Adrian Suciu <adrian.suciu@analog.com>
2023-08-01 18:26:28 +0300

    adc: implement rollingmode, singleshot, plot tags
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- gr-util/src/grdeviceaddon.cpp
+++ gr-util/src/grdeviceaddon.cpp
@@ -1,4 +1,5 @@
 #include "grdeviceaddon.h"
+#include <QDebug>
 
 
 using namespace scopy::grutil;
@@ -31,6 +32,22 @@ void GRDeviceAddon::onStop() {}
 void GRDeviceAddon::onInit() {}
 
 void GRDeviceAddon::onDeinit() {}
+
+void GRDeviceAddon::preFlowStart() {
+	qInfo()<<"READ SAMPLE RATE";
+}
+
+void GRDeviceAddon::postFlowStart() {
+
+}
+
+void GRDeviceAddon::preFlowStop() {
+
+}
+
+void GRDeviceAddon::postFlowStop() {
+
+}
 
 void GRDeviceAddon::onChannelAdded(ToolAddon *) {}
 
