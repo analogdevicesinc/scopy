bae5d38779b1c1c3317525b81d676302f981050b
andrei.danila <andrei.danila@analog.com>
2023-08-21 15:50:18 +0300

    core/DeviceFactory: Manage the creation of DeviceImpl objects.
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

--- core/src/devicefactory.cpp
+++ core/src/devicefactory.cpp
@@ -1,9 +1,14 @@
 #include "devicefactory.h"
+#include "iiodeviceimpl.h"
 
 using namespace scopy;
 
 
 DeviceImpl *DeviceFactory::build(QString param, PluginManager *pm, QString category, QObject *parent)
 {
-	return new DeviceImpl(param, pm, category, parent);
+	if(category.compare("iio") == 0) {
+		return new IIODeviceImpl(param, pm, parent);
+	} else {
+		return new DeviceImpl(param, pm, category, parent);
+	}
 }
