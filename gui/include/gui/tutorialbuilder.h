2945d1a7d4cbefb3eed03170fcc42cec4c636d3f
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-09-05 14:30:26 +0300

    Tutorial: Changed the tutorial to use the HoverWidget functionality
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- gui/include/gui/tutorialbuilder.h
+++ gui/include/gui/tutorialbuilder.h
@@ -31,6 +31,15 @@ namespace scopy::gui {
 class SCOPY_GUI_EXPORT TutorialBuilder : public scopy::gui::TutorialOverlay {
 	Q_OBJECT
 public:
+	/**
+	 * @brief This acts as a wrapper to the scopy::gui::TutorialOverlay class that is meant to simplify adding
+	 * chapters in order.
+	 * @param topWidget The topmost widget that contains all the children widgets that will be part of the tutorial.
+	 * @param jsonFile A json file that describes each chapter and attributes related to it (e.g. description, order).
+	 * @param jsonEntry The entry in the jsonFile that represents the current tutorial.
+	 * @param parent The widget that will receive the tinted overlay meant to highlight tutorial subjects.
+	 * @inherit scopy::gui::TutorialOverlay
+	 * */
 	explicit TutorialBuilder(QWidget* topWidget, const QString& jsonFile, QString jsonEntry, QWidget* parent = nullptr);
 	explicit TutorialBuilder(QWidget* parent = nullptr);
 
@@ -45,25 +54,27 @@ public:
 
 	/**
 	 * @brief Collects and adds all qt objects that are part of the tutorial and are contained in the m_topWidget
-	 * widget (given as constructor parameter) widget recursively in the chapter list. The widgets are considered
+	 * widget (given as constructor parameter) recursively in the chapter list. The widgets are considered
 	 * part of the tutorial if their object name is in the m_jsonFile file under the m_jsonEntry section. After
 	 * they are collected, they are sorted based on the number read from the json file and added as chapters. The
 	 * chapter description will be matched and added from the same json file.
+	 * @throws std::runtime_error Thrown then no topWidget was provided.
 	 * @return The number of tutorial objects collected.
 	 * */
 	uint16_t collectChapters();
 
 	/**
-	 * @brief Opens the m_jsonFile and searches for the m_jsonEntry array object. After that it collects the index,
-	 * name, description and populates m_chapters with the specified attributes.
+	 * @brief Opens the m_jsonFile and searches for the m_jsonEntry array object. After that it collects the
+	 * specific attributes and populates m_chapters with the entries.
+	 * @throws std::runtime_error Thrown when there are issues opening or parsing the json file.
 	 * */
 	void readTutorialRequirements();
 
 public Q_SLOTS:
 	/**
- 	* @brief Calls collectChapters() and readTutorialRequirements() before calling the start() method from the
- 	* base class.
- 	* */
+ 	 * @brief Calls collectChapters() and readTutorialRequirements() before calling the start() method from the
+ 	 * base class.
+ 	 * */
 	void start() override;
 
 private:
@@ -72,20 +83,35 @@ private:
 	QWidget* m_topWidget;
 
 	struct ChapterInstructions {
-		QString name;
+		// map representing the widget property string related to the tutorial and the related widget
+		QMap<QString, QWidget*> widgets;
+
+		// this is a name of the widget from the map declared above, the description of the chapter will
+		// be anchored to this widget
+		QString mainWidget;
 		QString description;
-		QWidget* widget;
+		int x_offset;
+		int y_offset;
+		HoverPosition anchor;
+		HoverPosition content;
 	};
 
-	QMap<uint16_t, QList<ChapterInstructions>> m_chapters;
+	QMap<uint16_t, ChapterInstructions*> m_chapters;
 
 	/**
 	 * @brief Searches for the ChapterInstructions entries that have the name field matching the name parameter
 	 * @param name QString with the name of the ChapterInstructions that will be searched
 	 * @throws std::runtime_error Thrown when the name is not found in the m_chapters QMap
-	 * @return The list of references to the ChapterInstructions that were found with this name
+	 * @return The list of pointers to the ChapterInstructions that were found with this name
 	 * */
-	QList<std::reference_wrapper<TutorialBuilder::ChapterInstructions>> getChapterInstructionFromName(const QString& name);
+	QList<TutorialBuilder::ChapterInstructions*> getChapterInstructionFromName(const QString& name);
+
+	/**
+	 * @brief Converts a QString to the corresponding HoverPosition value (e.g. "HP_TOPLEFT" -> scopy::HoverPosition::HP_TOPLEFT)
+	 * @param position QString that matches an entry of HoverPosition
+	 * @return HoverPosition entry, if there is no match, the default value of CENTER will be returned
+	 * */
+	 static HoverPosition convertStringToHoverPosition(const QString& position) noexcept;
 };
 }
 
