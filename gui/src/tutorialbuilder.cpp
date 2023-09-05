858de86082ed1c4252635edddc8bd3ae31f64fef
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-09-05 14:30:26 +0300

    Tutorial: Changed the tutorial to use the HoverWidget functionality
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- gui/src/tutorialbuilder.cpp
+++ gui/src/tutorialbuilder.cpp
@@ -19,7 +19,6 @@
  */
 
 
-
 #include "tutorialbuilder.h"
 #include <utility>
 #include <exception>
@@ -80,17 +79,20 @@ void TutorialBuilder::setTopWidget(QWidget *topWidget) {
 }
 
 void TutorialBuilder::start() {
-	this->readTutorialRequirements();
-	uint16_t chaptersCollected = this->collectChapters();
-	qInfo(CAT_TUTORIALBUILDER) << "Chapters collected" << chaptersCollected;
+	try {
+		this->readTutorialRequirements();
+		uint16_t chaptersCollected = this->collectChapters();
+		qInfo(CAT_TUTORIALBUILDER) << "Chapters collected" << chaptersCollected;
 
-	TutorialOverlay::start();
+		TutorialOverlay::start();
+	} catch (std::runtime_error& error) {
+		qCritical(CAT_TUTORIALBUILDER) << "Cannot start tutorial:" << error.what();
+	}
 }
 
 uint16_t TutorialBuilder::collectChapters() {
 	if (!m_topWidget) {
-		qCritical(CAT_TUTORIALBUILDER) << "No top widget was provided. Aborting tutorial.";
-		return 0;
+		throw std::runtime_error("No top widget was provided.");
 	}
 
 	uint16_t chaptersCollected = 0; // number of objects that are part of the tutorial
@@ -105,9 +107,10 @@ uint16_t TutorialBuilder::collectChapters() {
 		// if the object has no property named "tutorial_name", then it is not part of the tutorial
 		if (!currentObjectName.isEmpty()) {
 			try {
-				QList<std::reference_wrapper<TutorialBuilder::ChapterInstructions>> chapters = this->getChapterInstructionFromName(currentObjectName);
+				// get the chapters that use the widget with the property name currentObjectName
+				QList<TutorialBuilder::ChapterInstructions*> chapters = this->getChapterInstructionFromName(currentObjectName);
 				for (auto chapter: chapters) {
-					chapter.get().widget = qobject_cast<QWidget*>(child);
+					chapter->widgets[currentObjectName] = qobject_cast<QWidget*>(child);
 				}
 				++chaptersCollected;
 			} catch (std::runtime_error &error) {
@@ -119,21 +122,17 @@ uint16_t TutorialBuilder::collectChapters() {
 	qDebug(CAT_TUTORIALBUILDER) << "Collected a total of" << totalChaptersCollected << "in the" << m_jsonEntry << "entry.";
 
 	// add the now sorted chapters into the tutorial overlay
-	for (const auto &chapterList: qAsConst(m_chapters)) {
-		// init an empty list for the chapter widgets, all wiglets should have the same description
-		QList<QWidget*> chapterWidgets;
-		QString description = "";
-		for (const auto &chapter: qAsConst(chapterList)) {
-			if (chapter.widget) {
-				chapterWidgets.push_back(chapter.widget);
-				description = chapter.description;
-			} else {
-				// no widget with this name was found, it will not be included in the tutorial and a warning will be issued
-				qWarning(CAT_TUTORIALBUILDER) << "No object with the name" << chapter.name << "was found. The tutorial will skip this chapter.";
-			}
-		}
-
-		this->addChapter(chapterWidgets, description);
+	for (const ChapterInstructions *chapter: qAsConst(m_chapters)) {
+		// if there is no mainWidget specified, the first widget from the list will be set to be the mainWidget
+		this->addChapter(
+			chapter->widgets.values(),
+			chapter->description,
+			(chapter->mainWidget.isNull()) ? chapter->widgets.first() : chapter->widgets[chapter->mainWidget],
+			chapter->x_offset,
+			chapter->y_offset,
+			chapter->anchor,
+			chapter->content
+		);
 	}
 
 	return chaptersCollected;
@@ -141,78 +140,118 @@ uint16_t TutorialBuilder::collectChapters() {
 
 void TutorialBuilder::readTutorialRequirements() {
 	// open and read the file
-	QString contents;
 	if (m_jsonFile.fileName().isEmpty()) {
-		qCritical(CAT_TUTORIALBUILDER) << "No file name was provided. Aborting tutorial.";
-		return;
+		throw std::runtime_error("No file name was provided.");
 	}
 
 	m_jsonFile.open(QIODevice::ReadOnly | QIODevice::Text);
 	if (!m_jsonFile.isOpen()) {
-		qCritical(CAT_TUTORIALBUILDER) << "File could not be opened (read): " << m_jsonFile.fileName();
-		return;
+		throw std::runtime_error("File could not be opened (read): " + m_jsonFile.fileName().toStdString());
 	} else {
 		qDebug(CAT_TUTORIALBUILDER) << "File opened (read): " << m_jsonFile.fileName();
 	}
-	contents = m_jsonFile.readAll();
+	QString contents = m_jsonFile.readAll();
 	m_jsonFile.close();
 
 	// try to parse the contents as a json
 	QJsonParseError parse_error{};
 	QJsonDocument document = QJsonDocument::fromJson(contents.toUtf8(), &parse_error);
 	if (document.isNull()) {
-		qCritical(CAT_TUTORIALBUILDER) << "Invalid json: " << parse_error.errorString();
-		return;
+		throw std::runtime_error("Invalid json: " + parse_error.errorString().toStdString());
 	}
 
 	// extract the values related to the m_jsonEntry mode
 	QJsonObject document_object = document.object();
 	QJsonValue value = document_object.value(m_jsonEntry);
 	if (value == QJsonValue::Undefined) {
-		qCritical(CAT_TUTORIALBUILDER) << "Invalid json: Could not extract value " << m_jsonEntry;
-		return;
+		throw std::runtime_error("Invalid json: Could not extract value " + m_jsonEntry.toStdString());
 	}
 
 	// turn the json values into an array of ChapterInstructions and save the data
 	// each element from the json array must have the following structure:
 	// {
 	//	"index": int
-	//	"name": string
+	//	"name": [string]
 	//	"description": string
+	//	"main_widget": string
+	//	"x_offset": int, optional
+	//	"y_offset": int, optional
+	//	"anchor": string, HoverPosition, optional
+	//	"content": string, HoverPosition, optional
 	// }
 	QJsonArray chapters_array = value.toArray();
 	for (QJsonValueRef item: chapters_array) {
 		QJsonObject item_object = item.toObject();
 		int index = item_object.value("index").toInt(-1);
-		QString name = item_object.value("name").toString();
+		QStringList names;
+		QJsonArray jsonNameArray = item_object.value("names").toArray();
+		for (auto name: jsonNameArray) {
+			names.push_back(name.toString());
+		}
 		QString description = item_object.value("description").toString();
+		QString mainWidget = item_object.value("main_widget").toString();
+		int x_offset = item_object.value("x_offset").toInt(0);
+		int y_offset = item_object.value("y_offset").toInt(0);
+		QString anchorString = item_object.value("anchor").toString();
+		QString contentString = item_object.value("content").toString();
+		HoverPosition anchor = convertStringToHoverPosition(anchorString);
+		HoverPosition content = convertStringToHoverPosition(contentString);
 
-		ChapterInstructions chapterInstructions;
-		chapterInstructions.name = name;
-		chapterInstructions.description = tr(description.toStdString().c_str());
-		chapterInstructions.widget = nullptr;
+		auto *chapterInstructions = new ChapterInstructions;
+		for (const auto& name: qAsConst(names)) {
+			chapterInstructions->widgets.insert(name, nullptr); // the widgets will be set later
+		}
+		chapterInstructions->mainWidget = mainWidget;
+		chapterInstructions->description = description;
+		chapterInstructions->x_offset = x_offset;
+		chapterInstructions->y_offset = y_offset;
+		chapterInstructions->anchor = anchor;
+		chapterInstructions->content = content;
 		if (m_chapters.contains(index)) {
-			m_chapters[index].append(chapterInstructions);
+			qWarning(CAT_TUTORIALBUILDER) << "There is already a chapter with the key" << index;
 		} else {
-			m_chapters.insert(index, {chapterInstructions});
+			m_chapters.insert(index, chapterInstructions);
 		}
 	}
 }
 
-QList<std::reference_wrapper<TutorialBuilder::ChapterInstructions>> TutorialBuilder::getChapterInstructionFromName(const QString& name) {
-	QList<std::reference_wrapper<TutorialBuilder::ChapterInstructions>> result;
-	for (auto& chapterList: m_chapters) {
-		for (auto& item: chapterList) {
-			if (item.name == name) {
-				result.push_back(item);
+QList<TutorialBuilder::ChapterInstructions*> TutorialBuilder::getChapterInstructionFromName(const QString& name) {
+	QList<TutorialBuilder::ChapterInstructions*> result;
+	for (auto& chapter: m_chapters) {
+		const QList<QString> keys = chapter->widgets.keys();
+		for (const auto& itemName: keys) {
+			if (itemName == name) {
+				result.push_back(chapter);
 			}
 		}
 	}
 
 	if (result.isEmpty()) {
-		throw std::runtime_error("Invalid name.");
+		throw std::runtime_error("No ChapterInstructions that need this name were found.");
 	} else {
 		return result;
+	}
+}
+
+scopy::HoverPosition TutorialBuilder::convertStringToHoverPosition(const QString &position) noexcept {
+	if (position == "HP_LEFT") {
+		return scopy::HoverPosition::HP_LEFT;
+	} else if (position == "HP_TOPLEFT") {
+		return scopy::HoverPosition::HP_TOPLEFT;
+	} else if (position == "HP_TOP") {
+		return scopy::HoverPosition::HP_TOP;
+	} else if (position == "HP_TOPRIGHT") {
+		return scopy::HoverPosition::HP_TOPRIGHT;
+	} else if (position == "HP_RIGHT") {
+		return scopy::HoverPosition::HP_RIGHT;
+	} else if (position == "HP_BOTTOMRIGHT") {
+		return scopy::HoverPosition::HP_BOTTOMRIGHT;
+	} else if (position == "HP_BOTTOM") {
+		return scopy::HoverPosition::HP_BOTTOM;
+	} else if (position == "HP_BOTTOMLEFT") {
+		return scopy::HoverPosition::HP_BOTTOMLEFT;
+	} else { // position == "HP_CENTER"
+		return scopy::HoverPosition::HP_CENTER;
 	}
 }
 
--- gui/src/tutorialchapter.cpp
+++ gui/src/tutorialchapter.cpp
@@ -1,6 +1,30 @@
+/*
+ * Copyright (c) 2023 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see https://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #include "tutorialchapter.h"
+#include <QLoggingCategory>
 
 using namespace scopy::gui;
+
+Q_LOGGING_CATEGORY(CAT_TUTORIALCHAPTER, "TutorialChapter")
 
 TutorialChapter::TutorialChapter(QObject *parent) : QObject(parent)
 {
@@ -12,13 +36,24 @@ TutorialChapter::~TutorialChapter()
 
 }
 
-TutorialChapter *TutorialChapter::build(QList<QWidget *> subjects, QString description, QObject *parent)
+TutorialChapter *TutorialChapter::build(const QList<QWidget *>& subjects,
+					const QString& description,
+					QWidget* mainSubject,
+					int x_offset,
+					int y_offset,
+					HoverPosition anchor,
+					HoverPosition content, QObject *parent)
 {
 	TutorialChapter *ch = new TutorialChapter(parent);
 	for(QWidget *subject : subjects) {
 		ch->addSubject(subject);
 	}
 	ch->setDescription(description);
+	ch->setMainSubject(mainSubject);
+	ch->setXOffset(x_offset);
+	ch->setYOffset(y_offset);
+	ch->setAnchor(anchor);
+	ch->setContent(content);
 	return ch;
 }
 
@@ -35,4 +70,47 @@ const QString &TutorialChapter::getDescription() const
 void TutorialChapter::setDescription(const QString &newDescription)
 {
 	description = newDescription;
+}
+
+QWidget *TutorialChapter::getMainSubject() const {
+	return mainSubject;
+}
+
+void TutorialChapter::setMainSubject(QWidget *newMainSubject) {
+	if (!subjects.contains(newMainSubject)) {
+		qWarning(CAT_TUTORIALCHAPTER) << "The new main subject is not part of the subject list.";
+	}
+	this->mainSubject = newMainSubject;
+}
+
+int TutorialChapter::getXOffset() const {
+	return x_offset;
+}
+
+void TutorialChapter::setXOffset(int xOffset) {
+	x_offset = xOffset;
+}
+
+int TutorialChapter::getYOffset() const {
+	return y_offset;
+}
+
+void TutorialChapter::setYOffset(int yOffset) {
+	y_offset = yOffset;
+}
+
+scopy::HoverPosition TutorialChapter::getAnchor() const {
+	return anchor;
+}
+
+void TutorialChapter::setAnchor(scopy::HoverPosition newAnchor) {
+	anchor = newAnchor;
+}
+
+scopy::HoverPosition TutorialChapter::getContent() const {
+	return content;
+}
+
+void TutorialChapter::setContent(scopy::HoverPosition newContent) {
+	content = newContent;
 }
