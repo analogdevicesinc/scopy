102dec9566e78fab399b72c8aaf179e6c44c879c
Bindea Cristian <cristian.bindea@analog.com>
2023-09-14 16:43:37 +0300

    ci/windows: create another Scopy.exe named Scopy-console.exe that has console output turned on
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- cmake/Modules/ScopyWindows.cmake
+++ cmake/Modules/ScopyWindows.cmake
@@ -6,3 +6,21 @@ set(__INCLUDED_SCOPY_WINDOWS_CMAKE TRUE)
 configure_file(windows/scopy.iss.cmakein ${CMAKE_CURRENT_BINARY_DIR}/windows/scopy.iss @ONLY)
 configure_file(windows/scopy-32.iss.cmakein ${CMAKE_CURRENT_BINARY_DIR}/windows/scopy-32.iss @ONLY)
 configure_file(windows/scopy-64.iss.cmakein ${CMAKE_CURRENT_BINARY_DIR}/windows/scopy-64.iss @ONLY)
+
+macro(duplicate_target ORIGINAL NEW)
+	get_target_property(original_libraries ${ORIGINAL} LINK_LIBRARIES)
+	get_target_property(original_includes ${ORIGINAL} INCLUDE_DIRECTORIES)
+	get_target_property(original_options ${ORIGINAL} COMPILE_OPTIONS)
+
+	add_executable(${NEW}
+		WIN32
+		${PROJECT_SOURCES}
+		${SCOPY_RESOURCES}
+		${SCOPY_DEPENDENCIES}
+		${SCOPY_WIN32_RESOURCES}
+	)
+
+	target_link_libraries(${NEW} PRIVATE ${original_libraries})
+	target_include_directories(${NEW} PRIVATE ${original_includes})
+	target_compile_options(${NEW} PRIVATE ${original_options})
+endmacro()
