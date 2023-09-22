6f79a2b0de84ba07b284a800cddad1ca071c594c
andrei.danila <andrei.danila@analog.com>
2023-09-22 09:21:57 +0300

    core: Added an api class for ScopyMainWindow.
    The api will serve to manage options from the command line and also for
    scopy JS mode.
    Fixed scopymainwindow.h spacing.
    
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

--- core/include/core/scopymainwindow.h
+++ core/include/core/scopymainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 
+#include "license_overlay.h"
 #include "scopyhomepage.h"
 #include "devicemanager.h"
 #include "scannediiocontextcollector.h"
@@ -20,50 +21,55 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class ScopyMainWindow; }
 QT_END_NAMESPACE
 namespace scopy {
+class ScopyMainWindow_API;
 class SCOPY_CORE_EXPORT ScopyMainWindow : public QMainWindow
 {
-    Q_OBJECT
+	friend class ScopyMainWindow_API;
+	Q_OBJECT
 
 public:
-    ScopyMainWindow(QWidget *parent = nullptr);
-    ~ScopyMainWindow();
-    void initAboutPage(PluginManager *pm = nullptr);
-    void initPreferencesPage(PluginManager *pm = nullptr);
-    void initPreferences();
-    void initTranslations();
-    void loadPluginsFromRepository(PluginRepository *pr = nullptr);
+	ScopyMainWindow(QWidget *parent = nullptr);
+	~ScopyMainWindow();
+	void initAboutPage(PluginManager *pm = nullptr);
+	void initPreferencesPage(PluginManager *pm = nullptr);
+	void initPreferences();
+	void initTranslations();
+	void loadPluginsFromRepository(PluginRepository *pr = nullptr);
 
 public Q_SLOTS:
-    void requestTools(QString id);
-    void addDeviceToUi(QString id, Device *d);
-    void removeDeviceFromUi(QString);
-    void save();
-    void load();
-    void save(QString file);
-    void load(QString file);
-    void handlePreferences(QString, QVariant);
+	void requestTools(QString id);
+	void addDeviceToUi(QString id, Device *d);
+	void removeDeviceFromUi(QString);
+	void save();
+	void load();
+	void save(QString file);
+	void load(QString file);
+	void handlePreferences(QString, QVariant);
 
 private:
-    ScopyAboutPage* about;
-    ScopyPreferencesPage* prefPage;
-    PluginRepository *pr;
-    ScopyHomePage *hp;
-    DeviceManager *dm;
-    Preferences *pref;
+	ScopyAboutPage* about;
+	ScopyPreferencesPage* prefPage;
+	PluginRepository *pr;
+	ScopyHomePage *hp;
+	DeviceManager *dm;
+	Preferences *pref;
 
-    CyclicalTask *scanCycle;
-    IIOScanTask *scanTask;
-    ScannedIIOContextCollector *scc;
-    ToolManager *toolman;
-    DetachedToolWindowManager *dtm;
+	CyclicalTask *scanCycle;
+	IIOScanTask *scanTask;
+	ScannedIIOContextCollector *scc;
+	ToolManager *toolman;
+	DetachedToolWindowManager *dtm;
 
-    Ui::ScopyMainWindow *ui;
+	LicenseOverlay *license = nullptr;
+	ScopyMainWindow_API *api;
+	Ui::ScopyMainWindow *ui;
 
-    void loadOpenGL();
-    void initPythonWIN32();
-    void loadDecoders();
+	void loadOpenGL();
+	void initPythonWIN32();
+	void loadDecoders();
+	void initApi();
 protected:
-    void closeEvent(QCloseEvent *event) override;
+	void closeEvent(QCloseEvent *event) override;
 };
 }
 #endif // SCOPYMAINWINDOW_H
---
+++ core/src/scopymainwindow_api.cpp
@@ -0,0 +1,165 @@
+#include "scopymainwindow_api.h"
+#include <pluginbase/scopyjs.h>
+#include "qapplication.h"
+using namespace scopy;
+
+Q_LOGGING_CATEGORY(CAT_SCOPY_API, "Scopy_API")
+
+
+ScopyMainWindow_API::ScopyMainWindow_API(ScopyMainWindow *w)
+	: ApiObject()
+	, m_w(w)
+{
+}
+
+ScopyMainWindow_API::~ScopyMainWindow_API()
+{
+}
+
+void ScopyMainWindow_API::acceptLicense()
+{
+	if (m_w->license) {
+		Q_EMIT m_w->license->getContinueBtn()->clicked();
+	}
+}
+
+QString ScopyMainWindow_API::addDevice(QString cat, QString uri)
+{
+	Q_ASSERT(m_w->dm != nullptr);
+	QString devID = "";
+	devID = m_w->dm->createDevice(cat, uri, false);
+	qInfo(CAT_SCOPY_API) << "Device with id " << devID << " has been created!";
+	return devID;
+}
+
+bool ScopyMainWindow_API::connectDevice(int idx)
+{
+	Q_ASSERT(m_w->dm != nullptr);
+	Device *dev = nullptr;
+	bool isConnected = false;
+	bool successfulConnection = false;
+	QList mapKeys = m_w->dm->map.keys();
+	std::sort(mapKeys.begin(), mapKeys.end(), sortByUUID);
+	if (idx < mapKeys.size()) {
+		dev = m_w->dm->map[mapKeys[idx]];
+	}
+	if (dev) {
+		isConnected = m_w->dm->connectedDev.contains(dev->id());
+		if (!isConnected) {
+			dev->connectDev();
+			successfulConnection = true;
+		} else {
+			qWarning(CAT_SCOPY_API) << "The device is already connected!";
+		}
+	} else {
+		qWarning(CAT_SCOPY_API) << "The device is not available!";
+	}
+	return successfulConnection;
+}
+
+bool ScopyMainWindow_API::connectDevice(QString devID)
+{
+	Q_ASSERT(m_w->dm != nullptr);
+	Device *dev = m_w->dm->getDevice(devID);
+	bool successfulConnection = false;
+	if (dev) {
+		bool isConnected = m_w->dm->connectedDev.contains(dev->id());
+		if (!isConnected) {
+			dev->connectDev();
+			successfulConnection = true;
+		} else {
+			qWarning(CAT_SCOPY_API) << "The device is already connected!";
+		}
+	} else {
+		qWarning(CAT_SCOPY_API) << "The device is not available!";
+	}
+	return successfulConnection;
+}
+
+bool ScopyMainWindow_API::disconnectDevice(QString devID)
+{
+	Q_ASSERT(m_w->dm != nullptr);
+	Device *dev = m_w->dm->getDevice(devID);
+	if (dev) {
+		dev->disconnectDev();
+	} else {
+		qWarning(CAT_SCOPY_API) << "Device with id " << devID << " is not available!";
+		return false;
+	}
+	return true;
+}
+
+bool ScopyMainWindow_API::disconnectDevice()
+{
+	Q_ASSERT(m_w->dm != nullptr);
+	QString devID = "";
+	if (!m_w->dm->connectedDev.isEmpty()) {
+		devID = m_w->dm->connectedDev.back();
+	}
+	Device *dev = m_w->dm->getDevice(devID);
+	if (dev) {
+		dev->disconnectDev();
+	} else {
+		qWarning(CAT_SCOPY_API) << "Device with id " << devID << " is not available!";
+		return false;
+	}
+	return true;
+}
+
+void ScopyMainWindow_API::switchTool(QString devID, QString toolName)
+{
+	Q_ASSERT(m_w->dm != nullptr);
+	Device *dev = m_w->dm->getDevice(devID);
+	if (dev) {
+		ToolMenuEntry *tool = ToolMenuEntry::findToolMenuEntryByName(dev->toolList(), toolName);
+		if (!tool) {
+			qWarning(CAT_SCOPY_API) << "Tool " << toolName << " doesn't exist for " << dev->displayName();
+			return;
+		}
+		Q_EMIT m_w->dm->requestTool(tool->uuid());
+	}
+	else {
+		qWarning(CAT_SCOPY_API) << "Device with id " << devID << " is not available!";
+	}
+}
+
+void ScopyMainWindow_API::switchTool(QString toolName)
+{
+	Q_ASSERT(m_w->dm != nullptr);
+	QString devID = "";
+	if (!m_w->dm->connectedDev.isEmpty()) {
+		devID = m_w->dm->connectedDev.back();
+	}
+	Device *dev = m_w->dm->getDevice(devID);
+	if (dev) {
+		ToolMenuEntry *tool = ToolMenuEntry::findToolMenuEntryByName(dev->toolList(), toolName);
+		if (!tool) {
+			qWarning(CAT_SCOPY_API) << "Tool " << toolName << " doesn't exist for " << dev->displayName();
+			return;
+		}
+		Q_EMIT m_w->dm->requestTool(tool->uuid());
+	}
+	else {
+		qWarning(CAT_SCOPY_API) << "Device with id " << devID << " is not available!";
+	}
+}
+
+void ScopyMainWindow_API::runScript(QString content, QString fileName)
+{
+	QJSValue val = ScopyJS::GetInstance()->engine()->evaluate(content, fileName);
+	int ret = EXIT_SUCCESS;
+	if (val.isError()) {
+		qWarning(CAT_SCOPY_API) << "Exception:" << val.toString();
+		ret = EXIT_FAILURE;
+	} else if (!val.isUndefined()) {
+		qWarning(CAT_SCOPY_API) << val.toString();
+	}
+
+	/* Exit application */
+	qApp->exit(ret);
+}
+
+bool ScopyMainWindow_API::sortByUUID(const QString &k1, const QString &k2)
+{
+	return k1.split("_").last() < k2.split("_").last();
+}
