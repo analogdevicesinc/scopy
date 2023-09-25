3a95ee9874734bc11bfddc1a1892842412da0470
Adrian Suciu <adrian.suciu@analog.com>
2023-09-25 15:26:55 +0300

    general: added clang-format
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- cmake/Modules/ScopyTranslation.cmake
+++ cmake/Modules/ScopyTranslation.cmake
@@ -1,11 +1,14 @@
 if(DEFINED __INCLUDED_SCOPY_TRANSLATIONS_CMAKE)
-    return()
+	return()
 endif()
 set(__INCLUDED_SCOPY_TRANSLATIONS_CMAKE TRUE)
 
 function(generate_translations)
-	FILE(GLOB TS_FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/translations/*.ts)
-	set_source_files_properties(${TS_FILES} PROPERTIES OUTPUT_LOCATION ${CMAKE_BINARY_DIR}/translations MACOSX_PACKAGE_LOCATION ${CMAKE_BINARY_DIR}/translations)
+	file(GLOB TS_FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/translations/*.ts)
+	set_source_files_properties(
+		${TS_FILES} PROPERTIES OUTPUT_LOCATION ${CMAKE_BINARY_DIR}/translations
+				       MACOSX_PACKAGE_LOCATION ${CMAKE_BINARY_DIR}/translations
+	)
 	qt_add_translation(QM_FILES ${TS_FILES})
 
 	set(TRANSLATIONS)
@@ -14,8 +17,8 @@ function(generate_translations)
 		set(TRANSLATIONS "${TRANSLATIONS}\n<file>${CMAKE_BINARY_DIR}/translations/${file_name}.qm</file>")
 	endforeach()
 
-	configure_file(${CMAKE_CURRENT_SOURCE_DIR}/resources/translations.qrc
-		${CMAKE_CURRENT_BINARY_DIR}/translations.qrc
-		@ONLY)
+	configure_file(
+		${CMAKE_CURRENT_SOURCE_DIR}/resources/translations.qrc ${CMAKE_CURRENT_BINARY_DIR}/translations.qrc
+		@ONLY
+	)
 endfunction()
-
