9705e63dbd935debee635c5a414f894c71bee639
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-09-27 13:08:54 +0300

    ScopyHomeInfoPage: added "Report a bug" link
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- core/include/core/scopyhomeinfopage.h
+++ core/include/core/scopyhomeinfopage.h
@@ -2,22 +2,28 @@
 #define SCOPYHOMEINFOPAGE_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <hoverwidget.h>
 #include "scopy-core_export.h"
 
 namespace Ui {
 class ScopyHomeInfoPage;
 }
 
+namespace scopy {
 class SCOPY_CORE_EXPORT ScopyHomeInfoPage : public QWidget
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
-    explicit ScopyHomeInfoPage(QWidget *parent = nullptr);
-    ~ScopyHomeInfoPage();
+	explicit ScopyHomeInfoPage(QWidget *parent = nullptr);
+	~ScopyHomeInfoPage();
 
 private:
-    Ui::ScopyHomeInfoPage *ui;
+	Ui::ScopyHomeInfoPage *ui;
+
+	void initReportButton();
 };
+}
 
 #endif // SCOPYHOMEINFOPAGE_H
