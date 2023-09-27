5e6f71ee8c47400f8ad1ca158e3e65cf37107614
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-09-27 13:08:54 +0300

    ScopyHomeInfoPage: added "Report a bug" link
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- core/src/scopyhomeinfopage.cpp
+++ core/src/scopyhomeinfopage.cpp
@@ -1,16 +1,39 @@
 #include "scopyhomeinfopage.h"
+#include <stylehelper.h>
+#include <QDesktopServices>
 #include "ui_scopyhomeinfopage.h"
 
+using namespace scopy;
+
 ScopyHomeInfoPage::ScopyHomeInfoPage(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::ScopyHomeInfoPage)
+	QWidget(parent),
+	ui(new Ui::ScopyHomeInfoPage)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+//	initReportButton();
 }
 
 ScopyHomeInfoPage::~ScopyHomeInfoPage()
 {
-    delete ui;
+	delete ui;
+}
+
+void ScopyHomeInfoPage::initReportButton() {
+	auto reportButton = new QPushButton("Report a bug");
+	StyleHelper::BlueButton(reportButton, "reportButton");
+	reportButton->setFixedSize(100, 40);
+
+	auto reportBtnHoverWidget = new HoverWidget(reportButton, ui->textBrowser, this);
+	reportBtnHoverWidget->setContentPos(HP_TOPLEFT);
+	reportBtnHoverWidget->setAnchorPos(HP_BOTTOMRIGHT);
+	reportBtnHoverWidget->setAnchorOffset(QPoint(-10, -10));
+	reportBtnHoverWidget->setVisible(true);
+	reportBtnHoverWidget->raise();
+
+	connect(reportButton, &QPushButton::clicked, [] () {
+		const QUrl url("https://wiki.analog.com/university/tools/m2k/scopy/report");
+		QDesktopServices::openUrl(url);
+	});
 }
 
 #include "moc_scopyhomeinfopage.cpp"
