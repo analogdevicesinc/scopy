b5499ac3c91be7c3baa195f4d7b1e2cd53d73675
andrei.danila <andrei.danila@analog.com>
2023-10-05 17:04:45 +0300

    core: Created IioTabWidget class.
    I removed the elements related to iio devices from the ScopyHomeAddPage
    and inserted them into a new widget.
    
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

--- core/include/core/scopyhomeaddpage.h
+++ core/include/core/scopyhomeaddpage.h
@@ -9,6 +9,7 @@
 #include "scopy-core_export.h"
 #include <QFuture>
 #include "emuwidget.h"
+#include "iiotabwidget.h"
 #include <iio.h>
 
 namespace Ui {
@@ -27,50 +28,36 @@ public:
 Q_SIGNALS:
 	void requestAddDevice(QString, QString);
 	void requestDevice(QString);
-	void newDeviceAvailable(DeviceImpl *d);
-	void deviceInfoAvailable(QMap<QString, QString> ctxInfo);
-	void uriChanged(QString uri);
+	void newDeviceAvailable(scopy::DeviceImpl *d);
+	void verifyFinished(bool valid);
 
-protected:
-	void showEvent(QShowEvent *event) override;
 private Q_SLOTS:
-	void futureVerify();
-	void futureScan();
-	bool verify();
+	void futureVerify(QString uri, QString cat);
 	void deviceAddedToUi(QString);
-	void scanFinished();
-	void verifyFinished();
+	void onVerifyFinished();
 	void deviceLoaderInitialized();
-	void updateUri(QString uri);
 	void addBtnClicked();
+	void backBtnClicked();
 	void onEmuDeviceAvailable(QString uri);
 
 private:
+	void addTabs();
 	void initAddPage();
 	void initSubSections();
-	void findAvailableSerialPorts();
-	QMap<QString, QString> readContextAttributes(struct iio_context *ctx);
-	void verifyIioBackend();
-	void createDevice(QString cat = "");
+	void loadDeviceInfoPage();
+	void initializeDevice();
 	void removePluginsCheckBoxes();
-	QString getSerialPath();
-	void createBackEndCheckBox(QString backEnd);
-	void addScanFeedbackMsg(QString message);
 
 	Ui::ScopyHomeAddPage *ui;
 	EmuWidget *emuWidget;
+	IioTabWidget *iioTabWidget;
 	QString pendingUri;
 	QFutureWatcher<bool> *fw;
-	QFutureWatcher<int> *fwScan;
 
-	QStringList scanParamsList;
-	QStringList scanList;
 	InfoPage *deviceInfoPage;
 	PluginManager *pluginManager;
 	DeviceImpl *deviceImpl;
 	QList<PluginEnableWidget*> pluginDescriptionList;
-
-	const QVector<unsigned int> availableBaudRates = {2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 230400, 460800, 921600};
 };
 }
 #endif // SCOPYHOMEADDPAGE_H
