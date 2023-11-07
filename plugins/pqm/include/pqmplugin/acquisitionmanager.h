83b1b9603d732bb0fe469333e6920e389621cbf1
andrei.danila <andrei.danila@analog.com>
2023-11-07 11:50:58 +0200

    plugins: Implemented the power quality monitor(PQM) plugin.
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

---
+++ plugins/pqm/include/pqmplugin/acquisitionmanager.h
@@ -0,0 +1,58 @@
+#ifndef ACQUISITIONMANAGER_H
+#define ACQUISITIONMANAGER_H
+
+#include "qtimer.h"
+#include <iio.h>
+#include <QObject>
+#include <QMap>
+#include <QFutureWatcher>
+
+#include <iioutil/commandqueue.h>
+
+#define MAX_ATTR_SIZE 1024
+#define BUFFER_SIZE 1024
+#define DEVICE_PQM "pqm"
+#define SWAP_UINT32(x) (((x) >> 24) | (((x)&0x00FF0000) >> 8) | (((x)&0x0000FF00) << 8) | ((x) << 24))
+namespace scopy::pqm {
+class AcquisitionManager : public QObject
+{
+	Q_OBJECT
+public:
+	AcquisitionManager(iio_context *ctx, QObject *parent = nullptr);
+	~AcquisitionManager();
+
+public Q_SLOTS:
+	void toolEnabled(bool en, QString toolName);
+	void setConfigAttr(QMap<QString, QMap<QString, QString>>);
+Q_SIGNALS:
+	void pqmAttrsAvailable(QMap<QString, QMap<QString, QString>>);
+	void bufferDataAvailable(QMap<QString, std::vector<double>>);
+
+private Q_SLOTS:
+	void futureReadData();
+	void onReadFinished();
+
+private:
+	void enableBufferChnls(iio_device *dev);
+	void readData();
+	void readPqmAttributes();
+	void readBufferedData();
+	void setData(QMap<QString, QMap<QString, QString>>);
+
+	iio_context *m_ctx;
+	iio_buffer *m_buffer;
+	QTimer *m_dataRefreshTimer;
+	QFutureWatcher<void> *m_readFw;
+	QFutureWatcher<void> *m_setFw;
+
+	QVector<QString> m_chnlsName;
+	QMap<QString, QMap<QString, QString>> m_pqmAttr;
+	QMap<QString, std::vector<double>> m_bufferData;
+	QMap<QString, bool> m_tools = {{"rms", false}, {"harmonics", false}, {"waveform", false}, {"settings", false}};
+
+	bool m_attrHaveBeenRead = false;
+	bool m_buffHaveBeenRead = false;
+};
+} // namespace scopy::pqm
+
+#endif // ACQUISITIONMANAGER_H
