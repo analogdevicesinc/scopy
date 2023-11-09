6ba45a97e3a6bb637cdec45af8e5588a9862be4a
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2023-11-09 15:40:14 +0200

    SmallProgressBar: added SmallProgressBar class for displaying progress
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

---
+++ gui/include/gui/widgets/smallprogressbar.h
@@ -0,0 +1,28 @@
+#ifndef SCOPY_SMALLPROGRESSBAR_H
+#define SCOPY_SMALLPROGRESSBAR_H
+
+#include <QWidget>
+#include <QTimer>
+#include <QProgressBar>
+#include "scopy-gui_export.h"
+
+namespace scopy {
+class SCOPY_GUI_EXPORT SmallProgressBar : public QProgressBar {
+	Q_OBJECT
+public:
+	explicit SmallProgressBar(QWidget *parent = nullptr);
+
+
+public Q_SLOTS:
+	void startProgress(int progressDurationMs = 1000, int steps = 10); // time is 1 second
+
+Q_SIGNALS:
+	void progressFinished();
+
+private:
+	QTimer *m_timer;
+	int m_increment;
+};
+}
+
+#endif // SCOPY_SMALLPROGRESSBAR_H
