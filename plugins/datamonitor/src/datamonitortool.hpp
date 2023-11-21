fdaa041d99a8c06c76466451451fa8939373bf09
Ionut Muthi <ionut.muthi@analog.com>
2023-11-21 03:07:19 -0500

    datamonitor: create DataMonitorPlugin
    Signed-off-by: Ionut Muthi <ionut.muthi@analog.com>

---
+++ plugins/datamonitor/src/datamonitortool.hpp
@@ -0,0 +1,43 @@
+#ifndef DATAMONITORTOOL_HPP
+#define DATAMONITORTOOL_HPP
+
+#include <QPushButton>
+#include <QWidget>
+#include <tooltemplate.h>
+#include <toolbuttons.h>
+#include <QMap>
+#include <QScrollArea>
+
+namespace scopy {
+
+class CollapsableMenuControlButton;
+
+class FlexGridLayout;
+namespace datamonitor {
+
+class DataMonitorTool : public QWidget
+{
+	Q_OBJECT
+public:
+	explicit DataMonitorTool(QWidget *parent = nullptr);
+
+	RunBtn *getRunButton() const;
+
+	void initDataMonitor();
+
+private:
+	ToolTemplate *tool;
+	QPushButton *openLatMenuBtn;
+	GearBtn *settingsButton;
+	InfoBtn *infoBtn;
+	RunBtn *runBtn;
+	SingleShotBtn *singleBtn;
+
+	FlexGridLayout *m_flexGridLayout;
+	QScrollArea *m_scrollArea;
+
+	QButtonGroup *grp;
+};
+} // namespace datamonitor
+} // namespace scopy
+#endif // DATAMONITORTOOL_HPP
