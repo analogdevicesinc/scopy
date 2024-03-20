236340acad133c40c3f54e35e9b12785f97d01ea
AlexandraTrifan <Alexandra.Trifan@analog.com>
2024-03-20 12:01:28 +0200

    m2kplugin: close all libm2k contexts when disconnecting the M2K from Scopy
    Fix the following scenario in the m2kplugin using libm2kv0.8.0: connect,
    disconnect, connect again - fails due to improper libm2k context deletion.
    See PR #1553 for more details on this issue.
    
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- plugins/m2k/src/m2kcontroller.cpp
+++ plugins/m2k/src/m2kcontroller.cpp
@@ -77,7 +77,7 @@ void M2kController::disconnectM2k()
 		if(identifyTask && identifyTask->isRunning()) {
 			identifyTask->requestInterruption();
 		}
-		contextClose(m_m2k, true);
+		contextCloseAll();
 	} catch(std::exception &ex) {
 		qDebug(CAT_M2KPLUGIN) << ex.what();
 	}
