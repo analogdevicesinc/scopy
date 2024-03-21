729ab5b82b1c82d34dbd3125598c2c972352aeb0
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2024-03-21 14:34:45 +0200

    iiodebugplugin/DetaisView: Add class for selecting between different item views
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- plugins/iiodebugplugin/include/iiodebugplugin/detailsview.h
+++ plugins/iiodebugplugin/include/iiodebugplugin/detailsview.h
@@ -2,23 +2,32 @@
 #define SCOPY_DETAILSVIEW_H
 
 #include <QWidget>
-#include <QLabel>
-#include "iiostandarditem.h"
+#include <QTabWidget>
+#include <QTextBrowser>
 #include <gui/subsection_separator.hpp>
+#include "iiostandarditem.h"
+#include "guidetailsview.h"
+#include "clidetailsview.h"
 
 namespace scopy::iiodebugplugin {
 class DetailsView : public QWidget
 {
 	Q_OBJECT
 public:
-	explicit DetailsView(QWidget *parent);
-
-	void setTitle(QString title);
+	explicit DetailsView(QWidget *parent = nullptr);
 	void setIIOStandardItem(IIOStandardItem *item);
+	void refreshIIOView();
 
 private:
+	void setupUi();
+
+	IIOStandardItem *m_currentItem;
+	GuiDetailsView *m_guiDetailsView;
+	CliDetailsView *m_cliDetailsView;
+	QTabWidget *m_tabWidget;
+	QWidget *m_guiView;
+	QWidget *m_iioView;
 	QLabel *m_titleLabel;
-	gui::SubsectionSeparator *m_topSeparator;
 };
 } // namespace scopy::iiodebugplugin
 
