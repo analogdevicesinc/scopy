04316a4ba26df749f02c496104034f35dfb49012
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2024-03-21 14:41:22 +0200

    iiodebugplugin/IIODebugInstrument: Modify instrument and plugin for better layout and state saving
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- plugins/iiodebugplugin/include/iiodebugplugin/iiodebugplugin.h
+++ plugins/iiodebugplugin/include/iiodebugplugin/iiodebugplugin.h
@@ -7,6 +7,7 @@
 #include <QObject>
 #include <pluginbase/plugin.h>
 #include <pluginbase/pluginbase.h>
+#include "iiodebuginstrument.h"
 
 namespace scopy::iiodebugplugin {
 class SCOPY_IIODEBUGPLUGIN_EXPORT IIODebugPlugin : public QObject, public PluginBase
@@ -15,6 +16,7 @@ class SCOPY_IIODEBUGPLUGIN_EXPORT IIODebugPlugin : public QObject, public Plugin
 	SCOPY_PLUGIN;
 
 public:
+	friend class IIODebugPlugin_API;
 	bool compatible(QString m_param, QString category) override;
 	bool loadPage() override;
 	bool loadIcon() override;
@@ -22,10 +24,40 @@ public:
 	void unload() override;
 	void initMetadata() override;
 	QString description() override;
+	QString version() override;
+	void saveSettings(QSettings &) override;
+	void loadSettings(QSettings &) override;
 
 public Q_SLOTS:
 	bool onConnect() override;
 	bool onDisconnect() override;
+
+private:
+	ApiObject *m_pluginApi;
+	IIODebugInstrument *m_iioDebugger;
 };
+
+class SCOPY_IIODEBUGPLUGIN_EXPORT IIODebugPlugin_API : public ApiObject
+{
+	Q_OBJECT
+public:
+	explicit IIODebugPlugin_API(IIODebugPlugin *p)
+		: ApiObject(p)
+		, p(p)
+	{}
+	~IIODebugPlugin_API(){};
+	IIODebugPlugin *p;
+
+	Q_PROPERTY(QString debugTest READ debugTest WRITE setDebugTest NOTIFY debugTestChanged FINAL)
+	QString debugTest() const;
+	void setDebugTest(const QString &newDebugTest);
+
+Q_SIGNALS:
+	void debugTestChanged();
+
+private:
+	QString m_debugTest;
+};
+
 } // namespace scopy::iiodebugplugin
 #endif // IIODEBUGPLUGIN_H
