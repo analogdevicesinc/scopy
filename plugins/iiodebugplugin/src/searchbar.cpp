3c5325d5bdd71535c69556b54a9f48b3537a7d2f
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2024-03-21 14:37:45 +0200

    iiodebugplugin/SearchBar: Add SearchBar with autocomplete
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- plugins/iiodebugplugin/src/searchbar.cpp
+++ plugins/iiodebugplugin/src/searchbar.cpp
@@ -1,14 +1,17 @@
 #include "searchbar.h"
 #include <QHBoxLayout>
+#include "stylehelper.h"
 
 using namespace scopy::iiodebugplugin;
 
-SearchBar::SearchBar(const QStringList &options, QWidget *parent)
+SearchBar::SearchBar(QSet<QString> options, QWidget *parent)
 	: QWidget(parent)
 	, m_lineEdit(new QLineEdit(this))
 	, m_label(new QLabel("Filter", this))
-	, m_completer(new QCompleter(options, this))
+	, m_completer(new QCompleter(options.values(), this))
 {
+	StyleHelper::BackgroundPage(this, "SearchBar");
+
 	m_completer->setCaseSensitivity(Qt::CaseInsensitive);
 	m_completer->setFilterMode(Qt::MatchContains);
 	m_lineEdit->setCompleter(m_completer);
@@ -16,8 +19,6 @@ SearchBar::SearchBar(const QStringList &options, QWidget *parent)
 	setLayout(new QHBoxLayout(this));
 	layout()->addWidget(m_label);
 	layout()->addWidget(m_lineEdit);
-	setStyleSheet("color: white; background-color: #272730");
-	m_label->setStyleSheet("color: white; background-color: #272730");
 }
 
 QLineEdit *SearchBar::getLineEdit() { return m_lineEdit; }
