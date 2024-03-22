5fafa8ebe118e8f3cc88a584ee585b4bd59605e6
Adrian Suciu <adrian.suciu@analog.com>
2024-03-22 17:55:51 +0200

    tools: format.sh skip build folder
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- tools/format.sh
+++ tools/format.sh
@@ -2,18 +2,19 @@
 set -x
 
 SCOPY_REPO=$(git rev-parse --show-toplevel)
+SKIP_BUILD="-not -path ./build/*"
 
 IN_PLACE="-i"
 VERBOSE="--verbose"
 CLANG_FORMAT="clang-format-12"
 CMAKE_FORMAT="cmake-format"
 
-find $SCOPY_REPO -type f -name "*.cpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
-find $SCOPY_REPO -type f -name "*.cc" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
-find $SCOPY_REPO -type f -name "*.hpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
-find $SCOPY_REPO -type f -name "*.h" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
-find $SCOPY_REPO -type f -name "CMakeLists.txt" | xargs $CMAKE_FORMAT $IN_PLACE
-find $SCOPY_REPO -type f -name "*.cmake" | xargs $CMAKE_FORMAT $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cc" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.hpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.h" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "CMakeLists.txt" | xargs $CMAKE_FORMAT $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cmake" | xargs $CMAKE_FORMAT $IN_PLACE
 pushd $SCOPY_REPO/tools
 ./includemocs.sh
 popd
