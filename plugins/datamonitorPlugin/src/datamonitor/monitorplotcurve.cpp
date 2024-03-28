e8b2dd296e5baa487343b4b5cfbabcb14cc98485
Ionut Muthi <ionut.muthi@analog.com>
2024-03-28 11:16:51 +0200

    datamonitor: replace curve style menu with the one in gui
    Signed-off-by: Ionut Muthi <ionut.muthi@analog.com>

--- plugins/datamonitorPlugin/src/datamonitor/monitorplotcurve.cpp
+++ plugins/datamonitorPlugin/src/datamonitor/monitorplotcurve.cpp
@@ -9,7 +9,6 @@ using namespace datamonitor;
 MonitorPlotCurve::MonitorPlotCurve(DataMonitorModel *dataMonitorModel, PlotWidget *plot, QObject *parent)
 	: m_dataMonitorModel(dataMonitorModel)
 {
-	m_curveStyleIndex = 0;
 	QPen chpen = QPen(dataMonitorModel->getColor(), 1);
 
 	m_plotch = new PlotChannel(dataMonitorModel->getName(), chpen, plot, plot->xAxis(), plot->yAxis(), this);
@@ -26,35 +25,6 @@ MonitorPlotCurve::MonitorPlotCurve(DataMonitorModel *dataMonitorModel, PlotWidge
 	});
 }
 
-void MonitorPlotCurve::changeCurveStyle(int style)
-{
-	m_curveStyleIndex = style;
-	m_plotch->curve()->setPaintAttribute(QwtPlotCurve::ClipPolygons, true);
-	m_plotch->curve()->setCurveAttribute(QwtPlotCurve::Fitted, false);
-
-	switch(style) {
-	case 0:
-		m_plotch->curve()->setStyle(QwtPlotCurve::CurveStyle::Lines);
-		break;
-	case 1:
-		m_plotch->curve()->setStyle(QwtPlotCurve::CurveStyle::Dots);
-		break;
-	case 2:
-		m_plotch->curve()->setStyle(QwtPlotCurve::CurveStyle::Steps);
-		break;
-	case 3:
-		m_plotch->curve()->setStyle(QwtPlotCurve::CurveStyle::Sticks);
-		break;
-	case 4:
-		m_plotch->curve()->setPaintAttribute(QwtPlotCurve::ClipPolygons, false);
-		m_plotch->curve()->setCurveAttribute(QwtPlotCurve::Fitted, true);
-		m_plotch->curve()->setStyle(QwtPlotCurve::CurveStyle::Lines);
-		break;
-	}
-}
-
-void MonitorPlotCurve::changeCurveThickness(double thickness) { m_plotch->setThickness(thickness); }
-
 void MonitorPlotCurve::clearCurveData()
 {
 	m_plotch->curve()->setRawSamples(m_dataMonitorModel->getXdata()->data(), m_dataMonitorModel->getYdata()->data(),
@@ -70,8 +40,6 @@ void MonitorPlotCurve::refreshCurve()
 void MonitorPlotCurve::toggleActive(bool toggled) { m_plotch->setEnabled(toggled); }
 
 PlotChannel *MonitorPlotCurve::plotch() const { return m_plotch; }
-
-int MonitorPlotCurve::curveStyleIndex() const { return m_curveStyleIndex; }
 
 double MonitorPlotCurve::curveMinVal() { return m_dataMonitorModel->minValue(); }
 
