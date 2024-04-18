42de6d00c24e3a9162f00d72d460016a8e8416cd
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2024-04-18 15:11:30 +0300

    iio-widgets/RangeGuiStrategy: Add safety checks for wrong data inputs
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- gui/include/gui/widgets/titlespinbox.h
+++ gui/include/gui/widgets/titlespinbox.h
@@ -3,7 +3,7 @@
 
 #include <QLabel>
 #include <QPushButton>
-#include <QSpinBox>
+#include <QLineEdit>
 #include <QWidget>
 #include "scopy-gui_export.h"
 
@@ -19,13 +19,43 @@ public:
 	void setTitle(QString title);
 	QPushButton *getSpinBoxUpButton();
 	QPushButton *getSpinBoxDownButton();
-	QSpinBox *getSpinBox();
+	QLineEdit *getLineEdit();
+
+	double step() const;
+	void setStep(double newStep);
+
+	double max() const;
+	void setMax(double newMax);
+
+	double min() const;
+	void setMin(double newMin);
+
+	void setValue(double newValue);
+	void setSpinButtonsDisabled(bool isDisabled);
 
 private:
+	/**
+	 * @brief truncValue This function is needed because the QString::number function that
+	 * would be normally used to parse a QString to a number does not work well with large
+	 * numbers that end with multiple zeroes. The QString returned will be in scientific
+	 * notation. The alternative is that we can set a number of fixed decimal points, but
+	 * it would look awkward so this function converts it to a QString and chops the
+	 * trailing zeroes.
+	 * @param value
+	 * @return
+	 */
+	static QString truncValue(double value);
+
 	QPushButton *m_spinBoxUpButton;
 	QPushButton *m_spinBoxDownButton;
 	QLabel *m_titleLabel;
-	QSpinBox *m_spinBox;
+
+	// This lineedit will act like a spinbox because the QSpinBox
+	// is more restrictive than we would like :/
+	QLineEdit *m_lineedit;
+	double m_min;
+	double m_max;
+	double m_step;
 };
 } // namespace scopy
 #endif // TITLESPINBOX_H
