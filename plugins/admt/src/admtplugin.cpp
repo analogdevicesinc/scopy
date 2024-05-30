2f6ee601f473d2afb93e07f3606ac8b7136625e4
John Lloyd Juanillo <Johnlloyd.Juanillo@analog.com>
2024-05-30 10:53:56 +0800

    admt: Configured ADMT plugin
    - Added logic for compatible
    - Changed Harmonic Calibration icon
    - Changed plugin description
    
    Signed-off-by: John Lloyd Juanillo <Johnlloyd.Juanillo@analog.com>

---
+++ plugins/admt/src/admtplugin.cpp
@@ -0,0 +1,122 @@
+#include "admtplugin.h"
+#include "harmoniccalibration.h"
+
+#include <QLoggingCategory>
+#include <QLabel>
+
+#include <iioutil/connectionprovider.h>
+
+Q_LOGGING_CATEGORY(CAT_ADMTPLUGIN, "ADMTPlugin")
+using namespace scopy::admt;
+
+bool ADMTPlugin::compatible(QString m_param, QString category)
+{
+	m_name = "ADMT4000";
+	bool ret = false;
+	Connection *conn = ConnectionProvider::open(m_param);
+
+	if(!conn) {
+		qWarning(CAT_ADMTPLUGIN) << "No context available for admt";
+		return false;
+	}
+
+	iio_device *admtDevice = iio_context_find_device(conn->context(), "admt4000");
+	if(admtDevice) {
+		ret = true;
+	}
+
+	ConnectionProvider::close(m_param);
+
+	return ret;
+}
+
+bool ADMTPlugin::loadPage()
+{
+	// Here you must write the code for the plugin info page 
+	// Below is an example for an iio device
+	/*m_page = new QWidget();
+	m_page->setLayout(new QVBoxLayout(m_page));
+	m_page->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+	m_infoPage = new InfoPage(m_page);
+	m_infoPage->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+	m_page->layout()->addWidget(m_infoPage);
+	m_page->layout()->addItem(new QSpacerItem(0, 0, QSizePolicy::Preferred, QSizePolicy::Expanding));
+
+	auto cp = ContextProvider::GetInstance();
+	struct iio_context *context = cp->open(m_param);
+	ssize_t attributeCount = iio_context_get_attrs_count(context);
+	for(int i = 0; i < attributeCount; ++i) {
+		const char *name;
+		const char *value;
+		int ret = iio_context_get_attr(context, i, &name, &value);
+		if(ret < 0) {
+			qWarning(CAT_ADMTPLUGIN) << "Could not read attribute with index:" << i;
+			continue;
+		}
+
+		m_infoPage->update(name, value);
+	}
+	cp->close(m_param);
+	m_page->ensurePolished();*/
+	return true;
+}
+
+bool ADMTPlugin::loadIcon()
+{
+	SCOPY_PLUGIN_ICON(":/gui/icons/adalm.svg");
+	return true;
+}
+
+void ADMTPlugin::loadToolList()
+{
+	m_toolList.append(
+		SCOPY_NEW_TOOLMENUENTRY("harmoniccalibration", "Harmonic Calibration", ":/gui/icons/scopy-default/icons/tool_oscilloscope.svg"));
+}
+
+void ADMTPlugin::unload() { /*delete m_infoPage;*/ }
+
+QString ADMTPlugin::description() { return "Plugin for ADMT Harmonic Calibration"; }
+
+bool ADMTPlugin::onConnect()
+{
+	// This method is called when you try to connect to a device and the plugin is 
+	// compatible to that device 
+	// In case of success the function must return true and false otherwise 
+    	
+	HarmonicCalibration *harmonicCalibration = new HarmonicCalibration();
+	m_toolList[0]->setTool(harmonicCalibration);
+	m_toolList[0]->setEnabled(true);
+	m_toolList[0]->setRunBtnVisible(true);
+    	return true;
+}
+
+bool ADMTPlugin::onDisconnect()
+{
+	// This method is called when the disconnect button is pressed
+	// It must remove all connections that were established on the connection
+	for(auto &tool : m_toolList) {
+		tool->setEnabled(false);
+		tool->setRunning(false);
+		tool->setRunBtnVisible(false);
+		QWidget *w = tool->tool();
+		if(w) {
+			tool->setTool(nullptr);
+			delete(w);
+		}
+	}
+	return true;
+}
+
+void ADMTPlugin::initMetadata()
+{
+	loadMetadata(
+		R"plugin(
+	{
+	   "priority":100,
+	   "category":[
+	      "iio"
+	   ],
+	   "exclude":[""]
+	}
+)plugin");
+}
