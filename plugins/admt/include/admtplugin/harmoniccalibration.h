b8fca81d2ce5b30f6ff8fc0baf9a4c4a0ae58306
John Lloyd Juanillo <Johnlloyd.Juanillo@analog.com>
2024-06-03 11:39:46 +0800

    admt: Added test GUI textbox and button for rotation, count, and angle
    Signed-off-by: John Lloyd Juanillo <Johnlloyd.Juanillo@analog.com>
    Signed-off-by: JJuanill <JohnLloyd.Juanillo@analog.com>

---
+++ plugins/admt/include/admtplugin/harmoniccalibration.h
@@ -0,0 +1,43 @@
+#ifndef HARMONICCALIBRATION_H
+#define HARMONICCALIBRATION_H
+
+#include "scopy-admtplugin_export.h"
+
+#include <QBoxLayout>
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QString>
+#include <QVBoxLayout>
+#include <QScrollArea>
+
+#include <toolbuttons.h>
+#include <tooltemplate.h>
+#include <menuheader.h>
+#include <menusectionwidget.h>
+#include <menucollapsesection.h>
+
+namespace scopy::admt {
+class SCOPY_ADMTPLUGIN_EXPORT HarmonicCalibration : public QWidget
+{
+	Q_OBJECT
+public:
+	HarmonicCalibration(QWidget *parent = nullptr);
+	~HarmonicCalibration();
+private:
+	ToolTemplate *tool;
+	GearBtn *settingsButton;
+	InfoBtn *infoButton;
+	RunBtn *runButton;
+	QPushButton *getRotationButton, *getCountButton, *getAngleButton;
+	QLineEdit *rotationLineEdit, *countLineEdit, *angleLineEdit;
+	MenuHeaderWidget *header;
+	QWidget *leftWidget, *leftBody;
+	QVBoxLayout *leftLayout, *leftBodyLayout;
+	MenuSectionWidget *rotationSection, *countSection, *angleSection;
+	MenuCollapseSection *rotationCollapse, *countCollapse, *angleCollapse;
+	QScrollArea *scrollArea;
+};
+} // namespace scopy::admt
+#endif // HARMONICCALIBRATION_H
