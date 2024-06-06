748a7e41b11c7608989a1760571be134db987cc0
Bindea Cristian <cristian.bindea@analog.com>
2024-06-06 16:54:37 +0300

    ci/kuiper: implement excludelist list for what libraries to be bundled in the appimage
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- CI/kuiper/copy-deps.sh
+++ CI/kuiper/copy-deps.sh
@@ -6,7 +6,8 @@ source $SRC_DIR/CI/kuiper/kuiper_build_config.sh
 
 BINARY=$1
 LOCATION=$2
-
+LIBS_ARRAY=()
+BLACKLISTED=($(wget --quiet https://raw.githubusercontent.com/probonopd/AppImages/master/excludelist -O - | sort | uniq | cut -d '#' -f 1 | grep -v "^#.*" | grep "[^-\s]"))
 
 if [ ! -f "${SRC_DIR}"/CI/kuiper/ldd-mod ]; then
 	sed 's|.*RTLDLIST=.*|RTLDLIST="/usr/arm-linux-gnueabihf/lib/ld-2.31.so /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3"|' /usr/bin/ldd | tee "${SRC_DIR}"/CI/kuiper/ldd-mod
@@ -14,7 +15,6 @@ if [ ! -f "${SRC_DIR}"/CI/kuiper/ldd-mod ]; then
 fi
 
 export LD_LIBRARY_PATH="${APP_DIR}/usr/lib:${SYSROOT}/lib:${SYSROOT}/lib/arm-linux-gnueabihf:${SYSROOT}/usr/arm-linux-gnueabihf/lib:${SYSROOT}/usr/local/qt5.15/lib:${SYSROOT}/usr/local/lib:${SRC_DIR}/build"
-LIBS_ARRAY=()
 run_ldd(){
 
 	if [ ! -z "$(${SRC_DIR}/CI/kuiper/ldd-mod $1 | grep "not found")" ]; then
@@ -25,15 +25,17 @@ run_ldd(){
 
 	for library in $("${SRC_DIR}"/CI/kuiper/ldd-mod "$1" | cut -d '>' -f 2 | awk '{print $1}')
 	do
-		# check if the library exists at that path and if it was processed already
-		if [ -f "${library}" ] && ! [[ "${LIBS_ARRAY[*]}" =~ "${library}" ]]; then
-			LIBS_ARRAY+=("${library}")
-			echo "---NEW: ${library}"
-			if [ ! -f "${LOCATION}"/"${library##*/}" ]; then
-				cp --verbose "${library}" "${LOCATION}"
-				[ -L "${library}" ] && cp --verbose "$(realpath "${library}")" "${LOCATION}"
+		# check if the library exists at that path and if it was processed already or blacklisted
+		if ! [[ "${BLACKLISTED[*]}" =~ "${library##*/}" ]]; then
+			if [ -f "${library}" ] && ! [[ "${LIBS_ARRAY[*]}" =~ ${library} ]]; then
+				LIBS_ARRAY+=("${library}")
+				echo "---Added new lib: ${library}"
+				if [ ! -f "${LOCATION}"/"${library##*/}" ]; then
+					cp  "${library}" "${LOCATION}"
+					[ -L "${library}" ] && cp  "$(realpath "${library}")" "${LOCATION}"
+				fi
+				run_ldd "${library}"
 			fi
-			run_ldd "${library}"
 		fi
 	done
 }
