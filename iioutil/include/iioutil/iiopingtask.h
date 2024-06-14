a337ddb6324c50a49e71dcbc79c74fbb37d7191b
andrei.danila <andrei.danila@analog.com>
2024-06-14 15:59:14 +0300

    iioutil: Create ping task interface.
    Currently, there are 2 types of application-level pings that implement
    the PingTask interface: CmdQPingTask and IIOPingTask. Any ping task that
    will appear in the future must implement the ping task interface.
    
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

--- iioutil/include/iioutil/iiopingtask.h
+++ iioutil/include/iioutil/iiopingtask.h
@@ -1,34 +1,27 @@
 #ifndef IIOPINGTASK_H
 #define IIOPINGTASK_H
 
+#include "pingtask.h"
 #include "scopy-iioutil_export.h"
 
 #include <iio.h>
-
-#include <QThread>
 
 namespace scopy {
 /**
  * @brief The IIOPingTask class
  * IIOPingTask verifies IIO connection and emits pingSuccess/pingFailed
  */
-class SCOPY_IIOUTIL_EXPORT IIOPingTask : public QThread
+class SCOPY_IIOUTIL_EXPORT IIOPingTask : public PingTask
 {
 	Q_OBJECT
 public:
 	IIOPingTask(iio_context *c, QObject *parent = nullptr);
 	~IIOPingTask();
 	virtual void run() override;
-
-	static bool ping(iio_context *ctx);
-
-Q_SIGNALS:
-	void pingSuccess();
-	void pingFailed();
+	virtual bool ping() override;
 
 protected:
-	iio_context *c;
-	bool enabled;
+	iio_context *m_ctx;
 };
 } // namespace scopy
 #endif // IIOPINGTASK_H
