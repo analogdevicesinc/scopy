968761682fc2814a5d402e7da8fe7ae102e3eb5b
Andrei Popa <andrei.popa@analog.com>
2024-06-25 15:05:26 +0300

    fix format

--- tools/format.sh
+++ tools/format.sh
@@ -9,10 +9,10 @@ VERBOSE="--verbose"
 CLANG_FORMAT="clang-format"
 CMAKE_FORMAT="cmake-format"
 
-#find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
-#find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cc" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
-#find $SCOPY_REPO $SKIP_BUILD -type f -name "*.hpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
-#find $SCOPY_REPO $SKIP_BUILD -type f -name "*.h" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cc" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.hpp" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
+find $SCOPY_REPO $SKIP_BUILD -type f -name "*.h" | xargs $CLANG_FORMAT $VERBOSE $IN_PLACE
 find $SCOPY_REPO $SKIP_BUILD -type f -name "CMakeLists.txt" | xargs $CMAKE_FORMAT $IN_PLACE
 find $SCOPY_REPO $SKIP_BUILD -type f -name "*.cmake" | xargs $CMAKE_FORMAT $IN_PLACE
 pushd $SCOPY_REPO/tools
