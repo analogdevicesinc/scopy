e3138d90ac15d08e9aaba42fa2b2a987fe32b76d
John Lloyd Juanillo <Johnlloyd.Juanillo@analog.com>
2024-07-23 10:30:34 +0800

    admt: Implemented calibration routine in controller - Renamed acquisition labels
    Signed-off-by: John Lloyd Juanillo <Johnlloyd.Juanillo@analog.com>

--- plugins/admt/include/admt/admtcontroller.h
+++ plugins/admt/include/admt/admtcontroller.h
@@ -9,6 +9,17 @@
 
 #include <iioutil/connectionprovider.h>
 
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <math.h>
+#include <numeric>
+#include <complex>
+#include <iterator>
+
+using namespace std;
+
 namespace scopy::admt {    
 class ADMTController : public QObject
 {
@@ -35,11 +46,18 @@ public:
     QString getChannelValue();
     int getChannelIndex(const char *channelName);
     double getChannelValue(const char *channelName, int bufferSize);
+    QString calibrate(vector<double> PANG);
 private:
     iio_context *m_iioCtx;
     iio_buffer *m_iioBuffer;
     Connection *m_conn;
     QString uri;
+
+    unsigned int bitReverse(unsigned int x, int log2n);
+    template <typename Iter_T>
+    void fft(Iter_T a, Iter_T b, int log2n);
+    int linear_fit(vector<double> x, vector<double> y, double* slope, double* intercept);
+    int calculate_angle_error(vector<double> angle_meas, vector<double>& angle_error_ret, double* max_angle_err);
 };
 } // namespace scopy::admt
 
