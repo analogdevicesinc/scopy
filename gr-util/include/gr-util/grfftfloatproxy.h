99a0ae4c029e9bb95d57947321dfdd8c3c732081
Adrian Suciu <adrian.suciu@analog.com>
2024-07-29 15:50:22 +0300

    adc: Added power offset
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- gr-util/include/gr-util/grfftfloatproxy.h
+++ gr-util/include/gr-util/grfftfloatproxy.h
@@ -13,6 +13,7 @@
 #include <gnuradio/blocks/complex_to_mag_squared.h>
 #include <gnuradio/blocks/nlog10_ff.h>
 #include <gnuradio/blocks/multiply_const.h>
+#include <gnuradio/blocks/add_const_v.h>
 
 namespace scopy::grutil {
 class SCOPY_GR_UTIL_EXPORT GRFFTFloatProc : public GRProxyBlock
@@ -20,18 +21,21 @@ class SCOPY_GR_UTIL_EXPORT GRFFTFloatProc : public GRProxyBlock
 public:
 	GRFFTFloatProc(QObject *parent = nullptr);
 	void setWindow(gr::fft::window::win_type w);
+	void setPowerOffset(double);
 	void build_blks(GRTopBlock *top);
 	void destroy_blks(GRTopBlock *top);
 
 protected:
+	double m_powerOffset;
 	gr::fft::fft_v<float, true>::sptr fft;
 
 	gr::blocks::complex_to_mag_squared::sptr ctm;
 	gr::blocks::multiply_const_ff::sptr mult_const1;
 	gr::blocks::nlog10_ff::sptr nlog10;
-
+	gr::blocks::add_const_vff::sptr powerOffset;
 
 	gr::fft::window::win_type m_fftwindow;
+	GRTopBlock *m_top;
 };
 
 class SCOPY_GR_UTIL_EXPORT GRFFTComplexProc : public GRProxyBlock
@@ -39,18 +43,21 @@ class SCOPY_GR_UTIL_EXPORT GRFFTComplexProc : public GRProxyBlock
 public:
 	GRFFTComplexProc(QObject *parent = nullptr);
 	void setWindow(gr::fft::window::win_type w);
+	void setPowerOffset(double);
 	void build_blks(GRTopBlock *top);
 	void destroy_blks(GRTopBlock *top);
 
 protected:
+	double m_powerOffset;
 	gr::fft::fft_v<gr_complex, true>::sptr fft_complex;
 
 	gr::blocks::complex_to_mag_squared::sptr ctm;
 	gr::blocks::multiply_const_ff::sptr mult_const1;
 	gr::blocks::nlog10_ff::sptr nlog10;
-
+	gr::blocks::add_const_vff::sptr powerOffset;
 
 	gr::fft::window::win_type m_fftwindow;
+	GRTopBlock *m_top;
 };
 } // namespace scopy::grutil
 
