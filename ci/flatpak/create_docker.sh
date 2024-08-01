fbc25fabd4c645092350831e6ac8d007898dad1f
Bindea Cristian <cristian.bindea@analog.com>
2024-08-01 16:12:11 +0300

    ci/flatpak: update and improve scripts
     - made use of flatpak caching system by shipping the docker image with the deps already built
     - added a script that builds the docker image
     - exclude flatpak folder from stylecheck
    
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

---
+++ ci/flatpak/create_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe
+
+SRC_DIR=$(git rev-parse --show-toplevel 2>/dev/null ) || \
+SRC_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../../ && pwd )
+
+build_docker(){
+	# need to test again
+	pushd $SRC_DIR/ci/flatpak/docker
+	docker build -t cristianbindea/scopy2-flatpak:testing  .
+	CONTAINER_NAME=builder-flatpak
+	docker run \
+		--name $CONTAINER_NAME \
+		--privileged \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/build_flatpak_deps.sh",target=/home/runner/config/build_flatpak_deps.sh,readonly \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/defined_variables.h",target=/home/runner/config/defined_variables.h,readonly \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/org.adi.Scopy.json.c",target=/home/runner/config/org.adi.Scopy.json.c,readonly \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/Makefile",target=/home/runner/config/Makefile,readonly \
+		cristianbindea/scopy2-flatpak:testing /bin/bash -c /home/runner/config/build_flatpak_deps.sh
+
+	docker commit $CONTAINER_NAME cristianbindea/scopy2-flatpak:testing
+	echo "Docker container $CONTAINER_NAME commited as cristianbindea/scopy2-flatpak:testing image"
+	docker container rm $CONTAINER_NAME
+	popd
+}
+
+build_docker
