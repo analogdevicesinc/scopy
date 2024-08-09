ba3939fdd956b337b0e32c840d91896d30e174e7
Adrian Suciu <adrian.suciu@analog.com>
2024-08-09 17:43:08 +0300

    gui: added menuspinbox
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- gui/include/gui/widgets/menuspinbox.h
+++ gui/include/gui/widgets/menuspinbox.h
@@ -1,6 +1,7 @@
 #ifndef MENUSPINBOX_H
 #define MENUSPINBOX_H
 
+#include "plot_utils.hpp"
 #include "qboxlayout.h"
 #include "utils.h"
 #include <cmath>
@@ -19,50 +20,86 @@ public:
 	virtual ~IncrementStrategy() {};
 	virtual double increment(double val) = 0;
 	virtual double decrement(double val) = 0;
+	virtual void setScale(double scale) = 0;
 };
 
-/*class SCOPY_GUI_EXPORT IncrementStrategy125 : public IncrementStrategy {
+class SCOPY_GUI_EXPORT IncrementStrategy125 : public IncrementStrategy {
 public:
-	IncrementStrategy125(){};
+	NumberSeries m_steps;
+
+	IncrementStrategy125() : m_steps(1e-9,1e9,10){
+	};
 	~IncrementStrategy125(){};
-	virtual double increment(double val) override;
-	virtual double decrement(double val) override;
+	virtual double increment(double val) override{
+		return m_steps.getNumberAfter(val);
+	}
+	virtual double decrement(double val) override{
+		return m_steps.getNumberBefore(val);
+	}
+
+	double m_scale;
+	void setScale(double scale) override {
+		m_scale = scale;
+	}
 };
 
 class SCOPY_GUI_EXPORT IncrementStrategyPower2 : public IncrementStrategy {
 public:
-	IncrementStrategyPower2(){};
+	QList<double> m_steps;
+	IncrementStrategyPower2() {
+		for(int i=30;i>=0;i--) {
+			m_steps.append(-(1<<i));
+		}
+		for(int i=0;i<31;i++) {
+			m_steps.append(1<<i);
+		}
+	};
 	~IncrementStrategyPower2(){};
-	virtual double increment(double val) override;
-	virtual double decrement(double val) override;
-};*/
+	virtual double increment(double val) override {
+		int i = 0;
+		val = val + 1;
+		while(val > m_steps[i]) {
+			i++;
+		}
+		return m_steps[i];
+	}
+	virtual double decrement(double val) override {
+		int i = m_steps.count()-1;
+		val = val - 1;
+		while(val < m_steps[i]) {
+			i--;
+		}
+		return m_steps[i];
+
+	}
+	double m_scale;
+
+	void setScale(double scale) override {
+		m_scale = scale;
+	}
+};
 class SCOPY_GUI_EXPORT IncrementStrategyFixed : public IncrementStrategy {
 public:
 	IncrementStrategyFixed(double k = 1) { m_k = k;};
 	~IncrementStrategyFixed(){};
 	virtual double increment(double val) override {
-		double pow10 = pow(10,nrOfDigits(val)-2);
-		val = val + m_k * pow10;
+		val = val + m_k * m_scale;
 		return val;
 	}
 	virtual double decrement(double val) override {
-		double pow10 = pow(10,nrOfDigits(val)-2);
-		val = val - m_k * pow10;
+		val = val - m_k * m_scale;
 		return val;
 	}
 	void setK(double val) {m_k = val;}
 	double k() { return m_k;}
 private:
 
-	int nrOfDigits(double val) {
-		int i = 0;
-		while(val >= 1) {
-			val = val / 10;
-			i++;
-		}
-		return i;
-	}
 	double m_k;
+	double m_scale;
+
+	void setScale(double scale) override {
+		m_scale = scale;
+	}
 };
 
 class SCOPY_GUI_EXPORT UnitPrefix {
@@ -79,11 +116,12 @@ class SCOPY_GUI_EXPORT MenuSpinbox : public QWidget
 	QWIDGET_PAINT_EVENT_HELPER
 
 public:
-	enum {
-		SS_SIMPLE,
-		SS_SCALE,
+	typedef enum {
+		IS_POW2,
+		IS_125,
+		IS_FIXED
 
-	} SpinboxStyle;
+	} IncrementMode;
 
 	MenuSpinbox(QString name, double val, QString unit, double min, double max, bool vertical = 0, bool left = 0, QWidget *parent = nullptr);
 	~MenuSpinbox();
@@ -91,15 +129,19 @@ public:
 	double value() const;
 	QString unit() const;
 	IncrementStrategy *incrementStrategy() const;
-
 	QString name() const;
+
+	void setScaleRange(double min, double max);
 
 public Q_SLOTS:
 	void setName(const QString &newName);
 	void setUnit(const QString &newUnit);
-	void setValue(double newValue, bool force = false);
-	//void setValue(QString s);
-	void setIncrementStrategy(IncrementStrategy *newIncrementStrategy);
+	void setMinValue(double);
+	void setMaxValue(double);
+	void setValueForce(double newValue, bool force = true);
+	void setValueString(QString s);
+	void setValue(double newValue);
+	void setIncrementMode(IncrementMode is);
 
 Q_SIGNALS:
 	void nameChanged(QString);
@@ -112,8 +154,9 @@ private Q_SLOTS:
 
 private:
 	void applyStylesheet();
-	void populateCombobox(QString unit, double min, double max);
 	int findLastDigit(QString str);
+	void layoutVertically(bool left);
+	void layoutHorizontally(bool left);
 	double clamp(double val, double min, double max);
 
 	QLabel *m_label;
@@ -123,6 +166,7 @@ private:
 	QPushButton *m_minus;
 
 	IncrementStrategy* m_incrementStrategy;
+	IncrementMode m_im;
 
 	Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
 	Q_PROPERTY(double value READ value WRITE setValue NOTIFY valueChanged)
@@ -130,6 +174,7 @@ private:
 
 	QString m_name;
 	double m_value, m_min, m_max;
+	double m_scaleMin, m_scaleMax;
 	QString m_unit;
 
 	QList<UnitPrefix> m_scales;
