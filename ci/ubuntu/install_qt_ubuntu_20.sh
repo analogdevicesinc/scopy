72820a3dfbaed572fea43c7f0b0b70a14cbd8f59
Bindea Cristian <cristian.bindea@analog.com>
2024-08-21 15:49:16 +0300

    ci/ubuntu: update and improve docker recipe and build script
    - removed ubuntu18_build_process
    - merged ubuntu20 and ubuntu22 recipe in a single file
    - added a script that builds Docker images
    - updated Dockerfiles
    
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- ci/ubuntu/install_qt_ubuntu_20.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get -y install vim git cmake qt5-default qtcreator qtdeclarative5-dev qtdeclarative5-dev-tools libqt5svg5 libqt5svg5-dev qttools5-dev qttools5-dev-tools libqt5opengl5
