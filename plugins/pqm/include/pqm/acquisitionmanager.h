3b5d9481d89eb111e29452c631154db787135f3c
andreidanila1 <andrei.danila@analog.com>
2024-10-10 11:12:20 +0300

    plugins/pqm: Added data logging infrastructure.
    The data is saved in files that the user selects from the instrument'
    settings menu. The logging option is available for harmonics and
    waveform.
    
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- plugins/pqm/include/pqm/acquisitionmanager.h
+++ plugins/pqm/include/pqm/acquisitionmanager.h
@@ -31,6 +31,8 @@
 #include <iioutil/commandqueue.h>
 #include <iioutil/pingtask.h>
 
+#include <pqmdatalogger.h>
+
 #define MAX_ATTR_SIZE 1024
 #define BUFFER_SIZE 256
 #define DEVICE_PQM "pqm"
@@ -52,6 +54,7 @@ public Q_SLOTS:
 Q_SIGNALS:
 	void pqmAttrsAvailable(QMap<QString, QMap<QString, QString>>);
 	void bufferDataAvailable(QMap<QString, QVector<double>>);
+	void logData(PqmDataLogger::ActiveInstrument instr, const QString &filePath);
 
 private Q_SLOTS:
 	void futureReadData();
@@ -69,6 +72,7 @@ private:
 
 	iio_context *m_ctx;
 	iio_buffer *m_buffer;
+	PqmDataLogger *m_pqmLog;
 
 	QTimer *m_pingTimer = nullptr;
 	PingTask *m_pingTask = nullptr;
