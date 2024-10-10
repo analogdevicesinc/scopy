c1afa49f1ac53f6aebfb1238a910a51a0f867b3d
andreidanila1 <andrei.danila@analog.com>
2024-10-10 16:17:32 +0300

    plugins/adcplugin: Solved x/y axis scale bug.
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- gui/include/gui/axishandle.h
+++ gui/include/gui/axishandle.h
@@ -12,12 +12,8 @@ class PlotWidget;
 
 enum HandlePos : int
 {
-	NORTH_WEST = 0,
-	SOUTH_EAST = 1,
-	NORTH = 0,
-	SOUTH = 1,
-	WEST = 0,
-	EAST = 1
+	NORTH_OR_WEST = 0, // west for y axis, north for x axis
+	SOUTH_OR_EAST = 1  // east for y axis, south for x axis
 };
 
 enum HandleOrientation : int
