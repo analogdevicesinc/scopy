e6defcd67115abfe4d604ed31c79ffb2bb063e0c
Andrei Popa <andrei.popa@analog.com>
2024-10-15 17:51:27 +0300

    Style: added style previewer tool in test plugin
    Signed-off-by: Andrei Popa <andrei.popa@analog.com>

--- gui/include/gui/style.h
+++ gui/include/gui/style.h
@@ -26,14 +26,43 @@ private:
 
 public:
 	static Style *GetInstance();
+
+	// get value from json file using the keys in style_attributes.h as QString
 	static QString getAttribute(const char *key);
+
+	// get color from json file using the keys in style_attributes.h as QColor
+	// value needs to be a color code
 	static QColor getColor(const char *key);
+
+	// get dimension from json file using the keys in style_attributes.h as int
+	// value needs to be an intiger
 	static int getDimension(const char *key);
+
+	// set style to a widget using keys in style_properties.h
+	// value: the property value the widget will have. Only change it if the style css code uses different values
+	// force: will do setStyleSheet on the widget. This may be needed if another stylesheet was previously set on this widget.
+	// using force is not recommended
 	static void setStyle(QWidget *widget, const char *style, QVariant value = true, bool force = false);
+
+	// get pixmap with color changed to json::theme::pixmap_color
+	// color: used for setting specific colors
 	static QPixmap getPixmap(QString pixmap, QColor color = QColor());
+
+	// get transparent color based on color picked from style_attributes.h
+	// value needs to be a color code
+	// transparency: percentage value 0-100
 	static QString getColorTransparent(const char *key, double transparency);
+
+	// get ch color for index
+	// THIS IS LIMITED TO CHANNELS IN JSON, if the index is too large use StyleHelper
 	static QColor getChannelColor(int index);
+
+	// get all ch colors
 	static QList<QColor> getChannelColorList();
+
+	// set background to widget
+	// color: keys in style_attributes.h or QString color code
+	// extend_to_children: using false is recommended
 	static void setBackgroundColor(QWidget *widget, const char *color, bool extend_to_children = false);
 	static void setBackgroundColor(QWidget *widget, QString color, bool extend_to_children = false);
 
@@ -41,6 +70,8 @@ public:
 	bool setTheme(QString theme);
 	QStringList getThemeList();
 
+	// recursively replaces keys from style_attributes.h
+	// you shouldn't have to use this
 	static QString replaceAttributes(QString style, int calls_limit = 10);
 
 	void setM2KStylesheet(QWidget *widget);
