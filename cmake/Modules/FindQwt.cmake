c6c436ccc7174bdb6593494a226ec647622b2fe6
Adrian Stanea <Adrian.Stanea@analog.com>
2024-10-21 14:47:17 +0300

    license-headers: run add headers script on files with missing license
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- cmake/Modules/FindQwt.cmake
+++ cmake/Modules/FindQwt.cmake
@@ -1,3 +1,23 @@
+#
+# Copyright (c) 2024 Analog Devices Inc.
+#
+# This file is part of Scopy
+# (see https://www.github.com/analogdevicesinc/scopy).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
 # * try to find Qwt libraries and include files QWT_INCLUDE_DIR where to find qwt_global.h, etc. QWT_LIBRARIES libraries
 #   to link against QWT_FOUND If false, do not try to use Qwt qwt_global.h holds a string with the QWT version; test to
 #   make sure it's at least 5.2
