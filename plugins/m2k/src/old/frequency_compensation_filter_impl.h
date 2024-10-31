2884f6968d81450cf02a138eb0f2323acf4ee0b5
Adrian Stanea <Adrian.Stanea@analog.com>
2024-10-31 12:19:29 +0200

    license-headers: handle inconsistent headers
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- plugins/m2k/src/old/frequency_compensation_filter_impl.h
+++ plugins/m2k/src/old/frequency_compensation_filter_impl.h
@@ -1,21 +1,22 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2019 Analog Devices Inc.
+ * Copyright (c) 2019 Analog Devices Inc.
  *
- * This is free software; you can redistribute it and/or modify
+ * This file is part of Scopy
+ * (see http://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef OVERSHOOT_FILTER_HPP
--- plugins/pqm/src/pqmdatalogger.cpp
+++ plugins/pqm/src/pqmdatalogger.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2024 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see https://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "pqmdatalogger.h"
 
 #include <QFile>
@@ -126,3 +147,5 @@ void PqmDataLogger::createHeader()
 		qWarning(CAT_PQMLOG) << m_filePath << "cannot be opened!";
 	}
 }
+
+#include "moc_pqmdatalogger.cpp"
