a59ce63032c413a9d2473a6c1590fb22c9556e13
Bindea Cristian <cristian.bindea@analog.com>
2024-11-04 14:45:07 +0200

    ci/arm: add new workflow for scopy-arm64
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- ci/arm/build_qt.sh
+++ ci/arm/build_qt.sh
@@ -2,23 +2,28 @@
 
 set -ex
 SRC_SCRIPT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SRC_SCRIPT/armhf_build_config.sh
+source $SRC_SCRIPT/arm_build_config.sh $1
 
 install_packages(){
 	sudo apt install -y build-essential cmake unzip gfortran gcc git bison \
-		python gperf pkg-config gdb-multiarch g++ flex texinfo gawk openssl \
+		gperf pkg-config gdb-multiarch g++ flex texinfo gawk openssl \
 		pigz libncurses-dev autoconf automake tar figlet libclang-dev
 }
 
-# Download and extract QT Source (QT 5.15.2)
+# Download and extract QT Source (QT 5.15.2 for armhf and QT 5.15.10 for arm64)
 download_qt(){
 	mkdir -p ${STAGING_AREA}
 	pushd ${STAGING_AREA}
 	if [ ! -d qt-everywhere-src ];then
 		wget --progress=dot:giga ${QT_DOWNLOAD_LINK}
-		tar -xf qt-everywhere-src-*.tar.xz && rm qt-everywhere-src-*.tar.xz && mv qt-everywhere-src-* qt-everywhere-src # unzip and rename
+		tar -xf qt-everywhere-*.tar.xz && rm qt-everywhere-*.tar.xz && mv qt-everywhere-* qt-everywhere-src # unzip and rename
 		cd qt-everywhere-src
-		patch -p1 < $SRC_SCRIPT/qt_patch.patch # Patch QT Source
+		# Patch QT Source
+		if [ $TOOLCHAIN_HOST == "aarch64-linux-gnu"  ]; then
+			patch -p1 -R < $SRC_SCRIPT/qt_patch_arm64.patch
+		elif [ $TOOLCHAIN_HOST == "arm-linux-gnueabihf" ]; then
+			patch -p1 < $SRC_SCRIPT/qt_patch_armhf.patch
+		fi
 	else
 		echo "QT already downloaded"
 	fi
@@ -37,23 +42,50 @@ download_crosscompiler(){
 	popd
 }
 
-build_qt5.15.2(){
-	mkdir -p $STAGING_AREA/build-qt5.15.2 && cd $STAGING_AREA/build-qt5.15.2
+build(){
+	mkdir -p "$STAGING_AREA"/build-qt && cd "$STAGING_AREA"/build-qt
 	../qt-everywhere-src/configure \
 	-v \
 	-release \
 	-opensource \
 	-confirm-license \
-	-sysroot $SYSROOT \
-	-prefix $QT_SYSTEM_LOCATION \
-	-extprefix $QT_BUILD_LOCATION \
+	-sysroot "$SYSROOT" \
+	-prefix "$QT_SYSTEM_LOCATION" \
+	-extprefix "$QT_BUILD_LOCATION" \
+	-egl \
 	-eglfs \
+	-reduce-exports \
 	-opengl desktop \
-	-device linux-rasp-pi4-v3d-g++ \
-	-device-option CROSS_COMPILE=$CROSS_COMPILER/bin/arm-linux-gnueabihf- \
+	-device $QT_DEVICE \
+	-device-option CROSS_COMPILE="$CROSS_COMPILER"/bin/"$TOOLCHAIN_HOST"- \
+	-skip qtandroidextras \
+	-skip qtcharts \
+	-skip qtdatavis3d \
+	-skip qtdoc \
+	-skip qtgamepad \
+	-skip qtgraphicaleffects \
+	-skip qtlocation \
+	-skip qtlottie \
+	-skip qtmacextras \
+	-skip qtnetworkauth \
+	-skip qtquick3d \
+	-skip qtquickcontrols \
+	-skip qtquickcontrols2 \
+	-skip qtquicktimeline \
+	-skip qtremoteobjects \
 	-skip qtscript \
+	-skip qtsensors \
+	-skip qtspeech \
+	-skip qttranslations \
+	-skip qtvirtualkeyboard \
 	-skip qtwayland \
+	-skip qtwebchannel \
 	-skip qtwebengine \
+	-skip qtwebglplugin \
+	-skip qtwebsockets \
+	-skip qtwebview \
+	-skip qtwinextras \
+	-nomake examples -no-compile-examples \
 	-nomake tests \
 	-make libs \
 	-pkg-config \
@@ -64,12 +96,12 @@ build_qt5.15.2(){
 	-bundled-xcb-xinput \
 	-qt-pcre \
 	-qpa eglfs \
-	-L$SYSROOT/usr/lib/arm-linux-gnueabihf -I$SYSROOT/usr/include/arm-linux-gnueabihf
+	-L"$SYSROOT"/usr/lib/"$TOOLCHAIN_HOST" -I"$SYSROOT"/usr/include/"$TOOLCHAIN_HOST"
 
 	make -j14
 	sudo make install # installs to $QT_BUILD_LOCATION
 }
 
-for arg in $@; do
+for arg in "${@:2}"; do
 	$arg
 done
