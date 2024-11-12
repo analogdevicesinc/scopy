69fe3bee5d60b0fa003312c12d14fc6bc3a0307d
dpineda2 <Dounell.Pineda@analog.com>
2024-11-13 07:28:13 +0800

    admt: Initial implement of style helper
    Signed-off-by: dpineda2 <Dounell.Pineda@analog.com>

---
+++ plugins/admt/include/admt/admtstylehelper.h
@@ -0,0 +1,37 @@
+#ifndef ADMTSTYLEHELPER_H
+#define ADMTSTYLEHELPER_H
+
+#include "scopy-admt_export.h"
+#include "stylehelper.h"
+
+#include <QApplication>
+#include <QComboBox>
+#include <QLineEdit>
+
+#include <plotwidget.h>
+
+namespace scopy {
+namespace admt{
+class SCOPY_ADMT_EXPORT ADMTStyleHelper : public QObject
+{
+    Q_OBJECT
+protected:
+    ADMTStyleHelper(QObject *parent = nullptr);
+    ~ADMTStyleHelper();
+public:
+    // singleton
+    ADMTStyleHelper(ADMTStyleHelper &other) = delete;
+    void operator=(const ADMTStyleHelper &) = delete;
+    static ADMTStyleHelper *GetInstance();
+public:
+    static void TopContainerButtonStyle(QPushButton *btn, QString objectName = "");
+    static void PlotWidgetStyle(PlotWidget *widget, QString objectName = "");
+    static void ComboBoxStyle(QComboBox *widget, QString objectName = "");
+    static void LineEditStyle(QLineEdit *widget, QString objectName = "");
+private:
+    static ADMTStyleHelper *pinstance_;
+};
+} // namespace admt
+} // namespace scopy
+
+#endif // ADMTSTYLEHELPER_H
