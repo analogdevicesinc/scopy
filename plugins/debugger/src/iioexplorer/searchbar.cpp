958b485fe94760c672ea6b28cc4f471bca11386f
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2024-11-21 10:47:06 +0200

    iioexplorer/searchbar: Remove "Filter" label
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- plugins/debugger/src/iioexplorer/searchbar.cpp
+++ plugins/debugger/src/iioexplorer/searchbar.cpp
@@ -29,21 +29,17 @@ using namespace scopy::debugger;
 SearchBar::SearchBar(QSet<QString> options, QWidget *parent)
 	: QWidget(parent)
 	, m_lineEdit(new QLineEdit(this))
-	, m_label(new QLabel("Filter", this))
 	, m_completer(new QCompleter(options.values(), this))
 {
 	Style::setStyle(m_lineEdit, style::properties::debugger::searchBar);
-	Style::setStyle(m_label, style::properties::label::menuSmall);
 	setFixedHeight(34); // Hardcoded to match similar container
 	m_completer->setCaseSensitivity(Qt::CaseInsensitive);
 	m_completer->setFilterMode(Qt::MatchContains);
 	m_lineEdit->setPlaceholderText("Type to filter");
 	m_lineEdit->setCompleter(m_completer);
 	m_lineEdit->setMinimumHeight(31);
-	m_label->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
 
 	setLayout(new QHBoxLayout(this));
-	layout()->addWidget(m_label);
 	layout()->addWidget(m_lineEdit);
 	layout()->setMargin(0);
 }
