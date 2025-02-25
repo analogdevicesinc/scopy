4684b85bf4b91c566fbc34a9df2b109019b335bf
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2025-02-25 15:17:38 +0200

    workflows/docs: Add step for test report generation
    The 'generate_doc.yml' will behave as usual, with the exeption that once
    a PR to main closes and it comes from a branch that starts with
    'testing_', it will extract from the branch name the version, create the
    test reports and and copy them to a folder for versioning called
    'scopy_testing_results/<tested_version>'
    
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- docs/test_report_generator.py
+++ docs/test_report_generator.py
@@ -2,82 +2,195 @@ import os
 import re
 import sys
 
-def count_tests_in_rst(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
-    test_pattern = re.compile(r'^Test \d+(:| -) ', re.MULTILINE)
-    tests = test_pattern.findall(content)
-    return len(tests), content
 
-def parse_test_results(content):
-    passed = 0
-    failed = 0
-    skipped = 0
-    parse_pattern = re.compile(r'\*\*Result:*\*\*:*\s*(.*?)(?:\n|$)', re.MULTILINE | re.DOTALL)
-    parsed_tests = parse_pattern.findall(content)
-    for result in parsed_tests:
-        if result.strip() == "FAIL":
-            failed += 1
-        elif result.strip() == "PASS":
-            passed += 1
-        else:
-            skipped += 1
-    return passed, failed, skipped
+def scopy_test_resources_base_dir(version: str) -> str:
+    return f"https://github.com/analogdevicesinc/scopy/blob/{version}/testing_results/"
 
-def process_tests_in_folder(folder_path, output_rst_file):
-    for root, _, files in os.walk(folder_path):
+
+class TestSuiteOverview:
+    @staticmethod
+    def output_rst_header(output_rst_file: str) -> None:
+        with open(output_rst_file, "w") as file:
+            file.write(f".. admonition:: Test Report {output_rst_file}\n\n")
+            file.write("   .. list-table::\n")
+            file.write("      :header-rows: 1\n")
+            file.write("      :widths: 50 30 30 50 50 50\n\n")
+            file.write("      * - NAME\n")
+            file.write("        - PASS\n")
+            file.write("        - FAIL\n")
+            file.write("        - SKIP\n")
+            file.write("        - Total\n")
+            file.write("        - Details\n")
+
+    @staticmethod
+    def output_to_rst_table(output_rst_file: str, name: str, link: str, passed: int, failed: int, skipped: int,
+                            total: int) -> None:
+        with open(output_rst_file, "a") as file:
+            file.write(f"      * - {name}\n")
+            file.write(f"        - {passed}\n")
+            file.write(f"        - {failed}\n")
+            file.write(f"        - {skipped}\n")
+            file.write(f"        - {total}\n")
+            file.write(f"        - :ref:`{link}`\n")
+
+    @staticmethod
+    def output_to_rst_footer(output_rst_file: str, tests: dict) -> None:
+        with open(output_rst_file, "a") as file:
+            file.write(
+                "\nContents\n"
+                "----------------------------------------------------------\n"
+                ".. toctree::\n"
+                "   :includehidden:\n"
+                "   :maxdepth: 3\n"
+                "\n")
+
+            for key in tests:
+                file.write(f"   {key}_report\n")
+
+
+class TestCaseOverview:
+    @staticmethod
+    def output_rst_header(output_rst_file: str, file_name: str) -> None:
+        with open(output_rst_file, "w") as file:
+            file.write(
+                f".. _{file_name}_report:\n"
+                f"\n"
+                f"Test Report for {file_name}\n"
+                f"==========================================================\n"
+                f"\n"
+                f"\n"
+                f".. admonition:: Test Report {file_name}\n\n"
+                f"   .. list-table::\n"
+                f"      :header-rows: 1\n"
+                f"      :widths: 10 10 10 10 10\n\n"
+                f"      * - UID\n"
+                f"        - TESTED OS\n"
+                f"        - COMMENTS\n"
+                f"        - RESULT\n"
+                f"        - TEST LINK\n")
+
+    @staticmethod
+    def output_to_rst_table(output_rst_file: str, uid: str, tested_os: str, comments: str, result: str,
+                            link_to_file: str) -> None:
+        with open(output_rst_file, "a") as file:
+            file.write(f"      * - {uid}\n")
+            file.write(f"        - {tested_os}\n")
+            file.write(f"        - {comments}\n")
+            file.write(f"        - {result}\n")
+            file.write(f"        - {link_to_file}\n")
+
+
+def maneuver(test_folder: str, test_report_dir: str, scopy_version: str) -> None:
+    tests = {}
+    test_folder_full = os.path.abspath(test_folder)
+    test_report_dir_full = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), test_report_dir))
+
+    if not os.path.exists(test_report_dir_full):
+        os.mkdir(test_report_dir_full)
+
+    for root, _, files in os.walk(test_folder_full):
         for file in files:
             if file.endswith('.rst'):
-                file_path = os.path.join(root, file)
-                num_tests, content = count_tests_in_rst(file_path)
-                passed, failed, skipped = parse_test_results(content)
-                output_to_rst_table(output_rst_file, file.split('.')[0], passed, 
-                failed, skipped, num_tests)
+                with open(os.path.join(root, file), 'r') as f:
+                    print(f"Processing {root}/{file}")
+                    content = f.read()
+                    # This mega regex is used to extract the test cases from
+                    # the rst files.
+                    matches = re.findall(
+                        r'\*\*UID:*\*\*:*\s*(.*?)\n.*?OS:\s*(.*?)\n.*?\*\*Steps:*\*\*:*\n(.*?)\*\*Tested OS:?\*\*:? ?(.*?)\n.*?\*\*Comments:?\*\*:? ?(.*?)\n.*?\*\*Result:?\*\*:? ?(.*?)\n',
+                        content,
+                        re.DOTALL)
 
-def output_rst_header(output_rst_file, folder_name):
-    with open(output_rst_file, "w") as file:
-        file.write(f".. admonition:: Test Report {folder_name}\n\n")
-        file.write(f"   .. list-table::\n")
-        file.write(f"      :header-rows: 1\n")
-        file.write(f"      :widths: 50 30 30 50 50\n\n")
-        file.write(f"      * - {folder_name}\n")
-        file.write(f"        - PASS\n")
-        file.write(f"        - FAIL\n")
-        file.write(f"        - SKIP\n")
-        file.write(f"        - Total\n")
+                    pass_count = 0
+                    fail_count = 0
+                    skip_count = 0
 
-def output_to_rst_table(output_rst_file, module, passed, failed, skipped, total):
-    if total == 0:
-        return
-    with open(output_rst_file, "a") as file:
-        file.write(f"      * - {module}\n")
-        file.write(f"        - {passed}\n")
-        file.write(f"        - {failed}\n")
-        file.write(f"        - {skipped}\n")
-        file.write(f"        - {total}\n")
+                    if len(matches) > 0:
+                        file_name = file.removesuffix(".rst")
+                        full_output_name = os.path.join(
+                            test_report_dir_full, f"{file_name}_report.rst")
+                        TestCaseOverview.output_rst_header(
+                            output_rst_file=full_output_name,
+                            file_name=file_name
+                        )
 
-def output_root_docs_include(docs_root_folder, folder_name):
-    with open(os.path.join(docs_root_folder, "tests", "test_report.rst"), "a") as file:
-        file.write(f".. include:: ../../testing_results/{folder_name}/test_report.rst\n")
+                        for match in matches:
+                            uid, _, _, tested_os, comments, result = match
+                            TestCaseOverview.output_to_rst_table(
+                                output_rst_file=full_output_name,
+                                uid=uid,
+                                tested_os=tested_os,
+                                comments=comments,
+                                result=result,
+                                link_to_file=scopy_test_resources_base_dir(
+                                    scopy_version) + test_folder + os.path.relpath(root, test_folder_full) + "/" + file
+                            )
 
-def process_directory(directory, docs_root_folder):
-    directory = os.path.abspath(directory)
-    folders = os.listdir(directory)
-    for testing_dir in folders:
-        folder_path = os.path.join(directory, testing_dir)
-        if os.path.isdir(folder_path):
-            output_rst_file = os.path.join(folder_path, "test_report.rst")
-            output_root_docs_include(docs_root_folder, testing_dir)
-            output_rst_header(output_rst_file, testing_dir)
-            process_tests_in_folder(folder_path, output_rst_file)
-            print(f'Tests in {testing_dir} were processed.')
+                            if result == "PASS":
+                                pass_count += 1
+                            elif result == "FAIL":
+                                fail_count += 1
+                            else:
+                                skip_count += 1
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python test_report_generator.py <docs_root_folder> <testing_results_dir>")
+                        tests[file_name] = {
+                            "pass": pass_count,
+                            "fail": fail_count,
+                            "skip": skip_count,
+                            "total": len(matches)
+                        }
+
+    overview_file_name = os.path.join(
+        test_report_dir_full, "test_report_overview.rst")
+    TestSuiteOverview.output_rst_header(overview_file_name)
+
+    for key in tests:
+        TestSuiteOverview.output_to_rst_table(
+            output_rst_file=overview_file_name,
+            name=key,
+            link=key + "_report",
+            passed=tests[key]["pass"],
+            failed=tests[key]["fail"],
+            skipped=tests[key]["skip"],
+            total=tests[key]["total"]
+        )
+
+    # Create the index for all test report files
+    index_file_name = os.path.join(test_report_dir_full, "index.rst")
+    with open(index_file_name, "w") as file:
+        file.write(".. _test_report:\n\n")
+        file.write("Test Report\n")
+        file.write(
+            "========================================================\n\n")
+        file.write(".. include:: test_report_overview.rst\n\n")
+        file.write("Contents\n")
+        file.write(
+            "----------------------------------------------------------\n")
+        file.write(".. toctree::\n")
+        file.write("   :maxdepth: 3\n")
+        file.write("\n")
+        for key in tests:
+            file.write(f"   {key}_report\n")
+
+    # Reference the created index in the above level
+    link_index = os.path.join(test_report_dir_full, "../index.rst")
+    with open(link_index, "a") as file:
+        file.write("   test_report/index\n")
+
+
+def main() -> None:
+    if len(sys.argv) != 4:
+        print("Usage: python test_report_generator.py <docs_root_folder> <testing_results_dir> <scopy_version>")
         sys.exit(1)
 
-    docs_root_folder = os.path.abspath(sys.argv[1])
-    testing_results_folder = sys.argv[2]
-    open(os.path.join(docs_root_folder, "tests", "test_report.rst"), "w").close()
-    process_directory(testing_results_folder, docs_root_folder)
+    test_folder = sys.argv[1]
+    test_output_dir = sys.argv[2]
+    scopy_version = sys.argv[3]
+
+    maneuver(test_folder=test_folder, test_report_dir=test_output_dir,
+             scopy_version=scopy_version)
+
+
+if __name__ == "__main__":
+    main()
