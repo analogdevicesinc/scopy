32930e8bb3ec910b4f5866cff0133cdad72152d7
John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>
2025-02-27 14:10:16 +0800

    admt: Applied clang-format for C++ files
    Signed-off-by: John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>
    Signed-off-by: JJuanill <JohnLloyd.Juanillo@analog.com>

--- plugins/admt/include/admt/widgets/registerblockwidget.h
+++ plugins/admt/include/admt/widgets/registerblockwidget.h
@@ -34,46 +34,51 @@
 #include <menusectionwidget.h>
 
 namespace scopy::admt {
-class SCOPY_ADMT_EXPORT RegisterBlockWidget : public QWidget {
-  Q_OBJECT
+class SCOPY_ADMT_EXPORT RegisterBlockWidget : public QWidget
+{
+	Q_OBJECT
 public:
-  enum ACCESS_PERMISSION { READ, WRITE, READWRITE };
+	enum ACCESS_PERMISSION
+	{
+		READ,
+		WRITE,
+		READWRITE
+	};
 
-  QPushButton *m_readButton, *m_writeButton;
+	QPushButton *m_readButton, *m_writeButton;
 
-  RegisterBlockWidget(QString header, QString description, uint32_t address,
-                      uint32_t cnvPage,
-                      RegisterBlockWidget::ACCESS_PERMISSION accessPermission,
-                      QWidget *parent = nullptr);
-  virtual ~RegisterBlockWidget();
-  QPushButton *readButton();
-  QPushButton *writeButton();
-  uint32_t getAddress();
-  uint32_t getCnvPage();
-  uint32_t getValue();
-  void setValue(uint32_t value);
-  RegisterBlockWidget::ACCESS_PERMISSION getAccessPermission();
+	RegisterBlockWidget(QString header, QString description, uint32_t address, uint32_t cnvPage,
+			    RegisterBlockWidget::ACCESS_PERMISSION accessPermission, QWidget *parent = nullptr);
+	virtual ~RegisterBlockWidget();
+	QPushButton *readButton();
+	QPushButton *writeButton();
+	uint32_t getAddress();
+	uint32_t getCnvPage();
+	uint32_t getValue();
+	void setValue(uint32_t value);
+	RegisterBlockWidget::ACCESS_PERMISSION getAccessPermission();
 public Q_SLOTS:
-  void onValueChanged(int);
+	void onValueChanged(int);
 
 private:
-  uint32_t m_address, m_value, m_cnvPage;
-  RegisterBlockWidget::ACCESS_PERMISSION m_accessPermission;
+	uint32_t m_address, m_value, m_cnvPage;
+	RegisterBlockWidget::ACCESS_PERMISSION m_accessPermission;
 
-  QSpinBox *m_spinBox;
+	QSpinBox *m_spinBox;
 
-  void addReadButton(QWidget *parent);
-  void addWriteButton(QWidget *parent);
+	void addReadButton(QWidget *parent);
+	void addWriteButton(QWidget *parent);
 };
 
-class SCOPY_ADMT_EXPORT PaddedSpinBox : public QSpinBox {
-  Q_OBJECT
+class SCOPY_ADMT_EXPORT PaddedSpinBox : public QSpinBox
+{
+	Q_OBJECT
 public:
-  PaddedSpinBox(QWidget *parent = nullptr);
-  virtual ~PaddedSpinBox();
+	PaddedSpinBox(QWidget *parent = nullptr);
+	virtual ~PaddedSpinBox();
 
 protected:
-  QString textFromValue(int value) const override;
+	QString textFromValue(int value) const override;
 };
 } // namespace scopy::admt
 
--- plugins/admt/src/widgets/horizontalspinbox.cpp
+++ plugins/admt/src/widgets/horizontalspinbox.cpp
@@ -25,107 +25,112 @@
 
 using namespace scopy::admt;
 
-HorizontalSpinBox::HorizontalSpinBox(QString header, double initialValue,
-                                     QString unit, QWidget *parent)
-    : QWidget(parent), m_value(initialValue), m_unit(unit) {
-  QVBoxLayout *container = new QVBoxLayout(this);
-  setLayout(container);
-  container->setMargin(0);
-  container->setSpacing(4);
+HorizontalSpinBox::HorizontalSpinBox(QString header, double initialValue, QString unit, QWidget *parent)
+	: QWidget(parent)
+	, m_value(initialValue)
+	, m_unit(unit)
+{
+	QVBoxLayout *container = new QVBoxLayout(this);
+	setLayout(container);
+	container->setMargin(0);
+	container->setSpacing(4);
 
-  if (header != "") {
-    QLabel *headerLabel = new QLabel(header, this);
-    Style::setStyle(headerLabel, style::properties::label::menuSmall);
-    container->addWidget(headerLabel);
-  }
+	if(header != "") {
+		QLabel *headerLabel = new QLabel(header, this);
+		Style::setStyle(headerLabel, style::properties::label::menuSmall);
+		container->addWidget(headerLabel);
+	}
 
-  QWidget *controlWidget = new QWidget(this);
-  QHBoxLayout *controlLayout = new QHBoxLayout(controlWidget);
-  controlWidget->setLayout(controlLayout);
-  controlLayout->setMargin(0);
-  controlLayout->setSpacing(2);
+	QWidget *controlWidget = new QWidget(this);
+	QHBoxLayout *controlLayout = new QHBoxLayout(controlWidget);
+	controlWidget->setLayout(controlLayout);
+	controlLayout->setMargin(0);
+	controlLayout->setSpacing(2);
 
-  m_lineEdit = new QLineEdit(controlWidget);
-  QDoubleValidator *validator = new QDoubleValidator(this);
-  validator->setNotation(QDoubleValidator::StandardNotation);
-  m_lineEdit->setValidator(validator);
-  applyLineEditStyle(m_lineEdit);
+	m_lineEdit = new QLineEdit(controlWidget);
+	QDoubleValidator *validator = new QDoubleValidator(this);
+	validator->setNotation(QDoubleValidator::StandardNotation);
+	m_lineEdit->setValidator(validator);
+	applyLineEditStyle(m_lineEdit);
 
-  if (QString::compare(m_unit, "") != 0) {
-    QWidget *lineEditContainer = new QWidget(controlWidget);
-    QHBoxLayout *lineEditLayout = new QHBoxLayout(lineEditContainer);
-    lineEditContainer->setLayout(lineEditLayout);
-    lineEditLayout->setMargin(0);
-    lineEditLayout->setSpacing(0);
+	if(QString::compare(m_unit, "") != 0) {
+		QWidget *lineEditContainer = new QWidget(controlWidget);
+		QHBoxLayout *lineEditLayout = new QHBoxLayout(lineEditContainer);
+		lineEditContainer->setLayout(lineEditLayout);
+		lineEditLayout->setMargin(0);
+		lineEditLayout->setSpacing(0);
 
-    m_unitLabel = new QLabel(m_unit, controlWidget);
-    applyUnitLabelStyle(m_unitLabel);
+		m_unitLabel = new QLabel(m_unit, controlWidget);
+		applyUnitLabelStyle(m_unitLabel);
 
-    m_lineEdit->setTextMargins(12, 4, 0, 4);
+		m_lineEdit->setTextMargins(12, 4, 0, 4);
 
-    lineEditLayout->addWidget(m_lineEdit);
-    lineEditLayout->addWidget(m_unitLabel);
-    controlLayout->addWidget(lineEditContainer);
-  } else {
-    controlLayout->addWidget(m_lineEdit);
-  }
+		lineEditLayout->addWidget(m_lineEdit);
+		lineEditLayout->addWidget(m_unitLabel);
+		controlLayout->addWidget(lineEditContainer);
+	} else {
+		controlLayout->addWidget(m_lineEdit);
+	}
 
-  m_minusButton = new QPushButton(controlWidget);
-  m_minusButton->setIcon(QIcon(":/admt/minus.svg"));
-  applyPushButtonStyle(m_minusButton);
+	m_minusButton = new QPushButton(controlWidget);
+	m_minusButton->setIcon(QIcon(":/admt/minus.svg"));
+	applyPushButtonStyle(m_minusButton);
 
-  m_plusButton = new QPushButton(controlWidget);
-  m_plusButton->setIcon(QIcon(":/admt/plus.svg"));
-  applyPushButtonStyle(m_plusButton, 0, 4, 0, 4);
+	m_plusButton = new QPushButton(controlWidget);
+	m_plusButton->setIcon(QIcon(":/admt/plus.svg"));
+	applyPushButtonStyle(m_plusButton, 0, 4, 0, 4);
 
-  controlLayout->addWidget(m_minusButton);
-  controlLayout->addWidget(m_plusButton);
+	controlLayout->addWidget(m_minusButton);
+	controlLayout->addWidget(m_plusButton);
 
-  container->addWidget(controlWidget);
+	container->addWidget(controlWidget);
 
-  setValue(m_value);
-  connect(m_lineEdit, SIGNAL(editingFinished()), SLOT(onLineEditTextEdited()));
-  connect(m_minusButton, SIGNAL(clicked()), SLOT(onMinusButtonPressed()));
-  connect(m_plusButton, SIGNAL(clicked()), SLOT(onPlusButtonPressed()));
+	setValue(m_value);
+	connect(m_lineEdit, SIGNAL(editingFinished()), SLOT(onLineEditTextEdited()));
+	connect(m_minusButton, SIGNAL(clicked()), SLOT(onMinusButtonPressed()));
+	connect(m_plusButton, SIGNAL(clicked()), SLOT(onPlusButtonPressed()));
 }
 
-void HorizontalSpinBox::onMinusButtonPressed() {
-  m_value--;
-  setValue(m_value);
-  Q_EMIT m_lineEdit->editingFinished();
+void HorizontalSpinBox::onMinusButtonPressed()
+{
+	m_value--;
+	setValue(m_value);
+	Q_EMIT m_lineEdit->editingFinished();
 }
 
-void HorizontalSpinBox::onPlusButtonPressed() {
-  m_value++;
-  setValue(m_value);
-  Q_EMIT m_lineEdit->editingFinished();
+void HorizontalSpinBox::onPlusButtonPressed()
+{
+	m_value++;
+	setValue(m_value);
+	Q_EMIT m_lineEdit->editingFinished();
 }
 
-void HorizontalSpinBox::onLineEditTextEdited() {
-  QLineEdit *lineEdit = static_cast<QLineEdit *>(QObject::sender());
-  bool ok;
-  double value = lineEdit->text().toDouble(&ok);
-  if (ok) {
-    m_value = value;
-  }
-  setValue(m_value);
+void HorizontalSpinBox::onLineEditTextEdited()
+{
+	QLineEdit *lineEdit = static_cast<QLineEdit *>(QObject::sender());
+	bool ok;
+	double value = lineEdit->text().toDouble(&ok);
+	if(ok) {
+		m_value = value;
+	}
+	setValue(m_value);
 }
 
-void HorizontalSpinBox::setValue(double value) {
-  m_lineEdit->setText(QString::number(value));
+void HorizontalSpinBox::setValue(double value) { m_lineEdit->setText(QString::number(value)); }
+
+void HorizontalSpinBox::setEnabled(double value)
+{
+	m_lineEdit->setEnabled(value);
+	m_minusButton->setEnabled(value);
+	m_plusButton->setEnabled(value);
+	if(QString::compare(m_unit, "") != 0) {
+		applyUnitLabelStyle(m_unitLabel, value);
+	}
 }
 
-void HorizontalSpinBox::setEnabled(double value) {
-  m_lineEdit->setEnabled(value);
-  m_minusButton->setEnabled(value);
-  m_plusButton->setEnabled(value);
-  if (QString::compare(m_unit, "") != 0) {
-    applyUnitLabelStyle(m_unitLabel, value);
-  }
-}
-
-void HorizontalSpinBox::applyLineEditStyle(QLineEdit *widget) {
-  QString style = QString(R"css(
+void HorizontalSpinBox::applyLineEditStyle(QLineEdit *widget)
+{
+	QString style = QString(R"css(
                                 QLineEdit {
 									font-family: Open Sans;
 									font-size: 16px;
@@ -145,21 +150,18 @@ void HorizontalSpinBox::applyLineEditStyle(QLineEdit *widget) {
 									color: #9c4600;
                                 }
                                 )css");
-  style = style.replace(QString("&&colorname&&"),
-                        Style::getAttribute(json::global::ch0));
-  widget->setStyleSheet(style);
-  widget->setFixedHeight(30);
-  widget->setAlignment(Qt::AlignRight);
-  widget->setContentsMargins(0, 0, 0, 0);
-  widget->setTextMargins(6, 4, 6, 4);
+	style = style.replace(QString("&&colorname&&"), Style::getAttribute(json::global::ch0));
+	widget->setStyleSheet(style);
+	widget->setFixedHeight(30);
+	widget->setAlignment(Qt::AlignRight);
+	widget->setContentsMargins(0, 0, 0, 0);
+	widget->setTextMargins(6, 4, 6, 4);
 }
 
-void HorizontalSpinBox::applyPushButtonStyle(QPushButton *widget,
-                                             int topLeftBorderRadius,
-                                             int topRightBorderRadius,
-                                             int bottomLeftBorderRadius,
-                                             int bottomRightBorderRadius) {
-  QString style = QString(R"css(
+void HorizontalSpinBox::applyPushButtonStyle(QPushButton *widget, int topLeftBorderRadius, int topRightBorderRadius,
+					     int bottomLeftBorderRadius, int bottomRightBorderRadius)
+{
+	QString style = QString(R"css(
                                 QPushButton{
                                     background-color: black;
                                     font-family: Open Sans;
@@ -177,24 +179,19 @@ void HorizontalSpinBox::applyPushButtonStyle(QPushButton *widget,
 									color: #2d3d9c;
                                 }
                                 )css");
-  style =
-      style.replace(QString("&&colorname&&"),
-                    Style::getAttribute(json::theme::interactive_primary_idle));
-  style = style.replace(QString("&&topLeftBorderRadius&&"),
-                        QString::number(topLeftBorderRadius));
-  style = style.replace(QString("&&topRightBorderRadius&&"),
-                        QString::number(topRightBorderRadius));
-  style = style.replace(QString("&&bottomLeftBorderRadius&&"),
-                        QString::number(bottomLeftBorderRadius));
-  style = style.replace(QString("&&bottomRightBorderRadius&&"),
-                        QString::number(bottomRightBorderRadius));
-  widget->setStyleSheet(style);
-  widget->setFixedHeight(30);
-  widget->setFixedWidth(38);
+	style = style.replace(QString("&&colorname&&"), Style::getAttribute(json::theme::interactive_primary_idle));
+	style = style.replace(QString("&&topLeftBorderRadius&&"), QString::number(topLeftBorderRadius));
+	style = style.replace(QString("&&topRightBorderRadius&&"), QString::number(topRightBorderRadius));
+	style = style.replace(QString("&&bottomLeftBorderRadius&&"), QString::number(bottomLeftBorderRadius));
+	style = style.replace(QString("&&bottomRightBorderRadius&&"), QString::number(bottomRightBorderRadius));
+	widget->setStyleSheet(style);
+	widget->setFixedHeight(30);
+	widget->setFixedWidth(38);
 }
 
-void HorizontalSpinBox::applyUnitLabelStyle(QLabel *widget, bool isEnabled) {
-  QString style = QString(R"css(
+void HorizontalSpinBox::applyUnitLabelStyle(QLabel *widget, bool isEnabled)
+{
+	QString style = QString(R"css(
                                 background-color: &&backgroundcolor&&;
                                 font-family: Open Sans;
 								font-size: 16px;
@@ -202,18 +199,17 @@ void HorizontalSpinBox::applyUnitLabelStyle(QLabel *widget, bool isEnabled) {
                                 color: &&colorname&&;
                                 border: none;
                                 )css");
-  if (isEnabled) {
-    style = style.replace(QString("&&backgroundcolor&&"), "black");
-    style = style.replace(QString("&&colorname&&"),
-                          Style::getAttribute(json::global::ch0));
-  } else {
-    style = style.replace(QString("&&backgroundcolor&&"), "#18181d");
-    style = style.replace(QString("&&colorname&&"), "#9c4600");
-  }
-  widget->setStyleSheet(style);
-  widget->setFixedHeight(30);
-  widget->setAlignment(Qt::AlignRight);
-  widget->setContentsMargins(0, 4, 12, 4);
+	if(isEnabled) {
+		style = style.replace(QString("&&backgroundcolor&&"), "black");
+		style = style.replace(QString("&&colorname&&"), Style::getAttribute(json::global::ch0));
+	} else {
+		style = style.replace(QString("&&backgroundcolor&&"), "#18181d");
+		style = style.replace(QString("&&colorname&&"), "#9c4600");
+	}
+	widget->setStyleSheet(style);
+	widget->setFixedHeight(30);
+	widget->setAlignment(Qt::AlignRight);
+	widget->setContentsMargins(0, 4, 12, 4);
 }
 
 QLineEdit *HorizontalSpinBox::lineEdit() { return m_lineEdit; }
