77026f4ab602e6778c8ad893d08a73735f442d34
John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>
2025-02-27 14:10:16 +0800

    admt: Applied clang-format for C++ files
    Signed-off-by: John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>

--- plugins/admt/test/tst_pluginloader.cpp
+++ plugins/admt/test/tst_pluginloader.cpp
@@ -27,94 +27,104 @@
 
 using namespace scopy;
 
-class TST_ADMTPlugin : public QObject {
-  Q_OBJECT
+class TST_ADMTPlugin : public QObject
+{
+	Q_OBJECT
 private Q_SLOTS:
-  void fileExists();
-  void isLibrary();
-  void loaded();
-  void className();
-  void instanceNotNull();
-  void multipleInstances();
-  void qobjectcast_to_plugin();
-  void clone();
-  void name();
-  void metadata();
+	void fileExists();
+	void isLibrary();
+	void loaded();
+	void className();
+	void instanceNotNull();
+	void multipleInstances();
+	void qobjectcast_to_plugin();
+	void clone();
+	void name();
+	void metadata();
 };
 
 #define PLUGIN_LOCATION "../../plugins"
 #define FILENAME PLUGIN_LOCATION "/libscopy-admtplugin.so"
 
-void TST_ADMTPlugin::fileExists() {
-  QFile f(FILENAME);
-  bool ret;
-  ret = f.open(QIODevice::ReadOnly);
-  if (ret)
-    f.close();
-  QVERIFY(ret);
+void TST_ADMTPlugin::fileExists()
+{
+	QFile f(FILENAME);
+	bool ret;
+	ret = f.open(QIODevice::ReadOnly);
+	if(ret)
+		f.close();
+	QVERIFY(ret);
 }
 
 void TST_ADMTPlugin::isLibrary() { QVERIFY(QLibrary::isLibrary(FILENAME)); }
 
-void TST_ADMTPlugin::className() {
-  QPluginLoader qp(FILENAME, this);
-  QVERIFY(qp.metaData().value("className") == "ADMTPlugin");
+void TST_ADMTPlugin::className()
+{
+	QPluginLoader qp(FILENAME, this);
+	QVERIFY(qp.metaData().value("className") == "ADMTPlugin");
 }
 
-void TST_ADMTPlugin::loaded() {
-  QPluginLoader qp(FILENAME, this);
-  qp.load();
-  QVERIFY(qp.isLoaded());
+void TST_ADMTPlugin::loaded()
+{
+	QPluginLoader qp(FILENAME, this);
+	qp.load();
+	QVERIFY(qp.isLoaded());
 }
 
-void TST_ADMTPlugin::instanceNotNull() {
-  QPluginLoader qp(FILENAME, this);
-  QVERIFY(qp.instance() != nullptr);
+void TST_ADMTPlugin::instanceNotNull()
+{
+	QPluginLoader qp(FILENAME, this);
+	QVERIFY(qp.instance() != nullptr);
 }
 
-void TST_ADMTPlugin::multipleInstances() {
-  QPluginLoader qp1(FILENAME, this);
-  QPluginLoader qp2(FILENAME, this);
+void TST_ADMTPlugin::multipleInstances()
+{
+	QPluginLoader qp1(FILENAME, this);
+	QPluginLoader qp2(FILENAME, this);
 
-  QVERIFY(qp1.instance() == qp2.instance());
+	QVERIFY(qp1.instance() == qp2.instance());
 }
 
-void TST_ADMTPlugin::qobjectcast_to_plugin() {
-  QPluginLoader qp(FILENAME, this);
-  auto instance = qobject_cast<Plugin *>(qp.instance());
-  QVERIFY(instance != nullptr);
+void TST_ADMTPlugin::qobjectcast_to_plugin()
+{
+	QPluginLoader qp(FILENAME, this);
+	auto instance = qobject_cast<Plugin *>(qp.instance());
+	QVERIFY(instance != nullptr);
 }
 
-void TST_ADMTPlugin::clone() {
-  QPluginLoader qp(FILENAME, this);
+void TST_ADMTPlugin::clone()
+{
+	QPluginLoader qp(FILENAME, this);
 
-  Plugin *p1 = nullptr, *p2 = nullptr;
-  auto original = qobject_cast<Plugin *>(qp.instance());
-  p1 = original->clone();
-  QVERIFY(p1 != nullptr);
-  p2 = original->clone();
-  QVERIFY(p2 != nullptr);
-  QVERIFY(p1 != p2);
+	Plugin *p1 = nullptr, *p2 = nullptr;
+	auto original = qobject_cast<Plugin *>(qp.instance());
+	p1 = original->clone();
+	QVERIFY(p1 != nullptr);
+	p2 = original->clone();
+	QVERIFY(p2 != nullptr);
+	QVERIFY(p1 != p2);
 }
 
-void TST_ADMTPlugin::name() {
-  QPluginLoader qp(FILENAME, this);
+void TST_ADMTPlugin::name()
+{
+	QPluginLoader qp(FILENAME, this);
 
-  Plugin *p1 = nullptr, *p2 = nullptr;
-  auto original = qobject_cast<Plugin *>(qp.instance());
-  p1 = original->clone();
-  qDebug() << p1->name();
+	Plugin *p1 = nullptr, *p2 = nullptr;
+	auto original = qobject_cast<Plugin *>(qp.instance());
+	p1 = original->clone();
+	qDebug() << p1->name();
 }
 
-void TST_ADMTPlugin::metadata() {
-  QPluginLoader qp(FILENAME, this);
+void TST_ADMTPlugin::metadata()
+{
+	QPluginLoader qp(FILENAME, this);
 
-  Plugin *p1 = nullptr, *p2 = nullptr;
-  auto original = qobject_cast<Plugin *>(qp.instance());
-  original->initMetadata();
-  p1 = original->clone();
-  qDebug() << p1->metadata();
-  QVERIFY(!p1->metadata().isEmpty());
+	Plugin *p1 = nullptr, *p2 = nullptr;
+	auto original = qobject_cast<Plugin *>(qp.instance());
+	original->initMetadata();
+	p1 = original->clone();
+	qDebug() << p1->metadata();
+	QVERIFY(!p1->metadata().isEmpty());
 }
 
 QTEST_MAIN(TST_ADMTPlugin)
