dce09f3b70895cdb57b52a30e59f57bf189426dc
John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>
2025-02-27 12:52:37 +0800

    admt: Format C++ and CMakeLists
    Signed-off-by: John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>
    Signed-off-by: JJuanill <JohnLloyd.Juanillo@analog.com>

--- plugins/admt/src/admtplugin.cpp
+++ plugins/admt/src/admtplugin.cpp
@@ -23,8 +23,8 @@
 #include "admtcontroller.h"
 #include "harmoniccalibration.h"
 
-#include <QLoggingCategory>
 #include <QLabel>
+#include <QLoggingCategory>
 
 #include <iioutil/connectionprovider.h>
 
@@ -34,121 +34,120 @@ using namespace scopy::admt;
 
 const bool isDebug = false;
 
-bool ADMTPlugin::compatible(QString m_param, QString category)
-{
-	m_name = "ADMT4000";
-	bool ret = false;
-	Connection *conn = ConnectionProvider::GetInstance()->open(m_param);
+bool ADMTPlugin::compatible(QString m_param, QString category) {
+  m_name = "ADMT4000";
+  bool ret = false;
+  Connection *conn = ConnectionProvider::GetInstance()->open(m_param);
 
-	if(!conn) {
-		qWarning(CAT_ADMTPLUGIN) << "No context available for ADMT";
-		return false;
-	}
+  if (!conn) {
+    qWarning(CAT_ADMTPLUGIN) << "No context available for ADMT";
+    return false;
+  }
 
-	iio_device *admtDevice = iio_context_find_device(conn->context(), "admt4000");
-	if(admtDevice) {
-		ret = true;
-	}
+  iio_device *admtDevice = iio_context_find_device(conn->context(), "admt4000");
+  if (admtDevice) {
+    ret = true;
+  }
 
-	ConnectionProvider::close(m_param);
-	if(isDebug) return true;
-	return ret;
+  ConnectionProvider::close(m_param);
+  if (isDebug)
+    return true;
+  return ret;
 }
 
-bool ADMTPlugin::loadPage()
-{
-	// Here you must write the code for the plugin info page 
-	// Below is an example for an iio device
-	/*m_page = new QWidget();
-	m_page->setLayout(new QVBoxLayout(m_page));
-	m_page->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-	m_infoPage = new InfoPage(m_page);
-	m_infoPage->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-	m_page->layout()->addWidget(m_infoPage);
-	m_page->layout()->addItem(new QSpacerItem(0, 0, QSizePolicy::Preferred, QSizePolicy::Expanding));
+bool ADMTPlugin::loadPage() {
+  // Here you must write the code for the plugin info page
+  // Below is an example for an iio device
+  /*m_page = new QWidget();
+  m_page->setLayout(new QVBoxLayout(m_page));
+  m_page->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  m_infoPage = new InfoPage(m_page);
+  m_infoPage->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+  m_page->layout()->addWidget(m_infoPage);
+  m_page->layout()->addItem(new QSpacerItem(0, 0, QSizePolicy::Preferred,
+  QSizePolicy::Expanding));
 
-	auto cp = ContextProvider::GetInstance();
-	struct iio_context *context = cp->open(m_param);
-	ssize_t attributeCount = iio_context_get_attrs_count(context);
-	for(int i = 0; i < attributeCount; ++i) {
-		const char *name;
-		const char *value;
-		int ret = iio_context_get_attr(context, i, &name, &value);
-		if(ret < 0) {
-			qWarning(CAT_ADMTPLUGIN) << "Could not read attribute with index:" << i;
-			continue;
-		}
+  auto cp = ContextProvider::GetInstance();
+  struct iio_context *context = cp->open(m_param);
+  ssize_t attributeCount = iio_context_get_attrs_count(context);
+  for(int i = 0; i < attributeCount; ++i) {
+          const char *name;
+          const char *value;
+          int ret = iio_context_get_attr(context, i, &name, &value);
+          if(ret < 0) {
+                  qWarning(CAT_ADMTPLUGIN) << "Could not read attribute with
+  index:" << i; continue;
+          }
 
-		m_infoPage->update(name, value);
-	}
-	cp->close(m_param);
-	m_page->ensurePolished();*/
-	return true;
+          m_infoPage->update(name, value);
+  }
+  cp->close(m_param);
+  m_page->ensurePolished();*/
+  return true;
 }
 
-bool ADMTPlugin::loadIcon()
-{
-	SCOPY_PLUGIN_ICON(":/gui/icons/adalm.svg");
-	return true;
+bool ADMTPlugin::loadIcon() {
+  SCOPY_PLUGIN_ICON(":/gui/icons/adalm.svg");
+  return true;
 }
 
-void ADMTPlugin::loadToolList()
-{
-	m_toolList.append(
-		SCOPY_NEW_TOOLMENUENTRY("harmoniccalibration", "Harmonic Calibration", ":/gui/icons/scopy-default/icons/tool_oscilloscope.svg"));
+void ADMTPlugin::loadToolList() {
+  m_toolList.append(SCOPY_NEW_TOOLMENUENTRY(
+      "harmoniccalibration", "Harmonic Calibration",
+      ":/gui/icons/scopy-default/icons/tool_oscilloscope.svg"));
 }
 
-void ADMTPlugin::unload() { /*delete m_infoPage;*/ }
-
-QString ADMTPlugin::description() { return "Plugin for ADMT Harmonic Calibration"; }
-
-bool ADMTPlugin::onConnect()
-{
-	// This method is called when you try to connect to a device and the plugin is 
-	// compatible to that device 
-	// In case of success the function must return true and false otherwise 
-    	
-	Connection *conn = ConnectionProvider::GetInstance()->open(m_param);
-	if(conn == nullptr)
-		return false;
-	m_ctx = conn->context();
-	m_toolList[0]->setEnabled(true);
-	m_toolList[0]->setRunBtnVisible(false);
-
-	m_admtController = new ADMTController(m_param, this);
-	m_admtController->connectADMT();
-	harmonicCalibration = new HarmonicCalibration(m_admtController, isDebug);
-	m_toolList[0]->setTool(harmonicCalibration);
-	
-	return true;
+void ADMTPlugin::unload() { /*delete m_infoPage;*/
 }
 
-bool ADMTPlugin::onDisconnect()
-{
-	// This method is called when the disconnect button is pressed
-	// It must remove all connections that were established on the connection
-
-	dynamic_cast<HarmonicCalibration *>(harmonicCalibration)->requestDisconnect();
-	
-	for(auto &tool : m_toolList) {
-		tool->setEnabled(false);
-		tool->setRunning(false);
-		tool->setRunBtnVisible(false);
-		QWidget *w = tool->tool();
-		if(w) {
-			tool->setTool(nullptr);
-			delete(w);
-		}
-	}
-
-	m_admtController->disconnectADMT();
-	return true;
+QString ADMTPlugin::description() {
+  return "Plugin for ADMT Harmonic Calibration";
 }
 
-void ADMTPlugin::initMetadata()
-{
-	loadMetadata(
-		R"plugin(
+bool ADMTPlugin::onConnect() {
+  // This method is called when you try to connect to a device and the plugin is
+  // compatible to that device
+  // In case of success the function must return true and false otherwise
+
+  Connection *conn = ConnectionProvider::GetInstance()->open(m_param);
+  if (conn == nullptr)
+    return false;
+  m_ctx = conn->context();
+  m_toolList[0]->setEnabled(true);
+  m_toolList[0]->setRunBtnVisible(false);
+
+  m_admtController = new ADMTController(m_param, this);
+  m_admtController->connectADMT();
+  harmonicCalibration = new HarmonicCalibration(m_admtController, isDebug);
+  m_toolList[0]->setTool(harmonicCalibration);
+
+  return true;
+}
+
+bool ADMTPlugin::onDisconnect() {
+  // This method is called when the disconnect button is pressed
+  // It must remove all connections that were established on the connection
+
+  dynamic_cast<HarmonicCalibration *>(harmonicCalibration)->requestDisconnect();
+
+  for (auto &tool : m_toolList) {
+    tool->setEnabled(false);
+    tool->setRunning(false);
+    tool->setRunBtnVisible(false);
+    QWidget *w = tool->tool();
+    if (w) {
+      tool->setTool(nullptr);
+      delete (w);
+    }
+  }
+
+  m_admtController->disconnectADMT();
+  return true;
+}
+
+void ADMTPlugin::initMetadata() {
+  loadMetadata(
+      R"plugin(
 	{
 	   "priority":102,
 	   "category":[
@@ -158,3 +157,5 @@ void ADMTPlugin::initMetadata()
 	}
 )plugin");
 }
+
+#include "moc_admtplugin.cpp"
