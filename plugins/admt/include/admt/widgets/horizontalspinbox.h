555f9804d2df21d9f6bfbfd7196d32c737368ee8
John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>
2025-02-27 12:52:37 +0800

    admt: Format C++ and CMakeLists
    Signed-off-by: John Lloyd Juanillo <JohnLloyd.Juanillo@analog.com>

--- plugins/admt/include/admt/widgets/horizontalspinbox.h
+++ plugins/admt/include/admt/widgets/horizontalspinbox.h
@@ -22,41 +22,44 @@
 #ifndef HORIZONTALSPINBOX_H
 #define HORIZONTALSPINBOX_H
 
-#include "scopy-admt_export.h"
+#include "../scopy-admt_export.h"
 
-#include <QWidget>
+#include <QDoubleValidator>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
 #include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QLineEdit>
-#include <QLabel>
-#include <QPushButton>
-#include <QDoubleValidator>
+#include <QWidget>
 
 namespace scopy::admt {
-    class SCOPY_ADMT_EXPORT HorizontalSpinBox : public QWidget
-    {
-        Q_OBJECT
-    public:
-        HorizontalSpinBox(QString header = "", double initialValue = 0.0, QString unit = "", QWidget *parent = nullptr);
-        QLineEdit *lineEdit();
-        void setEnabled(double value);
-    public Q_SLOTS:
-        void setValue(double);
-    protected Q_SLOTS:
-        void onMinusButtonPressed();
-        void onPlusButtonPressed();
-        void onLineEditTextEdited();
-    private:
-        double m_value = 0;
-        QString m_unit = "";
-        QLineEdit *m_lineEdit;
-        QPushButton *m_minusButton, *m_plusButton;
-        QLabel *m_unitLabel;
-        void applyLineEditStyle(QLineEdit *widget);
-        void applyPushButtonStyle(QPushButton *widget, int topLeftBorderRadius = 0, int topRightBorderRadius = 0, int bottomLeftBorderRadius = 0, int bottomRightBorderRadius = 0);
-        void applyUnitLabelStyle(QLabel *widget, bool isEnabled = true);
-    };
+class SCOPY_ADMT_EXPORT HorizontalSpinBox : public QWidget {
+  Q_OBJECT
+public:
+  HorizontalSpinBox(QString header = "", double initialValue = 0.0,
+                    QString unit = "", QWidget *parent = nullptr);
+  QLineEdit *lineEdit();
+  void setEnabled(double value);
+public Q_SLOTS:
+  void setValue(double);
+protected Q_SLOTS:
+  void onMinusButtonPressed();
+  void onPlusButtonPressed();
+  void onLineEditTextEdited();
+
+private:
+  double m_value = 0;
+  QString m_unit = "";
+  QLineEdit *m_lineEdit;
+  QPushButton *m_minusButton, *m_plusButton;
+  QLabel *m_unitLabel;
+  void applyLineEditStyle(QLineEdit *widget);
+  void applyPushButtonStyle(QPushButton *widget, int topLeftBorderRadius = 0,
+                            int topRightBorderRadius = 0,
+                            int bottomLeftBorderRadius = 0,
+                            int bottomRightBorderRadius = 0);
+  void applyUnitLabelStyle(QLabel *widget, bool isEnabled = true);
+};
 } // namespace scopy::admt
 
 #endif // HORIZONTALSPINBOX_H
