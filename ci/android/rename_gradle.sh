7fa04747da96b922e55f5fbb5001d7f1e9206acd
Bindea Cristian <cristian.bindea@analog.com>
2025-03-24 14:26:36 +0200

    ci: android test
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

---
+++ ci/android/rename_gradle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -xe
+source ./android_toolchain.sh
+
+rename_gradle() {
+	echo "Patching to use gradle-7.4"
+
+	pushd $QT_INSTALL_PREFIX_NO_POSTFIX_arm64_v8a/src/3rdparty/gradle/gradle/wrapper
+	sed -i "s|https\\\:\/\/services\.gradle\.org\/distributions\/gradle.*\.zip|https\://services.gradle.org/distributions/gradle-7.4-all.zip|g" gradle-wrapper.properties
+	pushd $QT_INSTALL_PREFIX_NO_POSTFIX_armv7a/src/3rdparty/gradle/gradle/wrapper
+	sed -i "s|https\\\:\/\/services\.gradle\.org\/distributions\/gradle.*\.zip|https\://services.gradle.org/distributions/gradle-7.4-all.zip|g" gradle-wrapper.properties
+	popd
+}
+
+patch_sdk_build_tools_revision() {
+	echo "Patching qt android to support sdkBuildToolsRevision"
+	# https://bugreports.qt.io/browse/QTBUG-84302
+
+	pushd $QT_INSTALL_PREFIX/lib/cmake/Qt5Core/
+	cp Qt5AndroidSupport.cmake Qt5AndroidSupport.cmake.backup
+	sed '59 a "sdkBuildToolsRevision": "@ANDROID_SDK_BUILD_TOOLS_REVISION@",' Qt5AndroidSupport.cmake.backup  > Qt5AndroidSupport.cmake
+	rm -rf Qt5AndroidSupport.cmake.backup
+	popd
+}
+
+#rename_gradle
+#patch_sdk_build_tools_revision
+
