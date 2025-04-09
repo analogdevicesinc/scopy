991f21c39bf930a1ee7447a552d13f67484f8861
SeptimiuVana <vana.septimiu@analog.com>
2025-04-09 12:15:40 +0300

    plugins: add minimum code for imu
    Signed-off-by: SeptimiuVana <vana.septimiu@analog.com>

---
+++ plugins/imu/src/imuanalyzer.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2024 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see https://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "imuanalyzer.h"
+
+#include <QBoxLayout>
+#include <QJsonDocument>
+#include <QLabel>
+#include <QLoggingCategory>
+#include <QPushButton>
+#include <QSpacerItem>
+
+#include <pluginbase/messagebroker.h>
+#include <pluginbase/preferences.h>
+
+Q_LOGGING_CATEGORY(CAT_TESTPLUGIN, "IMUAnalyzer");
+using namespace scopy;
+
+bool IMUAnalyzer::compatible(QString m_param, QString category)
+{
+	qDebug(CAT_TESTPLUGIN) << "compatible";
+	return true;
+}
+
+void IMUAnalyzer::loadToolList()
+{
+	m_toolList.append(SCOPY_NEW_TOOLMENUENTRY("IMUAnalyzer_tool", "imuanalyzer", ":/gui/icons/home.svg"));
+}
+
+bool IMUAnalyzer::onConnect()
+{
+	m_toolList[0]->setEnabled(true);
+	m_toolList[0]->setTool(new QLabel("IMUAnalyzer"));
+	return true;
+}
+
+bool IMUAnalyzer::onDisconnect()
+{
+	m_toolList[0]->setEnabled(false);
+	m_toolList[0]->setTool(nullptr);
+	return true;
+}
+
+void IMUAnalyzer::initMetadata() // not actually needed - putting it here to set priority
+{
+	loadMetadata(
+		R"plugin(
+	{
+	   "priority":100,
+	   "category":[
+	      "iio"
+	   ]
+	}
+)plugin");
+}
+
+#include "moc_imuanalyzer.cpp"
