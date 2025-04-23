eaf0a61b2ad01a22716481479c9dd3d3520e09e1
andreidanila1 <andrei.danila@analog.com>
2025-04-23 16:25:35 +0300

    cmake/Modules/ScopyStyle: Added build folder as argument.
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- cmake/Modules/ScopyStyle.cmake
+++ cmake/Modules/ScopyStyle.cmake
@@ -24,11 +24,17 @@ endif()
 set(__INCLUDED_SCOPY_STYLE_CMAKE TRUE)
 
 # option can be --core or --plugin
-function(generate_style option style_folder headers_folder)
+function(
+	generate_style
+	option
+	style_folder
+	headers_folder
+	build_folder
+)
 	find_package(Python3 COMPONENTS Interpreter)
 	execute_process(
 		COMMAND ${Python3_EXECUTABLE} ${CMAKE_SOURCE_DIR}/tools/style_generator.py ${option} ${style_folder}
-			${headers_folder} ${CMAKE_BINARY_DIR} RESULT_VARIABLE ret
+			${headers_folder} ${build_folder} RESULT_VARIABLE ret
 	)
 	if(NOT ret EQUAL "0")
 		message(FATAL_ERROR "Failed to generate style files! error: ${ret}")
