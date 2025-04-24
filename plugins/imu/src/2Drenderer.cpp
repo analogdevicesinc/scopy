11d4f3f350ecf66c49ec10851a23df710717f471
SeptimiuVana <vana.septimiu@analog.com>
2025-04-24 17:42:16 +0300

    imu: add basic shapes to 2d render
    Signed-off-by: SeptimiuVana <vana.septimiu@analog.com>

--- plugins/imu/src/2Drenderer.cpp
+++ plugins/imu/src/2Drenderer.cpp
@@ -3,6 +3,24 @@
 using namespace scopy;
 
 BubbleLevelRenderer::BubbleLevelRenderer(QWidget *parent) : QWidget{parent} {
+	QHBoxLayout *lay = new QHBoxLayout(this);
+	lay->setMargin(0);
+	setLayout(lay);
+
+
+	m_rot = {0.0f, 0.0f, 0.0f};
+	//lay->addWidget(this);
+}
+
+void BubbleLevelRenderer::setRot(rotation rot){
+	m_rot = rot;
+	this->update();
+}
+
+void BubbleLevelRenderer::paintEvent(QPaintEvent *event){
+
 	QPainter painter(this);
-	painter.drawEllipse(QPointF(50, 50), 20, 20);
+	painter.setBrush(Qt::red);
+	painter.drawEllipse(QPointF(200.0f + m_rot.rotX, 100.0f), 100, 100);
+	painter.drawEllipse(QPointF(200.0f, 400 + m_rot.rotY), 100, 100);
 }
