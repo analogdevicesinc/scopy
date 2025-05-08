353c48e43a26d9e223b6462e21427facd27db1b0
andreidanila1 <andrei.danila@analog.com>
2025-05-08 16:35:06 +0300

    pkgmanager: Use GridWidget and CardWidget in pkgmanager GUI.
    This commit is kept separate from the previous one to allow for possible
    modifications. Once development is finalized, squash this with the
    previous commit, and then with the pkgmanage-gui commit. Also ensure to
    handle the porting commit correctly—where all files were initially added
    to the pkgmanager folder.
    
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- core/src/pkgmanager/pkggridwidget.cpp
+++ core/src/pkgmanager/pkggridwidget.cpp
@@ -29,40 +29,48 @@
 using namespace scopy;
 
 PkgGridWidget::PkgGridWidget(QWidget *parent)
-	: QWidget(parent)
-	, m_maxCol(1)
+	: GridWidget(parent)
 {
-	QVBoxLayout *lay = new QVBoxLayout(this);
-	lay->setMargin(0);
-
-	QWidget *pkgsW = new QWidget(this);
-	m_layout = new QGridLayout(pkgsW);
-	m_layout->setMargin(0);
-
-	lay->addWidget(pkgsW);
-	lay->addItem(new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding));
-
 	m_maxCol = Preferences::get("pkg_menu_columns").toInt();
-	connect(Preferences::GetInstance(), &Preferences::preferenceChanged, this, &PkgGridWidget::colNumberChanged);
+	connect(Preferences::GetInstance(), &Preferences::preferenceChanged, this, [this](QString pref, QVariant val) {
+		if(pref == "pkg_menu_columns") {
+			setColumnCount(val.toInt());
+		}
+	});
 }
 
 PkgGridWidget::~PkgGridWidget() {}
 
-void PkgGridWidget::addPkg(PkgItemWidget *pkgItem)
+void PkgGridWidget::searchPkg(const QStringList &fields, const QStringList &options)
 {
-	int gridSize = m_pkgMap.size();
-	m_layout->addWidget(pkgItem, gridSize / m_maxCol, gridSize % m_maxCol);
-	m_pkgMap.insert(pkgItem->id(), pkgItem);
+	hideAll();
+	int counter = 0;
+	for(auto it = m_widgetMap.begin(); it != m_widgetMap.end(); ++it) {
+		PkgCard *pkgCard = dynamic_cast<PkgCard *>(it.value());
+		if(!pkgCard) {
+			continue;
+		}
+		QVariantMap pkgMetadata = pkgCard->metadata();
+		bool matchFound = false;
+		for(const QString &field : fields) {
+			if(isFieldMatch(pkgMetadata, field, options)) {
+				matchFound = true;
+				break;
+			}
+		}
+		if(matchFound || options.isEmpty()) {
+			m_layout->addWidget(it.value(), counter / m_maxCol, counter % m_maxCol);
+			it.value()->show();
+			counter++;
+		}
+	}
 }
 
-void PkgGridWidget::removePkg(const QString &pkgName)
+void PkgGridWidget::updatePkgsStyle(bool property)
 {
-	if(!m_pkgMap.contains(pkgName)) {
-		return;
+	for(auto it = m_widgetMap.begin(); it != m_widgetMap.end(); ++it) {
+		Style::setStyle(it.value(), style::properties::widget::cardWidget, property);
 	}
-	QWidget *w = m_pkgMap.take(pkgName);
-	m_layout->removeWidget(w);
-	w->deleteLater();
 }
 
 bool PkgGridWidget::isFieldMatch(const QVariantMap &pkgMetadata, const QString &field, const QStringList &options)
@@ -82,78 +90,12 @@ bool PkgGridWidget::isFieldMatch(const QVariantMap &pkgMetadata, const QString &
 	return false;
 }
 
-void PkgGridWidget::searchPkg(const QStringList &fields, const QStringList &options)
-{
-	// Hide all the packages
-	hideAll();
-	// Reorganize the widgets within the layout
-	int counter = 0;
-	for(auto it = m_pkgMap.begin(); it != m_pkgMap.end(); ++it) {
-		QVariantMap pkgMetadata = it.value()->metadata();
-		bool matchFound = false;
-		for(const QString &field : fields) {
-			if(isFieldMatch(pkgMetadata, field, options)) {
-				matchFound = true;
-				break;
-			}
-		}
-		if(matchFound || options.isEmpty()) {
-			showPkg(it.value(), counter++);
-		}
-	}
-
-	Q_ASSERT(counter <= m_pkgMap.size());
-}
-
-void PkgGridWidget::colNumberChanged(QString pref, QVariant val)
-{
-	int col = val.toInt();
-	if(pref == "pkg_menu_columns") {
-		m_maxCol = col;
-		rebuildLayout();
-	}
-}
-
-void PkgGridWidget::hideAll()
-{
-	for(auto it = m_pkgMap.begin(); it != m_pkgMap.end(); ++it) {
-		it.value()->hide();
-	}
-}
-
-void PkgGridWidget::rebuildLayout()
-{
-	hideAll();
-	int counter = 0;
-	for(auto it = m_pkgMap.begin(); it != m_pkgMap.end(); ++it) {
-		showPkg(it.value(), counter++);
-	}
-	Q_ASSERT(counter <= m_pkgMap.size());
-}
-
-void PkgGridWidget::showPkg(QWidget *pkg, int index)
-{
-	m_layout->addWidget(pkg, index / m_maxCol, index % m_maxCol);
-	pkg->show();
-}
-
 bool PkgGridWidget::isSearchMatch(const QString &pkgVal, const QStringList &searchingValues)
 {
-	bool matchFound = false;
 	for(const QString &searchingVal : searchingValues) {
 		if(pkgVal.contains(searchingVal, Qt::CaseInsensitive)) {
-			matchFound = true;
-			break;
+			return true;
 		}
 	}
-	return matchFound;
+	return false;
 }
-
-void PkgGridWidget::updatePkgsStyle(bool property)
-{
-	for(auto it = m_pkgMap.begin(); it != m_pkgMap.end(); ++it) {
-		Style::setStyle(it.value(), style::properties::widget::pkgItem, property);
-	}
-}
-
-#include "moc_pkggridwidget.cpp"
