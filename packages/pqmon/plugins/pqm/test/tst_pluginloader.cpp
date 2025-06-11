41b77bca6aba20b8099f9413218dbc00d2125300
andreidanila1 <andrei.danila@analog.com>
2025-06-11 15:50:45 +0300

    pqmon: Change the location of plugins in tst_pluginloader.
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- packages/pqmon/plugins/pqm/test/tst_pluginloader.cpp
+++ packages/pqmon/plugins/pqm/test/tst_pluginloader.cpp
@@ -43,7 +43,7 @@ private Q_SLOTS:
 	void metadata();
 };
 
-#define PLUGIN_LOCATION "../../plugins"
+#define PLUGIN_LOCATION "../.."
 #define FILENAME PLUGIN_LOCATION "/libscopy-pqm.so"
 
 void TST_PQMPlugin::fileExists()
