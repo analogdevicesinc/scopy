d6143d974147f7153c4298570a42692c5c6495db
andreidanila1 <andrei.danila@analog.com>
2025-06-11 16:32:50 +0300

    swiot: Change the location of plugins in tst_pluginloader.
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- packages/swiot/plugins/swiot/test/tst_pluginloader.cpp
+++ packages/swiot/plugins/swiot/test/tst_pluginloader.cpp
@@ -43,7 +43,7 @@ private Q_SLOTS:
 	void metadata();
 };
 
-#define PLUGIN_LOCATION "../../plugins"
+#define PLUGIN_LOCATION "../.."
 #define FILENAME PLUGIN_LOCATION "/libscopy-swiot.so"
 
 void TST_SWIOTPlugin::fileExists()
