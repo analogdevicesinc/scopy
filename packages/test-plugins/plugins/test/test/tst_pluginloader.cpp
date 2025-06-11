8e47e4753650e74833cda52e99dbd2ce8068bc20
andreidanila1 <andrei.danila@analog.com>
2025-06-11 14:39:36 +0300

    test-plugins: Change the location of plugins in tst_pluginloader.
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- packages/test-plugins/plugins/test/test/tst_pluginloader.cpp
+++ packages/test-plugins/plugins/test/test/tst_pluginloader.cpp
@@ -44,7 +44,7 @@ private Q_SLOTS:
 	void metadata();
 };
 
-#define PLUGIN_LOCATION "../../plugins"
+#define PLUGIN_LOCATION "../.."
 #define FILENAME PLUGIN_LOCATION "/libscopy-test.so"
 
 void TST_TestPlugin::fileExists()
