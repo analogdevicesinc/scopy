908e1f67c971a51bafbc1d028ea7e38e7cce4608
Bindea Cristian <cristian.bindea@analog.com>
2025-06-25 12:59:49 +0300

    android 2: modificari in cod pt build

--- main.cpp
+++ main.cpp
@@ -33,6 +33,10 @@
 #include <gui/widgets/scopysplashscreen.h>
 #include <core/crashreport.h>
 
+#ifdef __ANDROID__
+	#include <QtAndroidExtras/QtAndroid>
+#endif
+
 using namespace scopy;
 
 Q_LOGGING_CATEGORY(CAT_RUNTIME_ENVIRONMENT_INFO, "RuntimeEnvironmentInfo")
@@ -103,6 +107,21 @@ int main(int argc, char *argv[])
 
 	initLogging();
 	CrashReport::initSignalHandler();
+
+	#ifdef  __ANDROID__
+
+	QAndroidJniObject jniObject = QtAndroid::androidActivity().callObjectMethod("getScaleFactor", "()Ljava/lang/String;");
+	QString scaleFactor = jniObject.toString();
+
+	qputenv("QT_SCALE_FACTOR", scaleFactor.toUtf8());
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+	qputenv("QT_USE_ANDROID_NATIVE_DIALOGS", "1");
+
+	QApplication::setAttribute(Qt::AA_CompressHighFrequencyEvents, true);
+	QApplication::setAttribute(Qt::AA_CompressTabletEvents, true);
+	qputenv("SCOPY_USE_OPEN_GL", "1");
+	#endif
 
 	QApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
 	QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
