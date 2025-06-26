d303eaff9427a151627138592e1f257e979489e9
Bindea <Cristian.Bindea@analog.com>
2025-06-26 16:54:25 +0300

    test

--- main.cpp
+++ main.cpp
@@ -16,7 +16,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
+ * 
+ * abcdefgh
  */
 
 #include "core/logging_categories.h"
