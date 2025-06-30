58341d672ce4e805448a686ceb8f0893ecd3b4f6
andreidanila1 <andrei.danila@analog.com>
2025-06-30 10:43:44 +0300

    m2k: Change the location of plugins in tst_pluginloader.
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- packages/m2k/plugins/m2k/test/tst_pluginloader.cpp
+++ packages/m2k/plugins/m2k/test/tst_pluginloader.cpp
@@ -45,7 +45,7 @@ private Q_SLOTS:
 	void unload();
 };
 
-#define PLUGIN_LOCATION "../../plugins"
+#define PLUGIN_LOCATION "../.."
 #define FILENAME PLUGIN_LOCATION "/libscopy-m2k.so"
 
 void TST_M2k::fileExists()
