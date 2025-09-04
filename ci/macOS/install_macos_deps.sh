dadf10d7dc01b4d83f8d1bc8f35d5add029015af
Bindea Cristian <cristian.bindea@analog.com>
2025-09-04 17:05:58 +0300

    ci/macos: remove cmake before installing other dependencies
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- ci/macOS/install_macos_deps.sh
+++ ci/macOS/install_macos_deps.sh
@@ -21,6 +21,9 @@ install_packages() {
 	rm -v /usr/local/bin/python3-config || true
 	rm -v /usr/local/bin/pip3.13 || true
 
+	# uninstall cmake before installing other dependencies https://github.com/actions/runner-images/issues/12912
+	brew uninstall --force cmake || true
+
 	brew update
 	# Workaround for brew taking a long time to upgrade existing packages
 	# Check if macOS version and upgrade packages only if the version is greater than macOS 12
@@ -31,10 +34,11 @@ install_packages() {
 		# Workaround: Install or update libtool package only if macOS version is greater than 12
 		# Note: libtool (v2.4.7) is pre-installed by default, but it can be updated to v2.5.3
 		PACKAGES="$PACKAGES libtool"
-		brew install --overwrite --display-times $PACKAGES
 	else
-		HOMEBREW_NO_AUTO_UPDATE=1 brew install --overwrite --display-times $PACKAGES
+		export HOMEBREW_NO_AUTO_UPDATE=1
 	fi
+
+	brew install --overwrite --display-times $PACKAGES
 
 	pip3 install --break-system-packages mako
 }
