6cd628e3bc56cebd6b2cd29ecb6405d7b275bd3f
IonutMuthi <ionut.muthi@analog.com>
2025-11-07 13:32:09 +0200

    generic: added global api register
    At plugin level we can now call "scopy::ApiList::registerApi(api);"
    We should also be sure to unregister the api onDisconnect() "scopy::ApiList::unregisterApi(api);"
    
    This would allow use to use api calls in other packages
    
    Signed-off-by: IonutMuthi <ionut.muthi@analog.com>

---
+++ pluginbase/include/pluginbase/apilist.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2024 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see https://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef APILIST_H
+#define APILIST_H
+
+#include "scopy-pluginbase_export.h"
+#include "apiobject.h"
+
+#include <QMap>
+#include <QPointer>
+#include <QMutex>
+#include <QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(CAT_APILIST)
+
+namespace scopy {
+
+class SCOPY_PLUGINBASE_EXPORT ApiList
+{
+public:
+	// Register API (similar to ScopyJS::registerApi)
+	static void registerApi(ApiObject* api);
+
+	// Unregister API (similar to ScopyJS::unregisterApi)
+	static void unregisterApi(ApiObject* api);
+
+	// Type-safe API access with availability checking
+	template<typename T>
+	static T* getApi(const QString& objectName) {
+		QMutexLocker locker(&s_mutex);
+		auto it = s_apis.find(objectName);
+		if (it == s_apis.end() || it->isNull()) {
+			return nullptr;
+		}
+		return qobject_cast<T*>(it->data());
+	}
+
+	// Check if API is available
+	static bool isAvailable(const QString& objectName);
+
+	// Get list of available APIs
+	static QStringList availableApis();
+
+private:
+	static QMap<QString, QPointer<ApiObject>> s_apis;
+	static QMutex s_mutex;
+
+	// Auto-cleanup when API object is destroyed
+	static void onApiDestroyed();
+};
+
+} // namespace scopy
+
+#endif // APILIST_H
---
+++ pluginbase/src/apilist.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2024 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see https://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "apilist.h"
+#include <QDebug>
+
+Q_LOGGING_CATEGORY(CAT_APILIST, "ApiList")
+
+using namespace scopy;
+
+// Static member definitions
+QMap<QString, QPointer<ApiObject>> ApiList::s_apis;
+QMutex ApiList::s_mutex;
+
+void ApiList::registerApi(ApiObject* api)
+{
+	if (!api) {
+		qWarning(CAT_APILIST) << "Cannot register null API";
+		return;
+	}
+
+	QMutexLocker locker(&s_mutex);
+	const QString objectName = api->objectName();
+
+	if (s_apis.contains(objectName)) {
+		qWarning(CAT_APILIST) << "API already registered for object:" << objectName << "- overriding";
+	}
+
+	s_apis[objectName] = QPointer<ApiObject>(api);
+	qInfo(CAT_APILIST) << "Registered API:" << objectName;
+
+	// Connect to destroyed signal for automatic cleanup
+	QObject::connect(api, &QObject::destroyed, &ApiList::onApiDestroyed);
+}
+
+void ApiList::unregisterApi(ApiObject* api)
+{
+	if (!api) {
+		return;
+	}
+
+	QMutexLocker locker(&s_mutex);
+	const QString objectName = api->objectName();
+
+	if (s_apis.remove(objectName) > 0) {
+		qInfo(CAT_APILIST) << "Unregistered API:" << objectName;
+	}
+}
+
+bool ApiList::isAvailable(const QString& objectName)
+{
+	QMutexLocker locker(&s_mutex);
+	auto it = s_apis.find(objectName);
+	return it != s_apis.end() && !it->isNull();
+}
+
+QStringList ApiList::availableApis()
+{
+	QMutexLocker locker(&s_mutex);
+	QStringList available;
+	for (auto it = s_apis.begin(); it != s_apis.end(); ++it) {
+		if (!it->isNull()) {
+			available << it.key();
+		}
+	}
+	return available;
+}
+
+void ApiList::onApiDestroyed()
+{
+	QMutexLocker locker(&s_mutex);
+	// Clean up null pointers from the map
+	auto it = s_apis.begin();
+	while (it != s_apis.end()) {
+		if (it->isNull()) {
+			qInfo(CAT_APILIST) << "Auto-cleaned destroyed API:" << it.key();
+			it = s_apis.erase(it);
+		} else {
+			++it;
+		}
+	}
+}
