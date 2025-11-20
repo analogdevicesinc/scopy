219772b1fcdecfc6d075e0d23143ac4aab4e0b51
IonutMuthi <ionut.muthi@analog.com>
2025-11-20 15:56:03 +0200

    adrv9009: add licence headers
    Signed-off-by: IonutMuthi <ionut.muthi@analog.com>

--- packages/adrv9009/plugins/adrv9009plugin/test/tst_pluginloader.cpp
+++ packages/adrv9009/plugins/adrv9009plugin/test/tst_pluginloader.cpp
@@ -43,7 +43,7 @@ private Q_SLOTS:
 	void metadata();
 };
 
-#define PLUGIN_LOCATION "../../plugins"
+#define PLUGIN_LOCATION "../.."
 #define FILENAME PLUGIN_LOCATION "/libscopy-adrv9009plugin.so"
 
 void TST_Adrv9009Plugin::fileExists()
