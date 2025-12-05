362381a6d7ab29dc34afaa90a1de0cfe9e477b59
IonutMuthi <ionut.muthi@analog.com>
2025-12-05 14:02:48 +0200

    adrv9009: adrv9009 advanced added tx settings, rx settings and orx settings
    Signed-off-by: IonutMuthi <ionut.muthi@analog.com>

---
+++ packages/adrv9009/plugins/adrv9009plugin/include/adrv9009plugin/advanced/orxsettingswidget.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Analog Devices Inc.
+ *
+ * This file is part of Scopy
+ * (see https://www.github.com/analogdevicesinc/scopy).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ORXSETTINGSWIDGET_H
+#define ORXSETTINGSWIDGET_H
+
+#include "scopy-adrv9009plugin_export.h"
+#include <QWidget>
+#include <gui/widgets/menusectionwidget.h>
+#include <iio-widgets/iiowidget.h>
+#include <iio.h>
+
+namespace scopy::adrv9009 {
+
+class SCOPY_ADRV9009PLUGIN_EXPORT OrxSettingsWidget : public QWidget
+{
+	Q_OBJECT
+public:
+	OrxSettingsWidget(iio_device *device, QWidget *parent = nullptr);
+	~OrxSettingsWidget();
+
+Q_SIGNALS:
+	void readRequested();
+
+private:
+	iio_device *m_device;
+
+	void setupUi();
+	QWidget *createOrxConfigSection(QWidget *parent);
+	QWidget *createOrxProfileSection(QWidget *parent);
+	QWidget *createAuxPllSection(QWidget *parent);
+};
+
+} // namespace scopy::adrv9009
+
+#endif // ORXSETTINGSWIDGET_H
