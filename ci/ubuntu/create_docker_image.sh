8addcd28732405c6f92e824d079071e61497c31b
Bindea Cristian <cristian.bindea@analog.com>
2025-12-08 16:10:47 +0200

    ci/ubuntu: add an ubuntu 24.04 image
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- ci/ubuntu/create_docker_image.sh
+++ ci/ubuntu/create_docker_image.sh
@@ -4,13 +4,28 @@ SRC_SCRIPT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ubuntu20(){
 	pushd $SRC_SCRIPT
-	docker build -t cristianbindea/scopy2-ubuntu20:testing -f docker_ubuntu20/Dockerfile .
+	docker build \
+		-t cristianbindea/scopy2-ubuntu20:testing \
+		--build-arg BASE_IMAGE=ubuntu:20.04 \
+		-f docker_ubuntu/Dockerfile .
 	popd
 }
 
 ubuntu22(){
 	pushd $SRC_SCRIPT
-	docker build -t cristianbindea/scopy2-ubuntu22:testing -f docker_ubuntu22/Dockerfile .
+	docker build \
+		-t cristianbindea/scopy2-ubuntu22:testing \
+		--build-arg BASE_IMAGE=ubuntu:22.04 \
+		-f docker_ubuntu/Dockerfile .
+	popd
+}
+
+ubuntu24(){
+	pushd $SRC_SCRIPT
+	docker build \
+		-t cristianbindea/scopy2-ubuntu24:testing \
+		--build-arg BASE_IMAGE=ubuntu:24.04 \
+		-f docker_ubuntu/Dockerfile .
 	popd
 }
 
