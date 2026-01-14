ffe8a12c6fd7a3fea89561f990ca21b65fd54d74
Bindea Cristian <cristian.bindea@analog.com>
2026-01-14 12:02:11 +0200

    ci/x86_64: add CI run for older Ubuntu versions
    Added an additional run to support Ubuntu versions earlier than 24.04.
    
    Signed-off-by: Bindea Cristian <cristian.bindea@analog.com>

--- ci/x86_64/create_docker_image.sh
+++ ci/x86_64/create_docker_image.sh
@@ -1,8 +1,25 @@
 #!/bin/bash -ex
 
 SRC_SCRIPT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd $SRC_SCRIPT
 
-docker build -t cristianbindea/scopy2-x86_64-appimage:testing -f docker/Dockerfile .
+ubuntu20(){
+	pushd $SRC_SCRIPT
+	docker build \
+		-t cristianbindea/scopy2-x86_64-appimage-ubuntu20:testing \
+		--build-arg BASE_IMAGE=ubuntu:20.04 \
+		-f docker/Dockerfile .
+	popd
+}
 
-popd
+ubuntu24(){
+	pushd $SRC_SCRIPT
+	docker build \
+		-t cristianbindea/scopy2-x86_64-appimage:testing \
+		--build-arg BASE_IMAGE=ubuntu:24.04 \
+		-f docker/Dockerfile .
+	popd
+}
+
+for arg in $@; do
+	$arg
+done
