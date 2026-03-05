53cd27ceca825d24d977ab81862a9f0d6d29ae37
Andrei Popa <andrei.popa@analog.com>
2026-03-05 17:39:28 +0200

    ADC: fix crash for unsupported bit count buffer capable devices - add support for 8 bit devices
    Signed-off-by: Andrei Popa <andrei.popa@analog.com>

--- gr-util/src/griiofloatchannelsrc.cpp
+++ gr-util/src/griiofloatchannelsrc.cpp
@@ -24,6 +24,7 @@
 #include "gnuradio/blocks/copy.h"
 #include "gnuradio/blocks/int_to_float.h"
 #include "gnuradio/blocks/short_to_float.h"
+#include "gnuradio/blocks/char_to_float.h"
 #include "grlog.h"
 #include "grtopblock.h"
 
@@ -59,16 +60,24 @@ void GRIIOFloatChannelSrc::build_blks(GRTopBlock *top)
 	qDebug(SCOPY_GR_UTIL) << "Building GRIIOFloatChannelSrc";
 	m_dev->addChannel(this);
 	switch(fmt->length) {
+	case 8:
+		x2f = gr::blocks::char_to_float::make();
+		break;
 	case 16:
 		x2f = gr::blocks::short_to_float::make();
 		break;
 	case 32:
 		x2f = gr::blocks::int_to_float::make();
 		break;
-	default:
-		qInfo(SCOPY_GR_UTIL) << "creating copy block of size " << fmt->length / 8;
-		x2f = gr::blocks::copy::make(fmt->length / 8);
+	default: {
+		static gr::logger logger("GRIIOFloatChannelSrc::build_blks");
+		std::string msg = "Unsupported IIO channel bit width: " + std::to_string(fmt->length) +
+			" - falling back to float copy block, data may be incorrect";
+		qCritical(SCOPY_GR_UTIL) << QString::fromStdString(msg);
+		logger.crit("{}", msg);
+		x2f = gr::blocks::copy::make(sizeof(float));
 		break;
+	}
 	}
 
 	s2v = gr::blocks::stream_to_vector::make(sizeof(float), top->vlen());
