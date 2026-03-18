c08400778b2113c649e65369900e2309d8fd96b8
IonutMuthi <Ionut.Muthi@analog.com>
2026-03-18 10:24:56 +0200

    cn0511: UI updates
    Signed-off-by: IonutMuthi <Ionut.Muthi@analog.com>

--- packages/cn0511/plugins/cn0511/src/cn0511.cpp
+++ packages/cn0511/plugins/cn0511/src/cn0511.cpp
@@ -30,6 +30,7 @@
 #include <style.h>
 #include <iio-widgets/iiowidgetgroup.h>
 
+#include <iio.h>
 #include <ad9166.h>
 
 Q_LOGGING_CATEGORY(CAT_CN0511, "CN0511");
@@ -101,6 +102,8 @@ CN0511::CN0511(iio_context *ctx, IIOWidgetGroup *group, QWidget *parent)
 		controlWidgetLayout->addWidget(generateSingleToneSection(controlsWidget));
 		controlWidgetLayout->addWidget(generateDacAmpSection(controlsWidget));
 		controlWidgetLayout->addItem(new QSpacerItem(1, 1, QSizePolicy::Preferred, QSizePolicy::Expanding));
+
+		connect(this, &CN0511::readRequested, this, &CN0511::readFrequencyFromDevice);
 	}
 
 	m_tool->addWidgetToCentralContainerHelper(controlsContainer);
@@ -116,47 +119,51 @@ QWidget *CN0511::generateSingleToneSection(QWidget *parent)
 
 	QVBoxLayout *layout = new QVBoxLayout(widget);
 	widget->setLayout(layout);
+	layout->setMargin(0);
+	layout->setContentsMargins(0, 0, 0, 0);
+	layout->setSpacing(10);
 
 	QLabel *titleLabel = new QLabel("Single Tone Mode", widget);
 	Style::setStyle(titleLabel, style::properties::label::menuBig);
 	layout->addWidget(titleLabel);
 
-	QGridLayout *gridLayout = new QGridLayout();
+	QHBoxLayout *controlsLayout = new QHBoxLayout();
+	controlsLayout->setMargin(0);
+	controlsLayout->setContentsMargins(0, 0, 0, 0);
 
-	// Frequency (MHz) - QDoubleSpinBox: 0-10000, 6 decimals, default 4500
-	QLabel *freqLabel = new QLabel("Frequency (MHz)", widget);
-	gridLayout->addWidget(freqLabel, 0, 0);
+	QVBoxLayout *configLayout = new QVBoxLayout();
+	configLayout->setMargin(0);
+	configLayout->setContentsMargins(0, 0, 0, 0);
+	configLayout->setSpacing(10);
 
-	m_freqSpinBox = new QDoubleSpinBox(widget);
-	m_freqSpinBox->setRange(0.0, 10000.0);
-	m_freqSpinBox->setDecimals(6);
-	m_freqSpinBox->setValue(4500.0);
-	m_freqSpinBox->setKeyboardTracking(false);
-	gridLayout->addWidget(m_freqSpinBox, 0, 1);
+	// Frequency (MHz) - MenuSpinbox: 0-10000, default 4500
+	m_freqSpinBox = new gui::MenuSpinbox("Frequency (MHz)", 0.0, "", 0.0, 10000.0, false, false, true, widget);
+	m_freqSpinBox->setScalingEnabled(false);
+	m_freqSpinBox->setIncrementMode(gui::MenuSpinbox::IS_FIXED);
+	m_freqSpinBox->incrementStrategy()->setScale(1.0);
+	m_freqSpinBox->setValue(4500);
+	configLayout->addWidget(m_freqSpinBox);
 
-	connect(m_freqSpinBox, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this,
+	connect(m_freqSpinBox, &gui::MenuSpinbox::valueChanged, this,
 		[this](double val) { m_calibFreq = val * MHZ_SCALE; });
 
-	// Amplitude (dBm) - QSpinBox: -91 to 0, default 0
-	QLabel *ampLabel = new QLabel("Amplitude (dBm)", widget);
-	gridLayout->addWidget(ampLabel, 1, 0);
+	// Amplitude (dBm) - MenuSpinbox: -91 to 0, default 0
+	m_ampSpinBox = new gui::MenuSpinbox("Amplitude (dBm)", 0.0, "", -91.0, 0.0, false, false, true, widget);
+	m_ampSpinBox->setScaleRange(1.0, 1.0); // Force combobox to have unit scale (empty otherwise since max=0)
+	m_ampSpinBox->setScalingEnabled(false);
+	m_ampSpinBox->setIncrementMode(gui::MenuSpinbox::IS_FIXED);
+	configLayout->addWidget(m_ampSpinBox);
 
-	m_ampSpinBox = new QSpinBox(widget);
-	m_ampSpinBox->setRange(-91, 0);
-	m_ampSpinBox->setValue(0);
-	m_ampSpinBox->setKeyboardTracking(false);
-	gridLayout->addWidget(m_ampSpinBox, 1, 1);
+	connect(m_ampSpinBox, &gui::MenuSpinbox::valueChanged, this, [this](double val) { m_amplitude = (int)val; });
 
-	connect(m_ampSpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this,
-		[this](int val) { m_amplitude = val; });
+	// Enter button - triggers calibration with loading animation
+	m_enterBtn = new AnimatedLoadingButton("Enter", widget);
+	connect(m_enterBtn, &QPushButton::clicked, this, &CN0511::applyCalibration);
 
-	// Enter button - triggers calibration
-	QPushButton *enterBtn = new QPushButton("Enter", widget);
-	Style::setStyle(enterBtn, style::properties::button::blueGrayButton);
-	connect(enterBtn, &QPushButton::clicked, this, &CN0511::applyCalibration);
-	gridLayout->addWidget(enterBtn, 2, 0, 1, 2);
+	controlsLayout->addLayout(configLayout, 1);
+	controlsLayout->addWidget(m_enterBtn, 1);
 
-	layout->addLayout(gridLayout);
+	layout->addLayout(controlsLayout);
 
 	return widget;
 }
@@ -199,27 +206,61 @@ void CN0511::applyCalibration()
 		return;
 	}
 
-	qDebug(CAT_CN0511) << "Applying calibration: freq=" << m_calibFreq << " amplitude=" << m_amplitude;
+	m_enterBtn->startAnimation();
 
-	struct ad9166_calibration_data *calibData = nullptr;
-	int ret = ad9166_context_find_calibration_data(m_ctx, "cn0511", &calibData);
-	if(ret < 0) {
-		qWarning(CAT_CN0511) << "Failed to find calibration data. Error:" << ret;
+	double calibFreq = m_calibFreq;
+	int amplitude = m_amplitude;
+
+	QFutureWatcher<void> *watcher = new QFutureWatcher<void>(this);
+	connect(
+		watcher, &QFutureWatcher<void>::finished, this,
+		[this, watcher]() {
+			m_enterBtn->stopAnimation();
+			watcher->deleteLater();
+		},
+		Qt::QueuedConnection);
+
+	QFuture<void> future = QtConcurrent::run([this, calibFreq, amplitude]() {
+		qDebug(CAT_CN0511) << "Applying calibration: freq=" << calibFreq << " amplitude=" << amplitude;
+
+		struct ad9166_calibration_data *calibData = nullptr;
+		int ret = ad9166_context_find_calibration_data(m_ctx, "cn0511", &calibData);
+		if(ret < 0) {
+			qWarning(CAT_CN0511) << "Failed to find calibration data. Error:" << ret;
+			return;
+		}
+
+		ret = ad9166_channel_set_freq(m_dacCh, calibFreq);
+		if(ret < 0) {
+			qWarning(CAT_CN0511) << "Failed to set frequency. Error:" << ret;
+		}
+
+		ret = ad9166_device_set_amplitude(m_dac, amplitude);
+		if(ret < 0) {
+			qWarning(CAT_CN0511) << "Failed to set amplitude. Error:" << ret;
+		}
+
+		ret = ad9166_device_set_iofs(m_dac, calibData, calibFreq);
+		if(ret < 0) {
+			qWarning(CAT_CN0511) << "Failed to set IOFS. Error:" << ret;
+		}
+	});
+
+	watcher->setFuture(future);
+}
+
+void CN0511::readFrequencyFromDevice()
+{
+	if(!m_dacCh || !m_freqSpinBox) {
 		return;
 	}
 
-	ret = ad9166_channel_set_freq(m_dacCh, m_calibFreq);
+	long long val = 0;
+	int ret = iio_channel_attr_read_longlong(m_dacCh, "nco_frequency", &val);
 	if(ret < 0) {
-		qWarning(CAT_CN0511) << "Failed to set frequency. Error:" << ret;
+		qWarning(CAT_CN0511) << "Failed to read nco_frequency. Error:" << ret;
+		return;
 	}
 
-	ret = ad9166_device_set_amplitude(m_dac, m_amplitude);
-	if(ret < 0) {
-		qWarning(CAT_CN0511) << "Failed to set amplitude. Error:" << ret;
-	}
-
-	ret = ad9166_device_set_iofs(m_dac, calibData, m_calibFreq);
-	if(ret < 0) {
-		qWarning(CAT_CN0511) << "Failed to set IOFS. Error:" << ret;
-	}
+	m_freqSpinBox->setValue(val / MHZ_SCALE);
 }
