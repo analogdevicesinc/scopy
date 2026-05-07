d713baa3115102d81f24feee6597e96f31774d7a
IonutMuthi <Ionut.Muthi@analog.com>
2026-05-07 11:13:00 +0300

    ci: add Windows Qt6 Docker image scripts
    Add toolchain, build process, installer, DLL deps, and Dockerfile
    for the Qt6 Windows Docker image using aqtinstall for Qt 6.8.3.
    
    
    Signed-off-by: IonutMuthi <Ionut.Muthi@analog.com>

---
+++ ci/windows/build_and_create_installer_qt6.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+if [ "$CI_SCRIPT" == "ON" ];
+	then
+		set -ex
+		export WORKDIR=$HOME
+		SRC_FOLDER=$WORKDIR/scopy
+	else
+		set -x
+		SRC_FOLDER=$(git rev-parse --show-toplevel)
+		export WORKDIR=$SRC_FOLDER
+fi
+
+BUILD_TARGET=x86_64
+ARCH_BIT=64
+
+USE_STAGING=OFF
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/mingw_toolchain_qt6.sh $USE_STAGING
+
+INSTALL_FOLDER=$STAGING_AREA/scopy-install
+BUILD_FOLDER=$WORKDIR/build_$ARCH
+ARTIFACT_FOLDER=$SRC_FOLDER/artifacts
+export DEST_FOLDER=$ARTIFACT_FOLDER/scopy-$ARCH
+DEBUG_FOLDER=$ARTIFACT_FOLDER/debug-$ARCH
+PYTHON_FILES=$STAGING_DIR/lib/python3.*
+EMU_BUILD_FOLDER=$WORKDIR/iio-emu/build
+
+download_tools() {
+	mkdir -p $STAGING_AREA
+	pacman -Qs mingw-w64-x86_64-wget2 > /dev/null || pacman -S --noconfirm mingw-w64-x86_64-wget2
+
+	pushd $STAGING_AREA
+	if [ ! -f windres.exe ]; then
+		wget2 http://swdownloads.analog.com/cse/build/windres.exe.gz
+		gunzip windres.exe.gz
+	fi
+
+	if [ ! -f dpinst.zip ]; then
+		wget2 http://swdownloads.analog.com/cse/m1k/drivers/dpinst.zip
+		unzip "dpinst.zip"
+	fi
+
+	if [ ! -f dfu-util.zip ]; then
+		wget2 http://swdownloads.analog.com/cse/m1k/drivers/dfu-util.zip
+		unzip "dfu-util.zip"
+	fi
+
+	if [ ! -f cv2pdb-dlls.zip ]; then
+		wget2 https://swdownloads.analog.com/cse/scopydeps/cv2pdb-dlls.zip
+		unzip "cv2pdb-dlls.zip"
+	fi
+	popd
+}
+
+
+build_scopy(){
+	echo "### Building Scopy"
+	download_cmake
+	[ -f $HOME/build-status ] && cp $HOME/build-status $SRC_FOLDER/build-status
+	mkdir -p $BUILD_FOLDER
+	cd $BUILD_FOLDER
+
+	$CMAKE $RC_COMPILER_OPT -DPYTHON_EXECUTABLE=$STAGING_DIR/bin/python3.exe \
+				-DENABLE_TESTING=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_FOLDER \
+				-DENABLE_PACKAGE_M2K=OFF \
+				$SRC_FOLDER
+	$MAKE_BIN $JOBS
+	ls -la $BUILD_FOLDER
+}
+
+build_iio-emu(){
+	echo "### Building IIO-EMU"
+	download_cmake
+	if [ ! -d "$WORKDIR/iio-emu" ]; then
+		git clone https://github.com/analogdevicesinc/iio-emu $WORKDIR/iio-emu
+	fi
+
+	mkdir -p $EMU_BUILD_FOLDER
+	cd $EMU_BUILD_FOLDER
+	$CMAKE -DBUILD_TOOLS=ON ../
+	$MAKE_BIN $JOBS
+
+	pushd $WORKDIR/iio-emu
+	echo "$(basename -a "$(git config --get remote.origin.url)") - $(git rev-parse --abbrev-ref HEAD) - $(git rev-parse --short HEAD)" >> $BUILD_STATUS_FILE
+	popd
+}
+
+bundle_drivers(){
+	echo "### Bundling drivers"
+	cp -R $SRC_FOLDER/ci/windows/drivers $DEST_FOLDER
+	if [[ $ARCH_BIT == "64" ]]; then
+		cp -R $STAGING_AREA/dfu-util-static-amd64.exe $DEST_FOLDER/drivers/dfu-util.exe
+		cp -R $STAGING_AREA/dpinst_amd64.exe $DEST_FOLDER/drivers/dpinst.exe
+	else
+		cp -R $STAGING_AREA/dfu-util-static.exe $DEST_FOLDER/drivers/dfu-util.exe
+		cp -R $STAGING_AREA/dpinst.exe $DEST_FOLDER/drivers/dpinst.exe
+	fi
+}
+
+
+deploy_app(){
+	echo "### Deploying application and dependencies"
+
+	rm -rf $INSTALL_FOLDER
+	pushd $BUILD_FOLDER
+	make $JOBS install
+	popd
+
+	rm -rf $DEST_FOLDER
+	mkdir -p $DEST_FOLDER
+
+	cp -v $INSTALL_FOLDER/bin/Scopy.exe $DEST_FOLDER/
+	cp -v $INSTALL_FOLDER/bin/Scopy-console.exe $DEST_FOLDER/
+	cp -v $INSTALL_FOLDER/bin/qt.conf $DEST_FOLDER/
+	cp -v $EMU_BUILD_FOLDER/iio-emu.exe $DEST_FOLDER/
+	cp -v $EMU_BUILD_FOLDER/tools/iio-emu_gen_xml.exe $DEST_FOLDER/
+
+	$QT/bin/windeployqt6.exe \
+		--dir $DEST_FOLDER \
+		--no-translations \
+		--no-system-d3d-compiler \
+		--no-compiler-runtime \
+		--no-quick-import \
+		--opengl \
+		--printsupport \
+		$DEST_FOLDER/Scopy.exe
+
+	cp -vr $INSTALL_FOLDER/lib/libscopy*.dll $DEST_FOLDER
+	cp -vr $INSTALL_FOLDER/lib/scopy/* $DEST_FOLDER
+	cp -vr $INSTALL_FOLDER/resources $DEST_FOLDER
+	cp -vr $STAGING_DIR/share/libsigrokdecode/decoders  $DEST_FOLDER/
+	rm -vfr $(find $DEST_FOLDER -name "*.dll.a" -type f)
+
+	cp -vr  $QT/plugins/renderers $DEST_FOLDER/
+	cp -vr  $QT/plugins/sceneparsers $DEST_FOLDER/
+
+	pushd $STAGING_DIR/bin
+	source $SRC_FOLDER/ci/windows/mingw_dll_deps_qt6
+	cp -vn "${DLL_DEPS[@]}" $DEST_FOLDER/
+	cp -nv iio_*.exe $DEST_FOLDER/
+	popd
+
+	if [ "$USE_STAGING" == "ON" ]; then
+		cp -v $STAGING_DIR/bin/libgenalyzer.dll $DEST_FOLDER/ || echo "Warning: genalyzer DLL not found in staging"
+	else
+		cp -v /$MINGW_VERSION/bin/libgenalyzer.dll $DEST_FOLDER/ || echo "Warning: genalyzer DLL not found in system"
+	fi
+
+	cp -r $PYTHON_FILES $DEST_FOLDER
+	cp $BUILD_FOLDER/windows/scopy-$ARCH_BIT.iss $DEST_FOLDER
+	cp $SRC_FOLDER/LICENSE $DEST_FOLDER/LICENSE.txt
+
+	bundle_drivers
+}
+
+
+extract_debug_symbols(){
+	echo "### Duplicating unstripped bundle"
+	rm -rf $DEBUG_FOLDER
+	mkdir -p $DEBUG_FOLDER
+	cp -r $DEST_FOLDER/* $DEBUG_FOLDER/
+
+	echo "### Stripping bundle for installer"
+	/$MINGW_VERSION/bin/strip.exe --verbose --strip-debug --strip-unneeded $(find $DEST_FOLDER -name "*.exe" -type f)
+	/$MINGW_VERSION/bin/strip.exe --verbose --strip-debug --strip-unneeded $(find $DEST_FOLDER -name "*.dll" -type f)
+}
+
+create_installer() {
+	echo "### Creating installer"
+	pushd $ARTIFACT_FOLDER
+	PATH="/c/innosetup:/c/Program Files (x86)/Inno Setup 6:$PATH"
+	iscc //p $BUILD_FOLDER/windows/scopy-$ARCH_BIT.iss
+
+	if [ "$CI_SCRIPT" == "ON" ]; then
+		mv $WORKDIR/scopy-$ARCH_BIT-setup.exe $ARTIFACT_FOLDER
+		zip scopy-$ARCH_BIT-setup.zip scopy-$ARCH_BIT-setup.exe
+		pushd $DEBUG_FOLDER
+		zip -r $ARTIFACT_FOLDER/debug-x86_64.zip .
+		popd
+		pushd $DEST_FOLDER
+		zip -r $ARTIFACT_FOLDER/scopy-x86_64.zip .
+		popd
+	else
+		mv $WORKDIR/scopy-$ARCH_BIT-setup.exe $ARTIFACT_FOLDER
+	fi
+
+	ls -la $ARTIFACT_FOLDER
+	echo "Done. Artifacts generated in $ARTIFACT_FOLDER"
+	popd
+}
+
+move_tools(){
+	[ -d /home/docker/staging ] && mv /home/docker/staging $STAGING_AREA || echo "Staging folder not found or already moved"
+	if [ ! -d $STAGING_AREA ]; then
+		echo "Missing tools folder, downloading now"
+		download_tools
+	fi
+}
+
+run_workflow(){
+	[ "$CI_SCRIPT" == "ON" ] && move_tools || download_tools
+	build_scopy
+	build_iio-emu
+	deploy_app
+	extract_debug_symbols
+	create_installer
+}
+
+for arg in $@; do
+	$arg
+done
---
+++ ci/windows/windows_build_process_qt6.sh
@@ -0,0 +1,427 @@
+#!/usr/bin/bash.exe
+
+set -xe
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+USE_STAGING=OFF
+
+source $SCRIPT_DIR/mingw_toolchain_qt6.sh $USE_STAGING
+
+install_packages() {
+	SYSTEM_PKGS="\
+		git\
+		svn\
+		vim\
+		unzip\
+		zip\
+	"
+
+	TOOLS_PKGS="\
+		mingw-w64-${ARCH}-wget2\
+		mingw-w64-${ARCH}-cmake\
+		mingw-w64-${ARCH}-gcc\
+		mingw-w64-${ARCH}-python3\
+		mingw-w64-${ARCH}-python-mako\
+		mingw-w64-${ARCH}-python-six\
+		mingw-w64-${ARCH}-python-pip\
+		mingw-w64-${ARCH}-make\
+		mingw-w64-${ARCH}-doxygen\
+		mingw-w64-${ARCH}-pcre2\
+		base-devel\
+		mingw-w64-${ARCH}-autotools\
+		libtool\
+		mingw-w64-${ARCH}-boost\
+		mingw-w64-${ARCH}-ccache
+	"
+
+	PACMAN_SYNC_DEPS="\
+		mingw-w64-${ARCH}-fftw\
+		mingw-w64-${ARCH}-orc\
+		mingw-w64-${ARCH}-libxml2\
+		mingw-w64-${ARCH}-libzip\
+		mingw-w64-${ARCH}-fftw\
+		mingw-w64-${ARCH}-libffi\
+		mingw-w64-${ARCH}-glib2\
+		mingw-w64-${ARCH}-glibmm\
+		mingw-w64-${ARCH}-doxygen\
+		mingw-w64-${ARCH}-zlib\
+		mingw-w64-${ARCH}-breakpad\
+		mingw-w64-${ARCH}-libusb\
+		mingw-w64-${ARCH}-matio
+	"
+
+	if [ "$USE_STAGING" == "ON" ]; then
+		mkdir -p $STAGING_DIR/var/lib/pacman/local
+		mkdir -p $STAGING_DIR/var/lib/pacman/sync
+		$PACMAN -Syuu bash filesystem mintty pacman
+	fi
+
+	pacman --noconfirm -S $SYSTEM_PKGS
+	$PACMAN -S $TOOLS_PKGS
+	$PACMAN -S $PACMAN_SYNC_DEPS
+
+	download_cmake
+}
+
+install_qt() {
+	pip3 install aqtinstall
+	python3 -m aqt install-qt --outputdir /c/Qt windows desktop 6.8.3 win64_mingw -m qt3d qtscxml
+}
+
+clone() {
+	echo "#######CLONE#######"
+	mkdir -p $STAGING_AREA
+	pushd $STAGING_AREA
+	[ -d 'libserialport' ] || git clone --recursive https://github.com/sigrokproject/libserialport -b $LIBSERIALPORT_BRANCH libserialport
+	[ -d 'libiio' ]		|| git clone --recursive https://github.com/analogdevicesinc/libiio.git -b $LIBIIO_VERSION libiio
+	[ -d 'libad9361' ]	|| git clone --recursive https://github.com/analogdevicesinc/libad9361-iio.git -b $LIBAD9361_BRANCH libad9361
+	[ -d 'libm2k' ]		|| git clone --recursive https://github.com/analogdevicesinc/libm2k.git -b $LIBM2K_BRANCH libm2k
+	[ -d 'spdlog' ]		|| git clone --recursive https://github.com/gabime/spdlog.git -b $SPDLOG_BRANCH spdlog
+	[ -d 'libsndfile' ]	|| git clone --recursive https://github.com/libsndfile/libsndfile -b $LIBSNDFILE_BRANCH libsndfile
+	[ -d 'gr-scopy' ]	|| git clone --recursive https://github.com/analogdevicesinc/gr-scopy.git -b $GRSCOPY_BRANCH gr-scopy
+	[ -d 'gr-m2k' ]		|| git clone --recursive https://github.com/analogdevicesinc/gr-m2k.git -b $GRM2K_BRANCH gr-m2k
+	[ -d 'volk' ]		|| git clone --recursive https://github.com/gnuradio/volk.git -b $VOLK_BRANCH volk
+	[ -d 'gnuradio' ]	|| git clone --recursive https://github.com/analogdevicesinc/gnuradio.git -b $GNURADIO_BRANCH gnuradio
+	[ -d 'qwt' ]		|| git clone --recursive https://github.com/cseci/qwt.git -b $QWT_BRANCH qwt
+	[ -d 'libsigrokdecode' ] || git clone --recursive https://github.com/sigrokproject/libsigrokdecode.git -b $LIBSIGROKDECODE_BRANCH libsigrokdecode
+	[ -d 'libtinyiiod' ]	|| git clone --recursive https://github.com/analogdevicesinc/libtinyiiod.git -b $LIBTINYIIOD_BRANCH libtinyiiod
+	[ -d 'KDDockWidgets' ] || git clone --recursive https://github.com/KDAB/KDDockWidgets.git -b $KDDOCK_BRANCH KDDockWidgets
+	[ -d 'extra-cmake-modules' ] || git clone --recursive https://github.com/KDE/extra-cmake-modules.git -b $ECM_BRANCH extra-cmake-modules
+	[ -d 'karchive' ] || git clone --recursive https://github.com/KDE/karchive.git -b $KARCHIVE_BRANCH karchive
+	[ -d 'genalyzer' ] || git clone --recursive https://github.com/analogdevicesinc/genalyzer.git -b $GENALYZER_BRANCH genalyzer
+	popd
+}
+
+create_build_status_file() {
+	touch $BUILD_STATUS_FILE
+	echo "Scopy2-MinGW-Qt6" >> $BUILD_STATUS_FILE
+	echo "Docker image built on $(date)" >> $BUILD_STATUS_FILE
+	echo "Deps installed using pacman" >> $BUILD_STATUS_FILE
+	echo "Qt6 installed via aqtinstall at $QT" >> $BUILD_STATUS_FILE
+	echo "" >> $BUILD_STATUS_FILE
+	echo "All explicitly installed packages on build machine" >> $BUILD_STATUS_FILE
+	echo "" >> $BUILD_STATUS_FILE
+	pacman --noconfirm -Qe >> $BUILD_STATUS_FILE
+	echo "" >> $BUILD_STATUS_FILE
+	echo "Deps built from sources" >> $BUILD_STATUS_FILE
+	echo "" >> $BUILD_STATUS_FILE
+}
+
+clean_build_dir() {
+	git clean -xdf
+	rm -rf $BUILD_FOLDER
+	mkdir $BUILD_FOLDER
+	cd $BUILD_FOLDER
+}
+
+build_with_cmake() {
+	INSTALL=$1
+	[ -z $INSTALL ] && INSTALL=ON
+	pushd $STAGING_AREA/$CURRENT_BUILD
+	BUILD_FOLDER=$PWD/build
+	clean_build_dir
+	eval $CURRENT_BUILD_POST_CLEAN
+	eval $CURRENT_BUILD_PATCHES
+
+	$CMAKE $CURRENT_BUILD_CMAKE_OPTS $STAGING_AREA/$CURRENT_BUILD
+	eval $CURRENT_BUILD_POST_CMAKE
+	make $JOBS
+	if [ "$INSTALL" == "ON" ];then
+		make install
+	fi
+	eval $CURRENT_BUILD_POST_MAKE
+
+	echo "$(basename -a "$(git config --get remote.origin.url)") - $(git rev-parse --abbrev-ref HEAD) - $(git rev-parse --short HEAD)" \
+	>> $BUILD_STATUS_FILE
+
+	if [ "$INSTALL" == "ON" ] && [ "$CI_SCRIPT" == "ON" ];then
+		git clean -xdf
+	fi
+
+	popd
+
+	CURRENT_BUILD_CMAKE_OPTS=""
+	CURRENT_BUILD_POST_CLEAN=""
+	CURRENT_BUILD_PATCHES=""
+	CURRENT_BUILD_POST_CMAKE=""
+	CURRENT_BUILD_POST_MAKE=""
+	CURRENT_BUILD=""
+}
+
+build_libserialport(){
+	CURRENT_BUILD=libserialport
+	pushd $STAGING_AREA/$CURRENT_BUILD
+	git clean -xdf
+
+	INSTALL=$1
+	[ -z $INSTALL ] && INSTALL=ON
+
+	./autogen.sh
+	[ "$USE_STAGING" == "ON" ] && ./configure --prefix $STAGING_AREA_DEPS ${AUTOCONF_OPTS} || ./configure ${AUTOCONF_OPTS}
+	make $JOBS
+	[ "$INSTALL" == "ON" ] && make install
+
+	if [ "$INSTALL" == "ON" ] && [ "$CI_SCRIPT" == "ON" ];then
+		git clean -xdf
+	fi
+
+	echo "$(basename -a "$(git config --get remote.origin.url)") - $(git rev-parse --abbrev-ref HEAD) - $(git rev-parse --short HEAD)" \
+	>> $BUILD_STATUS_FILE
+	popd
+}
+
+build_libiio() {
+	CURRENT_BUILD=libiio
+	CURRENT_BUILD_CMAKE_OPTS="\
+		${RC_COMPILER_OPT}\
+		-DWITH_USB_BACKEND:BOOL=ON\
+		-DWITH_SERIAL_BACKEND:BOOL=ON\
+		-DCSHARP_BINDINGS:BOOL=OFF\
+		-DPYTHON_BINDINGS:BOOL=OFF\
+		-DHAVE_DNS_SD:BOOL=ON\
+		-DENABLE_IPV6:BOOL=OFF\
+		-DWITH_EXAMPLES:BOOL=ON\
+	"
+	build_with_cmake $1
+}
+
+build_libad9361() {
+	echo "### Building libad9361 - branch $LIBAD9361_BRANCH"
+	CURRENT_BUILD=libad9361
+	build_with_cmake $1
+}
+
+build_spdlog() {
+	CURRENT_BUILD=spdlog
+	CURRENT_BUILD_CMAKE_OPTS="\
+		-DSPDLOG_BUILD_SHARED=ON\
+		-DSPDLOG_BUILD_EXAMPLE=OFF\
+		"
+	build_with_cmake $1
+}
+
+build_libm2k() {
+	CURRENT_BUILD=libm2k
+	CURRENT_BUILD_CMAKE_OPTS="\
+		-DENABLE_PYTHON=OFF\
+		-DENABLE_CSHARP=OFF\
+		-DBUILD_EXAMPLES=OFF\
+		-DENABLE_TOOLS=ON\
+		-DINSTALL_UDEV_RULES=OFF\
+		"
+	build_with_cmake $1
+}
+
+build_libsndfile() {
+	CURRENT_BUILD=libsndfile
+	CURRENT_BUILD_CMAKE_OPTS="\
+	-DENABLE_EXTERNAL_LIBS=OFF\
+	-DENABLE_MPEG=OFF\
+	-DBUILD_PROGRAMS=OFF\
+	-DBUILD_EXAMPLES=OFF\
+	-DENABLE_CPACK=OFF\
+	-DBUILD_SHARED_LIBS=OFF\
+	-DBUILD_TESTING=OFF"
+	build_with_cmake $1
+}
+
+build_volk() {
+	CURRENT_BUILD=volk
+	CURRENT_BUILD_POST_CLEAN="git submodule update --init ../cpu_features"
+	CURRENT_BUILD_CMAKE_OPTS="\
+		-DENABLE_MODTOOL=OFF\
+		-DENABLE_TESTING=OFF\
+		-DPYTHON_EXECUTABLE=$STAGING_DIR/bin/python3.exe\
+		-DGR_PYTHON_DIR==$STAGING_DIR/lib/python3.10/site-packages\
+		"
+	build_with_cmake $1
+
+}
+
+build_gnuradio() {
+	CURRENT_BUILD=gnuradio
+	CURRENT_BUILD_CMAKE_OPTS="\
+		-DENABLE_DEFAULT=OFF\
+		-DENABLE_GNURADIO_RUNTIME=ON\
+		-DENABLE_GR_ANALOG=ON\
+		-DENABLE_GR_BLOCKS=ON\
+		-DENABLE_GR_FFT=ON\
+		-DENABLE_GR_FILTER=ON\
+		-DENABLE_VOLK=ON\
+		-DENABLE_GR_IIO=ON\
+		-DENABLE_POSTINSTALL=OFF\
+		-DCMAKE_C_FLAGS=-fno-asynchronous-unwind-tables\
+		-DPYTHON_EXECUTABLE=$STAGING_DIR/bin/python3.exe\
+		-DGR_PYTHON_DIR==$STAGING_DIR/lib/python3.10/site-packages\
+		"
+	build_with_cmake $1
+}
+
+build_grscopy() {
+	CURRENT_BUILD=gr-scopy
+	CURRENT_BUILD_CMAKE_OPTS="\
+		-DPYTHON_EXECUTABLE=$STAGING_DIR/bin/python3.exe\
+		-DGR_PYTHON_DIR==$STAGING_DIR/lib/python3.10/site-packages\
+		"
+	build_with_cmake $1
+}
+
+build_grm2k() {
+	CURRENT_BUILD=gr-m2k
+	CURRENT_BUILD_CMAKE_OPTS="\
+		-DPYTHON_EXECUTABLE=$STAGING_DIR/bin/python3.exe\
+		-DGR_PYTHON_DIR==$STAGING_DIR/lib/python3.10/site-packages\
+		"
+	build_with_cmake $1
+}
+
+build_qwt() {
+	echo "### Building qwt - branch $QWT_BRANCH"
+	CURRENT_BUILD=qwt
+	pushd $STAGING_AREA/$CURRENT_BUILD
+	git clean -xdf
+
+patch -p1 <<-EOF
+--- a/qwtconfig.pri
++++ b/qwtconfig.pri
+@@ -24,7 +24,7 @@ unix {
+ }
+
+ win32 {
+-    QWT_INSTALL_PREFIX    = ""
++    QWT_INSTALL_PREFIX    = "/mingw64"
+     # QWT_INSTALL_PREFIX = C:/Qwt-\$\$QWT_VERSION-dev-qt-\$\$QT_VERSION
+ }
+
+EOF
+
+	INSTALL=$1
+	[ -z $INSTALL ] && INSTALL=ON
+
+	if [ "$USE_STAGING" == "ON" ]
+	then
+		$QMAKE INCLUDEPATH=$STAGING_AREA_DEPS/include LIBS=-L$STAGING_AREA_DEPS/lib qwt.pro
+		make $JOBS
+		if [ "$INSTALL" == "ON" ];then
+			make INSTALL_ROOT=$STAGING_AREA_DEPS install
+		fi
+		cp -r $STAGING_AREA_DEPS/usr/local/* $STAGING_AREA_DEPS/
+	else
+		$QMAKE qwt.pro
+		make $JOBS
+		make install
+	fi
+
+	cp $STAGING_DIR/lib/qwt.dll $STAGING_DIR/bin/qwt.dll
+
+	if [ "$INSTALL" == "ON" ] && [ "$CI_SCRIPT" == "ON" ];then
+		git clean -xdf
+	fi
+
+	echo "$(basename -a "$(git config --get remote.origin.url)") - $(git rev-parse --abbrev-ref HEAD) - $(git rev-parse --short HEAD)" \
+	>> $BUILD_STATUS_FILE
+	popd
+}
+
+build_libsigrokdecode() {
+	echo "### Building libsigrokdecode - branch $LIBSIGROKDECODE_BRANCH"
+	CURRENT_BUILD=libsigrokdecode
+	pushd $STAGING_AREA/$CURRENT_BUILD
+	git reset --hard
+	git clean -xdf
+	patch -p1 < ${WORKFOLDER}/sigrokdecode-windows-fix.patch
+	./autogen.sh
+
+	INSTALL=$1
+	[ -z $INSTALL ] && INSTALL=ON
+
+	if [ "$USE_STAGING" == "ON" ]
+	then
+		CPPFLAGS="-DLIBSIGROKDECODE_EXPORT=1" ./configure --prefix $STAGING_AREA_DEPS ${AUTOCONF_OPTS}
+		LD_RUN_PATH=$STAGING_AREA_DEPS/lib make $JOBS
+	else
+		CPPFLAGS="-DLIBSIGROKDECODE_EXPORT=1" ./configure ${AUTOCONF_OPTS}
+		make $JOBS
+	fi
+
+	if [ "$INSTALL" == "ON" ];then
+		make install
+	fi
+
+	if [ "$INSTALL" == "ON" ] && [ "$CI_SCRIPT" == "ON" ];then
+		git clean -xdf
+	fi
+
+	echo "$(basename -a "$(git config --get remote.origin.url)") - $(git rev-parse --abbrev-ref HEAD) - $(git rev-parse --short HEAD)" \
+	>> $BUILD_STATUS_FILE
+	popd
+}
+
+build_libtinyiiod() {
+	echo "### Building libtinyiiod - branch $LIBTINYIIOD_BRANCH"
+	CURRENT_BUILD=libtinyiiod
+	CURRENT_BUILD_CMAKE_OPTS="-DBUILD_EXAMPLES=OFF"
+	build_with_cmake $1
+}
+
+build_kddock () {
+	echo "### Building KDDockWidgets - version $KDDOCK_BRANCH"
+	CURRENT_BUILD=KDDockWidgets
+	CURRENT_BUILD_CMAKE_OPTS="-DKDDockWidgets_QT6=ON -DKDDockWidgets_FRONTENDS=qtwidgets -DKDDockWidgets_EXAMPLES=OFF -DKDDockWidgets_TESTS=OFF"
+	build_with_cmake $1
+}
+
+build_ecm() {
+	echo "### Building extra-cmake-modules (ECM) - branch $ECM_BRANCH"
+	CURRENT_BUILD=extra-cmake-modules
+	CURRENT_BUILD_CMAKE_OPTS="-DBUILD_TESTING=OFF -DBUILD_HTML_DOCS=OFF -DBUILD_MAN_DOCS=OFF -DBUILD_QTHELP_DOCS=OFF"
+	build_with_cmake $1
+}
+
+build_karchive () {
+	echo "### Building karchive - version $KARCHIVE_BRANCH"
+	CURRENT_BUILD=karchive
+	CURRENT_BUILD_CMAKE_OPTS="-DBUILD_TESTING=OFF"
+	build_with_cmake $1
+}
+
+build_genalyzer() {
+	echo "### Building genalyzer - branch $GENALYZER_BRANCH"
+	CURRENT_BUILD=genalyzer
+	CURRENT_BUILD_CMAKE_OPTS="\
+		-DBUILD_TESTING=OFF \
+		-DBUILD_SHARED_LIBS=ON \
+		"
+	build_with_cmake $1
+}
+
+build_deps() {
+	install_packages
+	install_qt
+	create_build_status_file
+	clone
+
+	build_libserialport ON
+	build_libiio ON
+	build_libad9361 ON
+	build_libm2k ON
+	build_spdlog ON
+	build_libsndfile ON
+	build_volk ON
+	build_gnuradio ON
+	build_grscopy ON
+	build_grm2k ON
+	build_qwt ON
+	build_libsigrokdecode ON
+	build_libtinyiiod ON
+	build_kddock ON
+	build_ecm ON
+	build_karchive ON
+	build_genalyzer ON
+}
+
+for arg in $@; do
+	$arg
+done
+
+build_deps
