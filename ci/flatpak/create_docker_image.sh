3fac5b5e8886e6da5d3d58206ee1125e8a0feeff
IonutMuthi <Ionut.Muthi@analog.com>
2026-07-10 17:02:14 +0300

    ci: add Qt6 Flatpak build pipeline
    Signed-off-by: IonutMuthi <Ionut.Muthi@analog.com>

---
+++ ci/flatpak/create_docker_image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+# Create Docker Image for Flatpak Build
+# ===========================
+# Usage: ./create_docker_image.sh
+
+SRC_DIR=$(git rev-parse --show-toplevel 2>/dev/null ) || \
+SRC_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../../ && pwd )
+
+build_docker(){
+	pushd $SRC_DIR/ci/flatpak/docker
+	docker build -t docker.cloudsmith.io/adi/scopy-dockers/scopy2-flatpak-qt6:testing  .
+	CONTAINER_NAME=builder-flatpak
+	docker run \
+		--name $CONTAINER_NAME \
+		--privileged \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/build_flatpak_deps.sh",target=/home/runner/config/build_flatpak_deps.sh,readonly \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/defined_variables.h",target=/home/runner/config/defined_variables.h,readonly \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/org.adi.Scopy.json.c",target=/home/runner/config/org.adi.Scopy.json.c,readonly \
+		--mount type=bind,source="$SRC_DIR/ci/flatpak/Makefile",target=/home/runner/config/Makefile,readonly \
+		docker.cloudsmith.io/adi/scopy-dockers/scopy2-flatpak-qt6:testing /bin/bash -c /home/runner/config/build_flatpak_deps.sh
+
+	docker commit $CONTAINER_NAME docker.cloudsmith.io/adi/scopy-dockers/scopy2-flatpak-qt6:testing
+	echo "Docker container $CONTAINER_NAME commited as docker.cloudsmith.io/adi/scopy-dockers/scopy2-flatpak-qt6:testing image"
+	docker container rm $CONTAINER_NAME
+	popd
+}
+
+build_docker
