318c46773499e6da9b3440f4998aa0cc47e73500
andreidanila1 <andrei.danila@analog.com>
2026-07-28 17:18:09 +0300

    core: Refactoring the executors.
    Consolidated the two command executors into a single implementation.
    Removed ConcurrentExecutor and SerialCmdExecutor, replacing them with
    PooledCmdExecutor
    
    Signed-off-by: andreidanila1 <andrei.danila@analog.com>

--- device-controller/core/include/core/icmdexecutor.h
+++ device-controller/core/include/core/icmdexecutor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QFuture>
+#include <QUuid>
 
 namespace scopy {
 
@@ -10,9 +11,12 @@ class ICmdExecutor
 {
 public:
 	virtual ~ICmdExecutor() = default;
+
 	virtual QFuture<void> execute(Command *cmd) = 0;
+
+	virtual void cancelById(const QUuid &id) = 0;
 	virtual void cancelByResource(void *resource) = 0;
-    virtual void cancelAll() = 0;
+	virtual void cancelAll() = 0;
 	virtual int pendingCount() const = 0;
 };
 
